cc16059534ad8be3be6afc4317f62f82359afd60
Russell Sears <sears@microsoft.com>
2012-11-13 11:09:30 -0800

    other example from documentation

---
+++ tang/src/test/java/com/example/TimerV1.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import javax.inject.Inject;
+
+import com.microsoft.tang.Configuration;
+import com.microsoft.tang.ConfigurationBuilder;
+import com.microsoft.tang.Injector;
+import com.microsoft.tang.Tang;
+import com.microsoft.tang.annotations.Name;
+import com.microsoft.tang.annotations.NamedParameter;
+import com.microsoft.tang.annotations.Parameter;
+import com.microsoft.tang.exceptions.BindException;
+import com.microsoft.tang.exceptions.InjectionException;
+
+public class TimerV1 {
+
+  @NamedParameter(default_value="10",
+      doc="Number of seconds to sleep", short_name="sec")
+  class Seconds implements Name<Integer> {}
+  private final int seconds;
+
+  @Inject
+  public TimerV1(@Parameter(Seconds.class) int seconds) {
+    this.seconds = seconds;
+  }
+
+  public void sleep() throws InterruptedException {
+    java.lang.Thread.sleep(seconds * 1000);
+  }
+  
+  public static void main(String[] args) throws BindException, InjectionException {
+    Tang tang = Tang.Factory.getTang();
+    ConfigurationBuilder cb = (ConfigurationBuilder)tang.newConfigurationBuilder();
+    cb.register(TimerV1.class);
+    Configuration conf = cb.build();
+    Injector injector = tang.newInjector(conf);
+    TimerV1 timer = injector.getInstance(TimerV1.class);
+    
+    try {
+      System.out.println("Tick...");
+      timer.sleep();
+      System.out.println("Tock.");
+    } catch(InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
