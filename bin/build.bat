0729a59f301ea1ddd82b3837c0fc431d784bb7b2
Rusty Sears <sears@microsoft.com>
2013-01-23 15:13:22 -0800

    merge pom fixes from Markus; tang now complies + passes tests

--- bin/build.bat
+++ bin/build.bat
@@ -1,6 +1,1 @@
 mvn -q -fae -DskipTests clean install eclipse:clean eclipse:eclipse package
-cd reef-examples
-#mvn -q -fae -DskipTests clean compile eclipse:clean eclipse:eclipse
-cd ..
-cd reef-distro
-mvn -q assembly:assembly
--- bin/test.bat
+++ bin/test.bat
@@ -1,3 +1,1 @@
 mvn -q clean install eclipse:clean eclipse:eclipse
-cd reef-examples
-mvn -q clean compile eclipse:clean eclipse:eclipse
