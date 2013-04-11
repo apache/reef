46f6306a853fc2608177e48cea151fd12cda18ab
Russell Sears <sears@microsoft.com>
2013-04-11 15:27:01 -0700

    Reindent TestExternalConstructor

--- tang/src/test/java/com/microsoft/tang/TestExternalConstructor.java
+++ tang/src/test/java/com/microsoft/tang/TestExternalConstructor.java
@@ -9,47 +9,51 @@ import com.microsoft.tang.exceptions.InjectionException;
 
 public class TestExternalConstructor {
 
-	static final class A {
-		A() {}
-	}
-	
-	static final class B {
-		B(final A a) {}
-	}
-	
-	static final class ACons implements ExternalConstructor<A> {
+  static final class A {
+    A() {
+    }
+  }
 
-		@Inject
-		ACons() {}
-		
-		@Override
-		public A newInstance() {
-			return new A();
-		}
-	}
-	
-	static final class BCons implements ExternalConstructor<B> {
-		
-		@Inject
-		BCons(final A a) {}
+  static final class B {
+    B(final A a) {
+    }
+  }
 
-		@Override
-		public B newInstance() {
-			return new B(null);
-		}
-	}
-	
+  static final class ACons implements ExternalConstructor<A> {
 
-	  @Test
-	  public void testExternalConstructor() throws BindException, InjectionException {
+    @Inject
+    ACons() {
+    }
 
-	    final JavaConfigurationBuilder b = Tang.Factory.getTang()
-	        .newConfigurationBuilder();
-	    b.bindSingleton(A.class); // NOTE: the test passes if you remove this line
-	    b.bindConstructor(A.class, ACons.class);
-	    b.bindConstructor(B.class, BCons.class);
-	    
-		Tang.Factory.getTang().newInjector(b.build()).getInstance(B.class);
-	  }
+    @Override
+    public A newInstance() {
+      return new A();
+    }
+  }
+
+  static final class BCons implements ExternalConstructor<B> {
+
+    @Inject
+    BCons(final A a) {
+    }
+
+    @Override
+    public B newInstance() {
+      return new B(null);
+    }
+  }
+
+  @Test
+  public void testExternalConstructor() throws BindException,
+      InjectionException {
+
+    final JavaConfigurationBuilder b = Tang.Factory.getTang()
+        .newConfigurationBuilder();
+    b.bindSingleton(A.class); // NOTE: the test passes if you remove this line
+    b.bindConstructor(A.class, ACons.class);
+    b.bindConstructor(B.class, BCons.class);
+
+    Tang.Factory.getTang().newInjector(b.build()).getInstance(B.class);
+  }
 
 }
