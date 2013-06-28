1ceeff8cd1b61a4ae05c123a0263771d1792322a
Rusty Sears <sears@microsoft.com>
2013-06-27 18:18:01 -0700

    bugfixes + make ConfigurationModule immutable

--- tang/src/main/java/com/microsoft/tang/formats/ConfigurationModule.java
+++ tang/src/main/java/com/microsoft/tang/formats/ConfigurationModule.java
@@ -16,29 +16,30 @@ import com.microsoft.tang.annotations.Name;
 import com.microsoft.tang.exceptions.BindException;
 import com.microsoft.tang.exceptions.ClassHierarchyException;
 
-public class ConfigurationModule {
-  final JavaConfigurationBuilder b = Tang.Factory.getTang()
-      .newConfigurationBuilder();
-  
-  public interface Impl<T> { };
-  public interface Param<T> { };
-  
+public abstract class ConfigurationModule {
+
+  public interface Impl<T> {
+  };
+
+  public interface Param<T> {
+  };
+
   public static final class RequiredImpl<T> implements Impl<T> {
     public RequiredImpl() {
     }
   }
 
-  public static final class OptionalImpl<T> implements Impl<T>{
+  public static final class OptionalImpl<T> implements Impl<T> {
     public OptionalImpl() {
     }
   }
 
-  public static final class RequiredParameter<T> implements Param<T>{
+  public static final class RequiredParameter<T> implements Param<T> {
     public RequiredParameter() {
     }
   }
 
-  public static final class OptionalParameter<T> implements Param<T>{
+  public static final class OptionalParameter<T> implements Param<T> {
     public OptionalParameter() {
     }
   }
@@ -47,6 +48,8 @@ public class ConfigurationModule {
       RequiredImpl.class, OptionalImpl.class, RequiredParameter.class,
       OptionalParameter.class));
 
+  final JavaConfigurationBuilder b = Tang.Factory.getTang()
+      .newConfigurationBuilder();
   // Sets of things that have been declared but not used in a bind.
   // Both must be empty before build().
   private final Set<Field> reqUse = new HashSet<>();
@@ -62,9 +65,42 @@ public class ConfigurationModule {
   private final Map<Param<?>, Class<? extends Name<?>>> freeParams = new HashMap<>();
   private final Map<Impl<?>, Class<?>> setImpls = new HashMap<>();
   private final Map<Param<?>, String> setParams = new HashMap<>();
-  
+
+  private final ConfigurationModule deepCopy() {
+    // ooh... this is a dirty trick --- we strip this's type off here,
+    // fortunately, we've all ready looked at the root object's enclosing
+    // class, so everything works out OK w.r.t. detecting fields in the
+    // same module as us.
+    return new ConfigurationModule(this) {
+    };
+  }
+
+  private ConfigurationModule(ConfigurationModule c) {
+    try {
+      b.addConfiguration(c.b.build());
+    } catch (BindException e) {
+      throw new RuntimeException(e);
+    }
+    reqUse.addAll(c.reqUse);
+    optUse.addAll(c.optUse);
+    reqSet.addAll(c.reqSet);
+    map.putAll(c.map);
+    freeImpls.putAll(c.freeImpls);
+    freeParams.putAll(c.freeParams);
+    setImpls.putAll(c.setImpls);
+    setParams.putAll(c.setParams);
+  }
+
   public ConfigurationModule() {
-    for (Field f : getClass().getDeclaredFields()) {
+    // TODO: Can we simply look at the stack to see what class invoked us?
+    // That would avoid the crazy {}'s.
+    if (getClass().getEnclosingClass() == null) {
+      throw new ClassHierarchyException(
+          "ConfigurationModules must be inner classes.  "
+              + "In other words, put a '{}' between 'new ConfigurationModule()' "
+              + "and the first '.bind...(...)'");
+    }
+    for (Field f : getClass().getEnclosingClass().getDeclaredFields()) {
       Class<?> t = f.getType();
       if (paramTypes.contains(t)) {
         if (!Modifier.isPublic(f.getModifiers())) {
@@ -106,143 +142,182 @@ public class ConfigurationModule {
     }
   }
 
-  @SuppressWarnings({ "unchecked", "rawtypes" })
-  public Configuration build() throws BindException {
-    if(!(reqUse.isEmpty() && optUse.isEmpty())) {
-      throw new ClassHierarchyException("Found declared options that were not used in binds: " + reqUse + " " + optUse);
+  private final String toString(Set<Field> s) {
+    StringBuilder sb = new StringBuilder("{");
+    boolean first = true;
+    for (Field f : s) {
+      sb.append((first ? " " : ", ") + f.getName());
+      first = false;
     }
-    if(!reqSet.isEmpty()) {
-      throw new BindException("Attempt to build configuration before setting required option(s): " + reqSet);
-    }
-    
-    for(Impl<?> i : setImpls.keySet()) {
-      b.bind(freeImpls.get(i), setImpls.get(i));
-    }
-    for(Param<?> p : setParams.keySet()) {
-      b.bindNamedParameter((Class)freeParams.get(p), setParams.get(p));
-    }
-    return b.build();
+    sb.append(" }");
+    return sb.toString();
   }
 
-  public <T> ConfigurationModule bind(Class<T> iface, Class<?> impl) {
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  public final Configuration build() throws BindException {
+    ConfigurationModule c = deepCopy();
+    if (!(c.reqUse.isEmpty() && c.optUse.isEmpty())) {
+      throw new ClassHierarchyException(
+          "Found declared options that were not used in binds: "
+              + toString(c.reqUse) + " " + toString(c.optUse));
+    }
+    if (!c.reqSet.isEmpty()) {
+      throw new BindException(
+          "Attempt to build configuration before setting required option(s): "
+              + toString(c.reqSet));
+    }
+
+    for (Impl<?> i : c.setImpls.keySet()) {
+      c.b.bind(c.freeImpls.get(i), c.setImpls.get(i));
+    }
+    for (Param<?> p : c.setParams.keySet()) {
+      c.b.bindNamedParameter((Class) c.freeParams.get(p), c.setParams.get(p));
+    }
+    return c.b.build();
+  }
+
+  public final <T> ConfigurationModule bind(Class<T> iface, Class<?> impl) {
+    ConfigurationModule c = deepCopy();
     try {
-      b.bind(iface, impl);
+      c.b.bind(iface, impl);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
+    return c;
   }
-  private <T> void processUse(Object impl) {
+
+  private final <T> void processUse(Object impl) {
     Field f = map.get(impl);
-    if(f == null) { /* throw */ }
+    if (f == null) { /* throw */
+    }
     reqUse.remove(f);
     optUse.remove(f);
   }
-  private <T> void processSet(Object impl) {
+
+  private final <T> void processSet(Object impl) {
     Field f = map.get(impl);
-    if(f == null) { /* throw */ }
+    if (f == null) { /* throw */
+    }
     reqSet.remove(f);
   }
-  public <T> ConfigurationModule set(Impl<T> opt, Class<? extends T> impl) {
-    processSet(opt);
-    setImpls.put(opt, impl);
-    return this;
-  }
-  public <T> ConfigurationModule set(Param<T> opt, String val) {
-    processSet(opt);
-    setParams.put(opt, val);
-    return this;
-  }
-  
-  public <T> ConfigurationModule bind(Class<?> iface, Impl<?> opt) {
-    processUse(opt);
-    freeImpls.put(opt, iface);
-    return this;
+
+  public final <T> ConfigurationModule set(Impl<T> opt, Class<? extends T> impl) {
+    ConfigurationModule c = deepCopy();
+    c.processSet(opt);
+    c.setImpls.put(opt, impl);
+    return c;
   }
 
-  public <T> ConfigurationModule bindImplementation(Class<T> iface,
+  public final <T> ConfigurationModule set(Param<T> opt, String val) {
+    ConfigurationModule c = deepCopy();
+    c.processSet(opt);
+    c.setParams.put(opt, val);
+    return c;
+  }
+
+  public final <T> ConfigurationModule bind(Class<?> iface, Impl<?> opt) {
+    ConfigurationModule c = deepCopy();
+    c.processUse(opt);
+    c.freeImpls.put(opt, iface);
+    return c;
+  }
+
+  public final <T> ConfigurationModule bindImplementation(Class<T> iface,
       Class<? extends T> impl) {
+    ConfigurationModule c = deepCopy();
     try {
-      b.bindImplementation(iface, impl);
+      c.b.bindImplementation(iface, impl);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
+    return c;
   }
 
-  public <T> ConfigurationModule bindImplementation(Class<T> iface, Impl<? extends T> opt) {
-    processUse(opt);
-    freeImpls.put(opt, iface);
-    return this;
+  public final <T> ConfigurationModule bindImplementation(Class<T> iface,
+      Impl<? extends T> opt) {
+    ConfigurationModule c = deepCopy();
+    c.processUse(opt);
+    c.freeImpls.put(opt, iface);
+    return c;
   }
 
-  public <T> ConfigurationModule bindSingletonImplementation(Class<T> iface,
-      Class<? extends T> impl) throws BindException {
+  public final <T> ConfigurationModule bindSingletonImplementation(
+      Class<T> iface, Class<? extends T> impl) throws BindException {
+    ConfigurationModule c = deepCopy();
     try {
-      b.bindSingletonImplementation(iface, impl);
+      c.b.bindSingletonImplementation(iface, impl);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
+    return c;
   }
 
-  public <T> ConfigurationModule bindSingletonImplementation(Class<T> iface,
-      Impl<? extends T> opt) throws BindException {
-    processUse(opt);
+  public final <T> ConfigurationModule bindSingletonImplementation(
+      Class<T> iface, Impl<? extends T> opt) throws BindException {
+    ConfigurationModule c = deepCopy();
+    c.processUse(opt);
     try {
-      b.bindSingleton(iface);
-      freeImpls.put(opt, iface);
+      c.b.bindSingleton(iface);
+      c.freeImpls.put(opt, iface);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
+    return c;
   }
 
-  public <T> ConfigurationModule bindSingleton(Class<T> iface) {
+  public final <T> ConfigurationModule bindSingleton(Class<T> iface) {
+    ConfigurationModule c = deepCopy();
     try {
-      b.bindSingleton(iface);
+      c.b.bindSingleton(iface);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
+    return c;
   }
 
-  public <T> ConfigurationModule bindNamedParameter(
+  public final <T> ConfigurationModule bindNamedParameter(
       Class<? extends Name<T>> name, String value) {
+    ConfigurationModule c = deepCopy();
     try {
-      b.bindNamedParameter(name, value);
+      c.b.bindNamedParameter(name, value);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
+    return c;
   }
-  public <T> ConfigurationModule bindNamedParameter(
+
+  public final <T> ConfigurationModule bindNamedParameter(
       Class<? extends Name<T>> name, Param<T> opt) {
-    processUse(opt);
-    freeParams.put(opt, name);
-    return this;
+    ConfigurationModule c = deepCopy();
+    c.processUse(opt);
+    c.freeParams.put(opt, name);
+    return c;
   }
 
-  public <T> ConfigurationModule bindNamedParameter(
+  public final <T> ConfigurationModule bindNamedParameter(
       Class<? extends Name<T>> iface, Class<? extends T> impl) {
+    ConfigurationModule c = deepCopy();
     try {
-      b.bindNamedParameter(iface, impl);
+      c.b.bindNamedParameter(iface, impl);
     } catch (BindException e) {
       throw new RuntimeException(e);
     }
-    return this;
-  }
-  public <T> ConfigurationModule bindNamedParameter(
-      Class<? extends Name<T>> iface, Impl<? extends T> opt) {
-    processUse(opt);
-    freeImpls.put(opt, iface);
-    return this;
+    return c;
   }
 
-  public <T> ConfigurationModule bindConstructor(Class<T> c,
+  public final <T> ConfigurationModule bindNamedParameter(
+      Class<? extends Name<T>> iface, Impl<? extends T> opt) {
+    ConfigurationModule c = deepCopy();
+    c.processUse(opt);
+    c.freeImpls.put(opt, iface);
+    return c;
+  }
+
+  public final <T> ConfigurationModule bindConstructor(Class<T> cons,
       Impl<? extends ExternalConstructor<? extends T>> v) {
-    processUse(v);
-    freeImpls.put(v, c);
-    return this;
+    ConfigurationModule c = deepCopy();
+    c.processUse(v);
+    c.freeImpls.put(v, cons);
+    return c;
   }
 }
