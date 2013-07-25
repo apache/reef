a4202356586664037b94f4a94eeeeb224e56a87f
Rusty Sears <sears@microsoft.com>
2013-07-24 17:07:21 -0700

    Add ConfigurationModuleBuilder

---
+++ tang/src/main/java/com/microsoft/tang/formats/ConfigurationModuleBuilder.java
@@ -0,0 +1,322 @@
+package com.microsoft.tang.formats;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+import java.util.Set;
+
+import com.microsoft.tang.ExternalConstructor;
+import com.microsoft.tang.JavaConfigurationBuilder;
+import com.microsoft.tang.Tang;
+import com.microsoft.tang.annotations.Name;
+import com.microsoft.tang.exceptions.BindException;
+import com.microsoft.tang.exceptions.ClassHierarchyException;
+import com.microsoft.tang.exceptions.NameResolutionException;
+import com.microsoft.tang.util.MonotonicHashMap;
+import com.microsoft.tang.util.MonotonicHashSet;
+import com.microsoft.tang.util.ReflectionUtilities;
+
+public abstract class ConfigurationModuleBuilder {
+
+  public interface Impl<T> {
+  }
+
+  public interface Param<T> {
+  }
+
+  public static final class Provides<T> { }
+
+  public static final class RequiredImpl<T> implements Impl<T> {
+  }
+
+  public static final class OptionalImpl<T> implements Impl<T> {
+  }
+
+  public static final class RequiredParameter<T> implements Param<T> {
+  }
+
+  public static final class OptionalParameter<T> implements Param<T> {
+  }
+
+  private final static Set<Class<?>> paramTypes = new MonotonicHashSet<Class<?>>(
+      RequiredImpl.class, OptionalImpl.class, RequiredParameter.class,
+      OptionalParameter.class);
+  final JavaConfigurationBuilder b = Tang.Factory.getTang()
+  .newConfigurationBuilder();
+  // Sets of things that have been declared
+  final Set<Field> reqDecl = new MonotonicHashSet<>();
+  private final Set<Field> optDecl = new MonotonicHashSet<>();
+  // Set of things that have been used in a bind. These must be equal
+  // to the decl counterparts before build() is called.
+  private final Set<Field> reqUsed = new MonotonicHashSet<>();
+  private final Set<Field> optUsed = new MonotonicHashSet<>();
+  // Maps from field instance variables to the fields that they
+  // are assigned to. These better be unique!
+  final Map<Object, Field> map = new MonotonicHashMap<>();
+  final Map<Class<?>, Impl<?>> freeImpls = new MonotonicHashMap<>();
+  final Map<Class<? extends Name<?>>,Param<?>> freeParams = new MonotonicHashMap<>();
+  final Set<Impl<?>> freeSingletons = new MonotonicHashSet<>();
+  private final Map<Class<?>, String> lateBindClazz = new MonotonicHashMap<>();
+
+  protected ConfigurationModuleBuilder() {
+    for (Field f : getClass().getDeclaredFields()) {
+      Class<?> t = f.getType();
+      if (paramTypes.contains(t)) {
+        if (!Modifier.isPublic(f.getModifiers())) {
+          throw new ClassHierarchyException(
+              "Found a non-public configuration option in " + getClass() + ": "
+                  + f);
+        }
+        if (!Modifier.isStatic(f.getModifiers())) {
+          throw new ClassHierarchyException(
+              "Found a non-static configuration option in " + getClass() + ": "
+                  + f);
+        }
+        if (!Modifier.isFinal(f.getModifiers())) {
+          throw new ClassHierarchyException(
+              "Found a non-final configuration option in " + getClass() + ": "
+                  + f);
+        }
+        final Object o;
+        try {
+          o = f.get(null);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+          throw new ClassHierarchyException(
+              "Could not look up field instance in " + getClass() + " field: "
+                  + f);
+        }
+        if (map.containsKey(o)) {
+          throw new ClassHierarchyException(
+              "Detected aliased instances in class " + getClass()
+                  + " for fields " + map.get(o) + " and " + f);
+        }
+        if (t == RequiredImpl.class || t == RequiredParameter.class) {
+          reqDecl.add(f);
+        } else {
+          optDecl.add(f);
+        }
+        map.put(o, f);
+      }
+    }
+  }
+
+  private ConfigurationModuleBuilder(ConfigurationModuleBuilder c) {
+    try {
+      b.addConfiguration(c.b.build());
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    reqDecl.addAll(c.reqDecl);
+    optDecl.addAll(c.optDecl);
+    reqUsed.addAll(c.reqUsed);
+    optUsed.addAll(c.optUsed);
+    map.putAll(c.map);
+    freeImpls.putAll(c.freeImpls);
+    freeParams.putAll(c.freeParams);
+    freeSingletons.addAll(c.freeSingletons);
+    lateBindClazz.putAll(c.lateBindClazz);
+    
+  }
+  public final <T> ConfigurationModuleBuilder bind(Class<?> iface, Impl<?> opt) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(opt);
+    c.freeImpls.put(iface, opt);
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindImplementation(Class<T> iface,
+      Class<? extends T> impl) {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bindImplementation(iface, impl);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindImplementation(Class<T> iface,
+      String impl) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.lateBindClazz.put(iface, impl);
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindImplementation(Class<T> iface,
+      Impl<? extends T> opt) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(opt);
+    c.freeImpls.put(iface, opt);
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindSingletonImplementation(
+      Class<T> iface, Class<? extends T> impl) throws BindException {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bindSingletonImplementation(iface, impl);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindSingletonImplementation(
+      Class<T> iface, Impl<? extends T> opt) throws BindException {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(opt);
+    try {
+      c.b.bindSingleton(iface);
+      c.freeImpls.put(iface, opt);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindSingleton(Class<T> iface) {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bindSingleton(iface);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindSingleton(Impl<T> iface) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(iface);
+    c.freeSingletons.add(iface);
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindNamedParameter(
+      Class<? extends Name<T>> name, String value) {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bindNamedParameter(name, value);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindNamedParameter(
+      Class<? extends Name<T>> name, Param<T> opt) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(opt);
+    c.freeParams.put(name, opt);
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindNamedParameter(
+      Class<? extends Name<T>> iface, Class<? extends T> impl) {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bindNamedParameter(iface, impl);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindNamedParameter(
+      Class<? extends Name<T>> iface, Impl<? extends T> opt) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(opt);
+    c.freeImpls.put(iface, opt);
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindConstructor(Class<T> clazz,
+      Class<? extends ExternalConstructor<? extends T>> constructor) {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bindConstructor(clazz, constructor);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  public final <T> ConfigurationModuleBuilder bindConstructor(Class<T> cons,
+      Impl<? extends ExternalConstructor<? extends T>> v) {
+    ConfigurationModuleBuilder c = deepCopy();
+    c.processUse(v);
+    c.freeImpls.put(cons, v);
+    return c;
+  }
+
+  private final <T> void processUse(Object impl) {
+    Field f = map.get(impl);
+    if (f == null) { /* throw */
+      if (f == null) { /* throw */
+        throw new ClassHierarchyException("Unknown Impl/Param when binding " + ReflectionUtilities.getSimpleName(impl.getClass()) + ".  Did you pass in a field from some other module?");
+      }
+    }
+    if(!reqUsed.contains(f)) { reqUsed.add(f); }
+    if(!optUsed.contains(f)) { optUsed.add(f); }
+  }
+
+  public final ConfigurationModule build() throws ClassHierarchyException {
+    ConfigurationModuleBuilder c = deepCopy();
+    
+    if (!(c.reqUsed.containsAll(c.reqDecl) && c.optUsed.containsAll(c.optDecl))) {
+      Set<Field> fset = new MonotonicHashSet<>();
+      for (Field f : c.reqDecl) {
+        if (!c.reqUsed.contains(f)) {
+          fset.add(f);
+        }
+      }
+      for (Field f : c.optDecl) {
+        if (!c.optUsed.contains(f)) {
+          fset.add(f);
+        }
+      }
+      throw new ClassHierarchyException(
+          "Found declared options that were not used in binds: "
+              + toString(fset));
+    }
+    for (Class<?> clz: c.lateBindClazz.keySet()) {
+      try {
+        c.b.bind(ReflectionUtilities.getFullName(clz), c.lateBindClazz.get(clz));
+      } catch (NameResolutionException e) {
+        throw new ClassHierarchyException("ConfigurationModule refers to unknown class: " + c.lateBindClazz.get(clz), e);
+      } catch (BindException e) {
+        throw new ClassHierarchyException("bind failed while initializing ConfigurationModuleBuilder", e);
+      }
+    }
+    return new ConfigurationModule(c);
+  }
+
+  public final <T> ConfigurationModuleBuilder bind(Class<T> iface, Class<?> impl) {
+    ConfigurationModuleBuilder c = deepCopy();
+    try {
+      c.b.bind(iface, impl);
+    } catch (BindException e) {
+      throw new ClassHierarchyException(e);
+    }
+    return c;
+  }
+
+  final ConfigurationModuleBuilder deepCopy() {
+    // ooh... this is a dirty trick --- we strip this's type off here,
+    // fortunately, we've all ready looked at the root object's class's
+    // fields, and we copy the information we extracted from them, so
+    // everything works out OK w.r.t. field detection.
+    return new ConfigurationModuleBuilder(this) {
+    };
+  }
+  final String toString(Set<Field> s) {
+    StringBuilder sb = new StringBuilder("{");
+    boolean first = true;
+    for (Field f : s) {
+      sb.append((first ? " " : ", ") + f.getName());
+      first = false;
+    }
+    sb.append(" }");
+    return sb.toString();
+  }
+
+}
--- tang/src/test/java/com/microsoft/tang/formats/TestConfigurationModule.java
+++ tang/src/test/java/com/microsoft/tang/formats/TestConfigurationModule.java
@@ -18,6 +18,76 @@ import com.microsoft.tang.exceptions.BindException;
 import com.microsoft.tang.exceptions.ClassHierarchyException;
 import com.microsoft.tang.exceptions.InjectionException;
 
+/*
+ * Define a configuration module that explains how Foo should be injected.
+ * 
+ * A configuration module is like a configuration builder, except that it
+ * is not language independent (it should be defined in the same jar / whatever 
+ * as the stuff it configures, and it has a concept of variables that can be
+ * required or optional.
+ * 
+ * If you call build() without setting the required variables (or if the
+ * configuration declares variables that it does not use), then it blows up
+ * in your face.
+ * 
+ * Note that MyConfigurationModule does not actually subclass
+ * ConfigurationModule.  Instead, it has a static final field that contains a
+ * configuration module, and some other ones that define the parameters, and
+ * their types.
+ * 
+ * There are some *ahem* non-idiomatic java things going on here.
+ * 
+ * Sorry about that; if you can find my java programming license, you can take it
+ * away. :)
+ *
+ * First, you need the " = new RequiredImpl<>()" after each parameter.  This is
+ * because you need to pass something into set (other than null).  References to
+ * live objects happen to be unique, so that works.
+ * 
+ * Second, ConfigurationModule() is abstract, and all of its methods are defined
+ * as final.  To instantiate it, you need to put the {}'s between the () and the
+ * .bind stuff.  This is so I can call getClass().getEnclosingClass() in its
+ * constructor, and discover all those juicy configuration parameters that
+ * were assigned above it.  On the bright side, if you forget the {}'s you get
+ * a compiler error.  It used to be that you'd get a cryptic NPE from the
+ * classloader.  Also, note that adding methods to ConfigurationModule() won't
+ * work.  The bind calls implement immutability by using a secret final clone
+ * method called deepCopy() that strips your subclass off, and uses an anonomyous
+ * inner class instead.
+ * 
+ * 
+ */
+
+final class MyBadConfigurationModule extends ConfigurationModuleBuilder {    
+
+}
+
+final class MyConfigurationModule extends ConfigurationModuleBuilder {    
+  // Tell us what implementation you want, or else!!    
+  public static final RequiredImpl<TestConfigurationModule.Foo> THE_FOO = new RequiredImpl<>();
+  // If you want, you can change the fooness.
+  public static final OptionalParameter<Integer> FOO_NESS = new OptionalParameter<>();
+  
+  public static final ConfigurationModule CONF = new MyConfigurationModule()
+
+    // This binds the above to tang configuration stuff.  You can use parameters more than
+    // once, but you'd better use them all at least once, or I'll throw exceptions at you.
+
+    .bindImplementation(TestConfigurationModule.Foo.class, MyConfigurationModule.THE_FOO)
+    .bindNamedParameter(TestConfigurationModule.Fooness.class, MyConfigurationModule.FOO_NESS)
+    .build();
+}
+final class MyMissingBindConfigurationModule extends ConfigurationModuleBuilder {    
+  // Tell us what implementation you want, or else!!    
+  public static final RequiredImpl<TestConfigurationModule.Foo> THE_FOO = new RequiredImpl<>();
+  // If you want, you can change the fooness.
+  public static final OptionalParameter<Integer> FOO_NESS = new OptionalParameter<>();
+  
+  // This conf doesn't use FOO_NESS.  Expect trouble below
+  public static final ConfigurationModule BAD_CONF = new MyMissingBindConfigurationModule()
+    .bindImplementation(TestConfigurationModule.Foo.class, THE_FOO)
+    .build();
+}
 public class TestConfigurationModule {
   /*
    *  Toy class hierarchy: FooImpl implements Foo, has a Fooness named
@@ -39,63 +109,6 @@ public class TestConfigurationModule {
     }
   }
 
-  /*
-   * Define a configuration module that explains how Foo should be injected.
-   * 
-   * A configuration module is like a configuration builder, except that it
-   * is not language independent (it should be defined in the same jar / whatever 
-   * as the stuff it configures, and it has a concept of variables that can be
-   * required or optional.
-   * 
-   * If you call build() without setting the required variables (or if the
-   * configuration declares variables that it does not use), then it blows up
-   * in your face.
-   * 
-   * Note that MyConfigurationModule does not actually subclass
-   * ConfigurationModule.  Instead, it has a static final field that contains a
-   * configuration module, and some other ones that define the parameters, and
-   * their types.
-   * 
-   * There are some *ahem* non-idiomatic java things going on here.
-   * 
-   * Sorry about that; if you can find my java programming license, you can take it
-   * away. :)
-   *
-   * First, you need the " = new RequiredImpl<>()" after each parameter.  This is
-   * because you need to pass something into set (other than null).  References to
-   * live objects happen to be unique, so that works.
-   * 
-   * Second, ConfigurationModule() is abstract, and all of its methods are defined
-   * as final.  To instantiate it, you need to put the {}'s between the () and the
-   * .bind stuff.  This is so I can call getClass().getEnclosingClass() in its
-   * constructor, and discover all those juicy configuration parameters that
-   * were assigned above it.  On the bright side, if you forget the {}'s you get
-   * a compiler error.  It used to be that you'd get a cryptic NPE from the
-   * classloader.  Also, note that adding methods to ConfigurationModule() won't
-   * work.  The bind calls implement immutability by using a secret final clone
-   * method called deepCopy() that strips your subclass off, and uses an anonomyous
-   * inner class instead.
-   * 
-   * 
-   */
-  public static final class MyConfigurationModule extends ConfigurationModule {    
-    // Tell us what implementation you want, or else!!    
-    public static final RequiredImpl<Foo> THE_FOO = new RequiredImpl<>();
-    // If you want, you can change the fooness.
-    public static final OptionalParameter<Integer> FOO_NESS = new OptionalParameter<>();
-    
-    public static final ConfigurationModule CONF = new MyConfigurationModule()
-
-      // This binds the above to tang configuration stuff.  You can use parameters more than
-      // once, but you'd better use them all at least once, or I'll throw exceptions at you.
-
-      .bindImplementation(Foo.class, THE_FOO)
-      .bindNamedParameter(Fooness.class, FOO_NESS);
-    
-    // This conf doesn't use FOO_NESS.  Expect trouble below
-    public static final ConfigurationModule BAD_CONF = new MyConfigurationModule()
-      .bindImplementation(Foo.class, THE_FOO);
-  }
   @Rule public ExpectedException thrown = ExpectedException.none();
  
   @Test
@@ -120,23 +133,29 @@ public class TestConfigurationModule {
     Assert.assertEquals(f.getFooness(), 42);
   }
   @Test
-  public void omitRequiredTest() throws BindException, InjectionException {
+  public void omitRequiredTest() throws Throwable {
     thrown.expect(BindException.class);
     thrown.expectMessage("Attempt to build configuration before setting required option(s): { THE_FOO }");
-    MyConfigurationModule.CONF
+    try {
+      MyConfigurationModule.CONF
         .set(MyConfigurationModule.FOO_NESS, ""+12)
         .build();
+    } catch (ExceptionInInitializerError e) {
+      throw e.getCause();
+    }
   }
-  public static final class MyBadConfigurationModule extends ConfigurationModule {    
-
-  }
-    @Test
-  public void badConfTest() throws BindException {
+  @Test
+  public void badConfTest() throws Throwable {
     thrown.expect(ClassHierarchyException.class);
     thrown.expectMessage("Found declared options that were not used in binds: { FOO_NESS }");
-    MyConfigurationModule.BAD_CONF
-        .set(MyConfigurationModule.THE_FOO, Foo.class)
-        .build();
+      try {
+        // Java's classloader semantics cause it to load a class when executing the
+        // first line that references the class in question.
+        @SuppressWarnings("unused")
+        Object o = MyMissingBindConfigurationModule.BAD_CONF;
+      } catch (ExceptionInInitializerError e) {
+        throw e.getCause();
+      }
   }
   @Test
   public void nonExistentStringBindOK() throws BindException, InjectionException {
@@ -150,7 +169,7 @@ public class TestConfigurationModule {
     new MyBadConfigurationModule().bindImplementation(Foo.class, "i.do.not.exist").build();
   }
 
-  public static final class MultiBindConfigurationModule extends ConfigurationModule {    
+  public static final class MultiBindConfigurationModule extends ConfigurationModuleBuilder {    
     // Tell us what implementation you want, or else!!    
     public static final RequiredImpl<Foo> THE_FOO = new RequiredImpl<>();
     // If you want, you can change the fooness.
@@ -163,7 +182,8 @@ public class TestConfigurationModule {
 
       .bindImplementation(Foo.class, THE_FOO)
       .bindImplementation(Object.class, THE_FOO)
-      .bindNamedParameter(Fooness.class, FOO_NESS);
+      .bindNamedParameter(Fooness.class, FOO_NESS)
+      .build();
     
   }
   @Test
@@ -182,18 +202,26 @@ public class TestConfigurationModule {
     Assert.assertFalse(f == g);
   }
   @Test
-  public void foreignSetTest() throws BindException, InjectionException {
+  public void foreignSetTest() throws Throwable {
     thrown.expect(ClassHierarchyException.class);
-    thrown.expectMessage("Unknown Impl/Param when setting com.microsoft.tang.formats.ConfigurationModule$RequiredImpl Did you pass in a field from some other module?");
-    // Pass in something from the wrong module, watch it fail.
-    MultiBindConfigurationModule.CONF.set(MyConfigurationModule.THE_FOO, FooImpl.class);
+    thrown.expectMessage("Unknown Impl/Param when setting RequiredImpl.  Did you pass in a field from some other module?");
+    try {
+      // Pass in something from the wrong module, watch it fail.
+      MultiBindConfigurationModule.CONF.set(MyConfigurationModule.THE_FOO, FooImpl.class);
+    } catch (ExceptionInInitializerError e) {
+      throw e.getCause();
+    }
   }
   @Test
-  public void foreignBindTest() throws BindException, InjectionException {
+  public void foreignBindTest() throws Throwable {
     thrown.expect(ClassHierarchyException.class);
-    thrown.expectMessage("Unknown Impl/Param when binding com.microsoft.tang.formats.ConfigurationModule$RequiredImpl Did you pass in a field from some other module?");
-    // Pass in something from the wrong module, watch it fail.
-    new MyConfigurationModule().bindImplementation(Object.class, MultiBindConfigurationModule.THE_FOO);
+    thrown.expectMessage("Unknown Impl/Param when binding RequiredImpl.  Did you pass in a field from some other module?");
+    try {
+      // Pass in something from the wrong module, watch it fail.
+      new MyConfigurationModule().bindImplementation(Object.class, MultiBindConfigurationModule.THE_FOO);
+    } catch (ExceptionInInitializerError e) {
+      throw e.getCause();
+    }
   }
   @Test
   public void singletonTest() throws BindException, InjectionException {
@@ -201,12 +229,11 @@ public class TestConfigurationModule {
       .bindImplementation(Foo.class, MyConfigurationModule.THE_FOO)
       .bindSingleton(MyConfigurationModule.THE_FOO)
       .bindNamedParameter(Fooness.class, MyConfigurationModule.FOO_NESS)
+      .build()
       .set(MyConfigurationModule.THE_FOO, FooImpl.class)
       .build();
     Injector i = Tang.Factory.getTang().newInjector(c);
     Assert.assertTrue(i.getInstance(Foo.class) == i.getInstance(Foo.class));
   }
-
-
 }
 
