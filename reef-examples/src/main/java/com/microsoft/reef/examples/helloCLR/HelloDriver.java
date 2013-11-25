a2a36d327bb8cc0debe82357e0676e9c01ee75c8
Markus Weimer <mweimer@microsoft.com>
2013-11-25 15:28:04 -0800

    Updated HelloCLR to create the config

--- reef-examples/src/main/java/com/microsoft/reef/examples/helloCLR/HelloDriver.java
+++ reef-examples/src/main/java/com/microsoft/reef/examples/helloCLR/HelloDriver.java
@@ -15,18 +15,25 @@
  */
 package com.microsoft.reef.examples.helloCLR;
 
-import com.microsoft.reef.driver.activity.ActivityConfiguration;
 import com.microsoft.reef.driver.context.ContextConfiguration;
 import com.microsoft.reef.driver.evaluator.AllocatedEvaluator;
 import com.microsoft.reef.driver.evaluator.EvaluatorRequest;
 import com.microsoft.reef.driver.evaluator.EvaluatorRequestor;
+import com.microsoft.tang.ClassHierarchy;
 import com.microsoft.tang.Configuration;
+import com.microsoft.tang.ConfigurationBuilder;
+import com.microsoft.tang.Tang;
 import com.microsoft.tang.annotations.Unit;
 import com.microsoft.tang.exceptions.BindException;
+import com.microsoft.tang.implementation.protobuf.ProtocolBufferClassHierarchy;
+import com.microsoft.tang.proto.ClassHierarchyProto;
 import com.microsoft.wake.EventHandler;
 import com.microsoft.wake.time.event.StartTime;
 
 import javax.inject.Inject;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -73,14 +80,31 @@ public final class HelloDriver {
         final Configuration contextConfiguration = ContextConfiguration.CONF
             .set(ContextConfiguration.IDENTIFIER, "HelloREEFContext")
             .build();
-        final Configuration activityConfiguration = ActivityConfiguration.CONF
-            .set(ActivityConfiguration.IDENTIFIER, "HelloREEFActivity")
-            .set(ActivityConfiguration.ACTIVITY, HelloActivity.class)
-            .build();
-        allocatedEvaluator.submitContextAndActivity(contextConfiguration, activityConfiguration);
+
+        final ConfigurationBuilder activityConfigurationBuilder = Tang.Factory.getTang()
+            .newConfigurationBuilder(loadClassHierarchy());
+        activityConfigurationBuilder.bind("com.microsoft.reef.driver.activity.ActivityConfigurationOptions.Identifier.class", "Hello_From_CLR");
+        activityConfigurationBuilder.bind("com.microsoft.reef.activity.IActivity", "com.microsoft.reef.activity.HelloActivity");
+        allocatedEvaluator.submitContextAndActivity(contextConfiguration, activityConfigurationBuilder.build());
       } catch (final BindException ex) {
-        throw new RuntimeException("Unable to setup Activity or Context configuration.", ex);
+        final String message = "Unable to setup Activity or Context configuration.";
+        LOG.log(Level.SEVERE, message, ex);
+        throw new RuntimeException(message, ex);
       }
     }
   }
+
+
+  private static ClassHierarchy loadClassHierarchy() {
+    try (final InputStream chin = new FileInputStream(HelloREEF.CLASS_HIERARCHY_FILENAME)) {
+      final ClassHierarchyProto.Node root = ClassHierarchyProto.Node.parseFrom(chin); // A
+      final ClassHierarchy ch = new ProtocolBufferClassHierarchy(root);
+      return ch;
+    } catch (final IOException e) {
+      final String message = "Unable to load class hierarchy.";
+      LOG.log(Level.SEVERE, message, e);
+      throw new RuntimeException(message, e);
+    }
+  }
 }
+
--- reef-examples/src/main/java/com/microsoft/reef/examples/helloCLR/HelloREEF.java
+++ reef-examples/src/main/java/com/microsoft/reef/examples/helloCLR/HelloREEF.java
@@ -23,7 +23,9 @@ import com.microsoft.reef.util.EnvironmentUtils;
 import com.microsoft.tang.Configuration;
 import com.microsoft.tang.exceptions.BindException;
 import com.microsoft.tang.exceptions.InjectionException;
+import com.microsoft.tang.formats.ConfigurationModule;
 
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,6 +34,9 @@ import java.util.logging.Logger;
  */
 public final class HelloREEF {
 
+
+  public static final String CLASS_HIERARCHY_FILENAME = "activity.bin";
+
   private static final Logger LOG = Logger.getLogger(HelloREEF.class.getName());
 
   /**
@@ -39,31 +44,46 @@ public final class HelloREEF {
    */
   private static final int JOB_TIMEOUT = 10000; // 10 sec.
 
-  public static LauncherStatus runHelloReef(final Configuration runtimeConf, final int timeOut)
+  public static LauncherStatus runHelloReef(final Configuration runtimeConf, final int timeOut, final String[] args)
       throws BindException, InjectionException {
 
-    final Configuration driverConf =
-        EnvironmentUtils.addClasspath(DriverConfiguration.CONF, DriverConfiguration.GLOBAL_LIBRARIES)
-          .set(DriverConfiguration.DRIVER_IDENTIFIER, "HelloREEF")
-          .set(DriverConfiguration.ON_DRIVER_STARTED, HelloDriver.StartHandler.class)
-          .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, HelloDriver.EvaluatorAllocatedHandler.class)
-        .build();
 
-    return DriverLauncher.getLauncher(runtimeConf).run(driverConf, timeOut);
+    ConfigurationModule driverConf =
+        EnvironmentUtils.addClasspath(DriverConfiguration.CONF, DriverConfiguration.GLOBAL_LIBRARIES)
+            .set(DriverConfiguration.DRIVER_IDENTIFIER, "HelloREEF")
+            .set(DriverConfiguration.ON_DRIVER_STARTED, HelloDriver.StartHandler.class)
+            .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, HelloDriver.EvaluatorAllocatedHandler.class);
+
+    for (final String fileName : args) {
+      final File f = new File(fileName);
+      if (!f.exists()) {
+        LOG.log(Level.FINEST, "Ignoring, as it doesn't exist: " + fileName);
+      } else if (f.isDirectory()) {
+        LOG.log(Level.FINEST, "Ignoring, as it is a folder: " + fileName);
+      } else if (!f.canRead()) {
+        LOG.log(Level.FINEST, "Ignoring, as it can't be read: " + fileName);
+      } else {
+        driverConf = driverConf.set(DriverConfiguration.GLOBAL_FILES, fileName);
+      }
+    }
+
+    return DriverLauncher.getLauncher(runtimeConf).run(driverConf.build(), timeOut);
   }
 
   /**
    * Start Hello REEF job. Runs method runHelloReef().
    *
    * @param args command line parameters.
-   * @throws com.microsoft.tang.exceptions.BindException      configuration error.
-   * @throws com.microsoft.tang.exceptions.InjectionException configuration error.
+   * @throws com.microsoft.tang.exceptions.BindException
+   *          configuration error.
+   * @throws com.microsoft.tang.exceptions.InjectionException
+   *          configuration error.
    */
   public static void main(final String[] args) throws BindException, InjectionException {
     final Configuration runtimeConfiguration = LocalRuntimeConfiguration.CONF
         .set(LocalRuntimeConfiguration.NUMBER_OF_THREADS, 2)
         .build();
-    final LauncherStatus status = runHelloReef(runtimeConfiguration, JOB_TIMEOUT);
+    final LauncherStatus status = runHelloReef(runtimeConfiguration, JOB_TIMEOUT, args);
     LOG.log(Level.INFO, "REEF job completed: {0}", status);
   }
 }
