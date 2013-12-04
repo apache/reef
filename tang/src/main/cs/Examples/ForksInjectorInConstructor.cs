299e429b256660c1f6c7f8eddf2a4e38f3b9c3fd
Julia Wang <jwang98052@yahoo.com>
2013-12-03 18:31:39 -0800

    Adding ForkInjection tests

---
+++ tang/src/main/cs/Examples/ForksInjectorInConstructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Microsoft.Tang.Annotations;
+using Com.Microsoft.Tang.Implementations;
+using Com.Microsoft.Tang.Interface;
+
+namespace Com.Microsoft.Tang.Examples
+{
+    public class ForksInjectorInConstructor
+    {
+        [Inject]
+        public ForksInjectorInConstructor(IInjector i)
+        {
+            ICsConfigurationBuilder cb = TangFactory.GetTang().NewConfigurationBuilder(new string[] { @"Com.Microsoft.Tang.Examples" });
+            //cb.BindImplementation(Number.class, typeof(Int32));
+            i.ForkInjector(new IConfiguration[] { cb.Build() });
+        }
+    }
+}
