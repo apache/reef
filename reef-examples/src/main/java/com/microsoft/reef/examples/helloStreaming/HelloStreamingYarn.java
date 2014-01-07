147880723e0a196c323fe119fc5dd9810ac1077e
Julia Wang <jwang98052@yahoo.com>
2014-01-06 17:55:42 -0800

    Add HelloStreaming
    HelloStreaming, HelloStreamingYarn, and increase timeout

---
+++ reef-examples/src/main/java/com/microsoft/reef/examples/helloStreaming/HelloStreamingYarn.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2013 Microsoft Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microsoft.reef.examples.helloStreaming;
+
+import com.microsoft.reef.client.LauncherStatus;
+import com.microsoft.reef.client.REEF;
+import com.microsoft.reef.examples.hello.HelloREEF;
+import com.microsoft.reef.runtime.yarn.client.YarnClientConfiguration;
+import com.microsoft.reef.util.EnvironmentUtils;
+import com.microsoft.tang.Configuration;
+import com.microsoft.tang.exceptions.BindException;
+import com.microsoft.tang.exceptions.InjectionException;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The Client for Hello REEF example.
+ */
+public final class HelloStreamingYarn {
+
+  private static final Logger LOG = Logger.getLogger(HelloStreamingYarn.class.getName());
+
+  /**
+   * Number of milliseconds to wait for the job to complete.
+   */
+  private static final int JOB_TIMEOUT = 30000; // 30 sec.
+
+  /**
+   * Start Hello REEF job. Runs method runHelloReef().
+   *
+   * @param args command line parameters.
+   * @throws com.microsoft.tang.exceptions.BindException      configuration error.
+   * @throws com.microsoft.tang.exceptions.InjectionException configuration error.
+   */
+  public static void main(final String[] args) throws BindException, InjectionException, IOException {
+
+    final Configuration runtimeConfiguration = YarnClientConfiguration.CONF
+        .set(YarnClientConfiguration.REEF_JAR_FILE, EnvironmentUtils.getClassLocationFile(REEF.class))
+        .build();
+
+    final LauncherStatus status = HelloREEF.runHelloReef(runtimeConfiguration, JOB_TIMEOUT);
+    LOG.log(Level.INFO, "REEF job completed: {0}", status);
+  }
+}
