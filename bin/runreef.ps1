7cc0477b86aff1b96fce67697a22dda65ff40434
Markus Weimer <mweimer@microsoft.com>
2014-02-03 16:32:13 -0800

    Added a script to launch REEF on HDP Windows clusters

---
+++ bin/runreef.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+  # the main class to run
+  [string]$Class,
+  # the jar file with the project inside
+  [string]$Jar
+)
+
+# Sanitize the classpath returned by "yarn classpath"
+$yarn_classpath = (yarn classpath).split(";") | where {$_ -ne ""} | Unique
+$yarn_classpath += $Jar
+
+# Assemble the classpath for the job
+$java_classpath = $yarn_classpath -join ';'
+
+# Assemble the command to run
+# Note: We need to put the classpath within "", as it contains ";"
+$command = "$env:JAVA_HOME\bin\java.exe -cp `"$java_classpath`"  $Class"
+Invoke-Expression -Command $command
