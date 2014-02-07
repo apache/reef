cbad05a4990590a33ffc64aad4224a7c56ff8ed2
Markus Weimer <mweimer@microsoft.com>
2014-02-06 17:55:27 -0800

    Updated the launch script for Windows Server 2012

--- bin/runreef.ps1
+++ bin/runreef.ps1
@@ -1,36 +1,35 @@
-﻿﻿<#
+﻿<#
  .SYNOPSIS
  Runs a given main class from a given JAR file on a YARN cluster.
  .DESCRIPTION
  Runs a given main class from a given JAR file on a YARN cluster. This assumes 
- that $JAVA_HOME is set and that yarn is on the $PATH.
- #>
-
-<# 
-#  Copyright (C) 2014 Microsoft Corporation
-# 
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-# 
-#          http://www.apache.org/licenses/LICENSE-2.0
-# 
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-# limitations under the License.
+ that JAVA_HOME and HADOOP_HOME are set.
 #>
 
-[CmdletBinding()]
-  param
+<# 
+Copyright (C) 2014 Microsoft Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+param
   (
     # the jar file with the project inside
     [Parameter(Mandatory=$True, HelpMessage="The JAR file to launch.", Position=1)]
     [string]$Jar,
+
     # the main class to run
     [Parameter(Mandatory=$True, HelpMessage="The main class to launch.", Position=2)]
     [string]$Class,
+    
     [Parameter(Mandatory=$False, HelpMessage="Options to be passed to java")]
     [string]$JavaOptions,
 
@@ -45,13 +44,7 @@ function Get-YARN-Classpath{
  .DESCRIPTION
  A simple wrapper around "yarn classpath" that cleans up the classpath returned and formats it into a list.
  #>
-   # check whether yarn is in the PATH
-  if (!(Get-Command yarn -errorAction SilentlyContinue))
-  {
-   Write-Host "Error: yarn is not on the PATH."
-   exit 
-  }
- (yarn classpath).split(";") | where {$_ -ne ""} | Unique
+  (Invoke-Expression -Command "$env:HADOOP_HOME\bin\yarn.cmd classpath").split(";") | Where-Object {$_ -ne ""} | Unique
 }
 
 function Submit-YARN-Application{
@@ -62,17 +55,19 @@ function Submit-YARN-Application{
  Runs a given main class from a given JAR file on a YARN cluster. This assumes 
  that $JAVA_HOME is set and that yarn is on the $PATH.
  #>
-[CmdletBinding()]
   param
   (
     # the jar file with the project inside
     [Parameter(Mandatory=$True, HelpMessage="The JAR file to launch.", Position=1)]
     [string]$Jar,
+    
     # the main class to run
     [Parameter(Mandatory=$True, HelpMessage="The main class to launch.", Position=2)]
     [string]$Class,
+    
     [Parameter(Mandatory=$False, HelpMessage="Options to be passed to java")]
     [string]$JavaOptions,
+
     [Parameter(ValueFromRemainingArguments=$True)]
     $Arguments
   )
@@ -91,7 +86,7 @@ function Submit-YARN-Application{
 
   # Assemble the command to run
   # Note: We need to put the classpath within "", as it contains ";"
-  $command = "$env:JAVA_HOME\bin\java.exe $LOGGING $JavaOptions -cp `"$CLASSPATH`"  $Class  $Arguments"
+  $command = "$env:JAVA_HOME\bin\java.exe $LOGGING $JavaOptions -cp `"$CLASSPATH`" $Class $Arguments"
   Invoke-Expression -Command $command
 }
 
