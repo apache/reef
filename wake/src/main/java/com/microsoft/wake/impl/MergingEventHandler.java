4df0a0453d79c63e49c7492c03844a736cace6b3
Sergiy Matusevych <sergiy.matusevych@gmail>
2014-02-10 15:16:26 -0800

    Create exactly one instance of Left and Right handlers in MergingEventHandler and make classes private. UPdate unit tests; also, some minor refactoring and code cleanup.
    P.S. MergingEventHandler is currently used only in Distributed Hash PCA
    application which still uses old REEF API, so we'll have to change it
    anyways.

--- wake/src/main/java/com/microsoft/wake/impl/MergingEventHandler.java
+++ wake/src/main/java/com/microsoft/wake/impl/MergingEventHandler.java
@@ -26,9 +26,9 @@ import com.microsoft.wake.EventHandler;
 /**
  * An EventHandler combines two events of different types into a single Pair of events.
  * Handler will block until both events are received.
- * 
+ *
  * onNext is thread safe
- * 
+ *
  * @param <L> type of event
  * @param <R> type of event
  *
@@ -36,82 +36,99 @@ import com.microsoft.wake.EventHandler;
  */
 @Unit
 public final class MergingEventHandler<L,R> {
+
   private static Logger LOG = Logger.getLogger(MergingEventHandler.class.getName());
 
-  private final Object mutex = new Object() { };
-  
+  private final Object mutex = new Object();
+
   private final EventHandler<Pair<L,R>> destination;
+
   private L leftEvent;
   private R rightEvent;
 
+  public final EventHandler<L> leftHandler = new Left();
+  public final EventHandler<R> rightHandler = new Right();
+
   @Inject
-  public MergingEventHandler(final EventHandler<Pair<L,R>> destination) {
+  public MergingEventHandler(final EventHandler<Pair<L, R>> destination) {
     this.destination = destination;
     reset();
   }
-  
-  public class Left implements EventHandler<L> {
-    
+
+  private class Left implements EventHandler<L> {
+
     @Override
     public void onNext(final L event) {
+
       L leftRef = null;
       R rightRef = null;
-      synchronized(mutex) {
-        while (leftEvent!=null) {
+
+      synchronized (mutex) {
+
+        while (leftEvent != null) {
           try {
             mutex.wait();
-          } catch (InterruptedException e) {
-            LOG.log(Level.SEVERE, e.getMessage());
-            e.printStackTrace();
+          } catch (final InterruptedException e) {
+            LOG.log(Level.SEVERE, "Wait interrupted.", e);
           }
         }
-        if (LOG.isLoggable(Level.FINEST)) LOG.log(Level.FINEST, "{0} producing left {1}", new Object[] {Thread.currentThread(), event});
+
+        if (LOG.isLoggable(Level.FINEST)) {
+          LOG.log(Level.FINEST, "{0} producing left {1}",
+                  new Object[] { Thread.currentThread(), event });
+        }
 
         leftEvent = event;
         leftRef = event;
-        if (rightEvent!=null) {
+
+        if (rightEvent != null) {
           rightRef = rightEvent;
           reset();
           mutex.notifyAll();
         }
       }
 
-      if (rightRef!=null) {
+      if (rightRef != null) {
         // I get to fire the event
         destination.onNext(new Pair<L,R>(leftRef, rightRef));
       }
     }
   }
 
-
-  public class Right implements EventHandler<R> {
+  private class Right implements EventHandler<R> {
 
     @Override
     public void onNext(final R event) {
+
       L leftRef = null;
       R rightRef = null;
-      synchronized(mutex) {
-        while (rightEvent!=null) {
+
+      synchronized (mutex) {
+
+        while (rightEvent != null) {
           try {
             mutex.wait();
-          } catch (InterruptedException e) {
-            LOG.log(Level.SEVERE, e.getMessage());
-            e.printStackTrace();
+          } catch (final InterruptedException e) {
+            LOG.log(Level.SEVERE, "Wait interrupted.", e);
           }
         }
 
-        if (LOG.isLoggable(Level.FINEST)) LOG.log(Level.FINEST, "{0} producing right {1}", new Object[] {Thread.currentThread(), event});
+        if (LOG.isLoggable(Level.FINEST)) {
+          LOG.log(Level.FINEST, "{0} producing right {1}",
+                  new Object[] { Thread.currentThread(), event });
+        }
 
         rightEvent = event;
         rightRef = event;
-        if (leftEvent!=null) {
+
+        if (leftEvent != null) {
           leftRef = leftEvent;
           reset();
           mutex.notifyAll();
         }
       }
 
-      if (leftRef!=null) {
+      if (leftRef != null) {
         // I get to fire the event
         destination.onNext(new Pair<L,R>(leftRef, rightRef));
       }
@@ -129,12 +146,10 @@ public final class MergingEventHandler<L,R> {
   public static class Pair<S1,S2> {
     public final S1 first;
     public final S2 second;
-    
+
     private Pair(S1 s1, S2 s2) {
       this.first = s1;
       this.second = s2;
     }
   }
-  
- 
 }
--- wake/src/test/java/com/microsoft/wake/test/MergingEventHandlerTest.java
+++ wake/src/test/java/com/microsoft/wake/test/MergingEventHandlerTest.java
@@ -15,96 +15,98 @@
  */
 package com.microsoft.wake.test;
 
-import static org.junit.Assert.*;
+import com.microsoft.wake.EventHandler;
+import com.microsoft.wake.impl.MergingEventHandler;
+import com.microsoft.wake.impl.MergingEventHandler.Pair;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import com.microsoft.wake.EventHandler;
-import com.microsoft.wake.impl.MergingEventHandler;
-import com.microsoft.wake.impl.MergingEventHandler.Pair;
 
 public class MergingEventHandlerTest {
 
   @Test
   public void testSingleInvocationSingleThread() {
-    int testLeft = 13;
-    int testRight = 23;
-    int expected = testLeft + 31*testRight;
+
+    final int testLeft = 13;
+    final int testRight = 23;
+    final int expected = testLeft + 31 * testRight;
     
     final AtomicInteger i = new AtomicInteger(0);
-    MergingEventHandler<Integer, Integer> dut = new MergingEventHandler<>(new EventHandler<Pair<Integer,Integer>>() {
+
+    final MergingEventHandler<Integer, Integer> dut =
+        new MergingEventHandler<>(new EventHandler<Pair<Integer, Integer>>() {
       @Override
       public void onNext(Pair<Integer, Integer> value) {
-        i.addAndGet(value.first+31*value.second);
+        i.addAndGet(value.first + 31 * value.second);
       }
     });
-    
-    MergingEventHandler<Integer, Integer>.Left left = dut.new Left(); 
-    MergingEventHandler<Integer, Integer>.Right right = dut.new Right(); 
-    
-    left.onNext(testLeft);
-    right.onNext(testRight);
-    assertEquals(expected, i.get());
 
-  }  
+    dut.leftHandler.onNext(testLeft);
+    dut.rightHandler.onNext(testRight);
+
+    Assert.assertEquals(expected, i.get());
+  }
  
   @Test
   public void testSingleInvocationSingleThreadReversed() {
-    int testLeft = 11;
-    int testRight = 47;
-    int expected = testLeft + 17*testRight;
+
+    final int testLeft = 11;
+    final int testRight = 47;
+    final int expected = testLeft + 17 * testRight;
     
     final AtomicInteger i = new AtomicInteger(0);
-    MergingEventHandler<Integer, Integer> dut = new MergingEventHandler<>(new EventHandler<Pair<Integer,Integer>>() {
+
+    final MergingEventHandler<Integer, Integer> dut =
+        new MergingEventHandler<>(new EventHandler<Pair<Integer, Integer>>() {
       @Override
-      public void onNext(Pair<Integer, Integer> value) {
-        i.addAndGet(value.first+17*value.second);
+      public void onNext(final Pair<Integer, Integer> value) {
+        i.addAndGet(value.first + 17 * value.second);
       }
     });
     
-    MergingEventHandler<Integer, Integer>.Left left = dut.new Left(); 
-    MergingEventHandler<Integer, Integer>.Right right = dut.new Right(); 
+    dut.rightHandler.onNext(testRight);
+    dut.leftHandler.onNext(testLeft);
 
-    right.onNext(testRight);
-    left.onNext(testLeft);
-    assertEquals(expected, i.get());
+    Assert.assertEquals(expected, i.get());
   }
 
   @Test
   public void testMultipleInvocationSingleThread() {
-    int testLeft1 = 13;
-    int testRight1 = 23;
-    int testLeft2 = 14;
-    int testRight2 = 1001;
-    int expected1 = testLeft1 + 31*testRight1;
-    int expected2 = testLeft2 + 31*testRight2;
+
+    final int testLeft1 = 13;
+    final int testRight1 = 23;
+    final int testLeft2 = 14;
+    final int testRight2 = 1001;
+    final int expected1 = testLeft1 + 31 * testRight1;
+    final int expected2 = testLeft2 + 31 * testRight2;
     
     final AtomicInteger i = new AtomicInteger(0);
-    MergingEventHandler<Integer, Integer> dut = new MergingEventHandler<>(new EventHandler<Pair<Integer,Integer>>() {
+
+    final MergingEventHandler<Integer, Integer> dut =
+        new MergingEventHandler<>(new EventHandler<Pair<Integer, Integer>>() {
       @Override
       public void onNext(Pair<Integer, Integer> value) {
-        i.addAndGet(value.first+31*value.second);
+        i.addAndGet(value.first + 31 * value.second);
       }
     });
     
-    MergingEventHandler<Integer, Integer>.Left left = dut.new Left(); 
-    MergingEventHandler<Integer, Integer>.Right right = dut.new Right(); 
-
-    left.onNext(testLeft1);
-    right.onNext(testRight1);
+    dut.leftHandler.onNext(testLeft1);
+    dut.rightHandler.onNext(testRight1);
     
-    left.onNext(testLeft2);
-    right.onNext(testRight2);
-    assertEquals(expected1+expected2, i.get());
+    dut.leftHandler.onNext(testLeft2);
+    dut.rightHandler.onNext(testRight2);
+
+    Assert.assertEquals(expected1+expected2, i.get());
   }  
   
   @Test
   public void testMultipleInvocationMultipleThread() {
+
     final int testLeft1 = 13;
     final int testRight1 = 23;
     final int testLeft2 = 14;
@@ -113,115 +115,121 @@ public class MergingEventHandlerTest {
     final int expected2 = testLeft2 + 31*testRight2;
     
     final AtomicInteger i = new AtomicInteger(0);
-    final MergingEventHandler<Integer, Integer> dut = new MergingEventHandler<>(new EventHandler<Pair<Integer,Integer>>() {
+
+    final MergingEventHandler<Integer, Integer> dut =
+        new MergingEventHandler<>(new EventHandler<Pair<Integer, Integer>>() {
       @Override
       public void onNext(Pair<Integer, Integer> value) {
-        i.addAndGet(value.first+31*value.second);
+        i.addAndGet(value.first + 31 * value.second);
       }
     });
     
-    final MergingEventHandler<Integer, Integer>.Left left = dut.new Left(); 
-    final MergingEventHandler<Integer, Integer>.Right right = dut.new Right(); 
-
     // relies on Executor using both threads
-    ExecutorService e = Executors.newFixedThreadPool(2);
-    e.submit(new Runnable() {
+    final ExecutorService pool = Executors.newFixedThreadPool(2);
+
+    pool.submit(new Runnable() {
       @Override
       public void run() {
-        left.onNext(testLeft1);
-        right.onNext(testRight2);
+        dut.leftHandler.onNext(testLeft1);
+        dut.rightHandler.onNext(testRight2);
       }
     });
 
-    e.submit(new Runnable() {
+    pool.submit(new Runnable() {
       @Override
       public void run() {
-        right.onNext(testRight1);
-        left.onNext(testLeft2);
+        dut.rightHandler.onNext(testRight1);
+        dut.leftHandler.onNext(testLeft2);
       }
     });
     
-    e.shutdown();
+    pool.shutdown();
+
     try {
-      e.awaitTermination(20, TimeUnit.SECONDS);
-    } catch (InterruptedException e1) {
-      fail("Timeout waiting for events to fire, perhaps due to deadlock");
+      pool.awaitTermination(20, TimeUnit.SECONDS);
+    } catch (final InterruptedException e1) {
+      Assert.fail("Timeout waiting for events to fire, perhaps due to deadlock");
     }
-    
-    assertEquals(expected1+expected2, i.get());
+
+    Assert.assertEquals(expected1+expected2, i.get());
   }  
 
   @Test
   public void testManyInvocations() {
+
     final int expectedEvents = 200;
     final int numLeftTasks = 2;
     final int numRightTasks = 4;
     
     final int eventsPerLeft = expectedEvents/numLeftTasks;
-    assertEquals("Test parameters must divide", expectedEvents, numLeftTasks*eventsPerLeft);
+    Assert.assertEquals("Test parameters must divide",
+        expectedEvents, numLeftTasks * eventsPerLeft);
+
     final int eventsPerRight = expectedEvents/numRightTasks;
-    assertEquals("Test parameters must divide", expectedEvents, numRightTasks*eventsPerRight);
+    Assert.assertEquals("Test parameters must divide",
+        expectedEvents, numRightTasks * eventsPerRight);
     
     final AtomicInteger i = new AtomicInteger(0);
-    final MergingEventHandler<Integer, Integer> dut = new MergingEventHandler<>(new EventHandler<Pair<Integer,Integer>>() {
+
+    final MergingEventHandler<Integer, Integer> dut =
+        new MergingEventHandler<>(new EventHandler<Pair<Integer, Integer>>() {
       @Override
       public void onNext(Pair<Integer, Integer> value) {
         i.incrementAndGet();
       }
     });
-    
-    final MergingEventHandler<Integer, Integer>.Left left = dut.new Left(); 
-    final MergingEventHandler<Integer, Integer>.Right right = dut.new Right(); 
 
     // relies on Executor making all tasks concurrent
-    ExecutorService e = Executors.newCachedThreadPool();
-    for (int l=0; l<numLeftTasks; l++) {
-      e.submit(new Runnable() {
+    final ExecutorService pool = Executors.newCachedThreadPool();
+
+    for (int l = 0; l < numLeftTasks; ++l) {
+      pool.submit(new Runnable() {
         @Override
         public void run() {
-          for (int kk=0; kk<eventsPerLeft; kk++) {
-            left.onNext(kk);
-          }
-        }
-      });
-    }
-    for (int r=0; r<numRightTasks; r++) {
-      e.submit(new Runnable() {
-        @Override
-        public void run() {
-          for (int kk=0; kk<eventsPerRight; kk++) {
-            right.onNext(kk);
+          for (int kk = 0; kk < eventsPerLeft; ++kk) {
+            dut.leftHandler.onNext(kk);
           }
         }
       });
     }
 
-    e.shutdown();
-    try {
-      e.awaitTermination(30, TimeUnit.SECONDS);
-    } catch (InterruptedException e1) {
-      fail("Timeout waiting for events to fire, perhaps due to deadlock");
+    for (int r = 0; r < numRightTasks; ++r) {
+      pool.submit(new Runnable() {
+        @Override
+        public void run() {
+          for (int kk = 0; kk < eventsPerRight; ++kk) {
+            dut.rightHandler.onNext(kk);
+          }
+        }
+      });
     }
-    
-    assertEquals(expectedEvents, i.get());
+
+    pool.shutdown();
+
+    try {
+      pool.awaitTermination(30, TimeUnit.SECONDS);
+    } catch (final InterruptedException e1) {
+      Assert.fail("Timeout waiting for events to fire, perhaps due to deadlock");
+    }
+
+    Assert.assertEquals(expectedEvents, i.get());
   }  
   
   @Test
   public void testDifferentTypes() {
     final AtomicInteger i = new AtomicInteger(0);
-    MergingEventHandler<Boolean, Double> dut = new MergingEventHandler<>(new EventHandler<Pair<Boolean,Double>>() {
+
+    final MergingEventHandler<Boolean, Double> dut =
+        new MergingEventHandler<>(new EventHandler<Pair<Boolean, Double>>() {
       @Override
       public void onNext(Pair<Boolean,Double> value) {
         i.incrementAndGet();
       }
     });
-    MergingEventHandler<Boolean, Double>.Left left = dut.new Left(); 
-    MergingEventHandler<Boolean, Double>.Right right = dut.new Right(); 
 
-    left.onNext(true);
-    right.onNext(104.0);
-    
-    assertEquals(1, i.get());
+    dut.leftHandler.onNext(true);
+    dut.rightHandler.onNext(104.0);
+
+    Assert.assertEquals(1, i.get());
   }
-
 }
