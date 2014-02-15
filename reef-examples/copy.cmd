699eb59937e62ef4c34a711eae80aeb50fb95286
Julia Wang <jwang98052@yahoo.com>
2014-02-14 16:43:19 -0800

    Name resolution changes on Streaming driver
    Add Name Resolution logic in Driver
    Rename activity to task
    update name space acoordingly

--- reef-examples/copy.cmd
+++ reef-examples/copy.cmd
@@ -16,20 +16,22 @@
 
 copy /y %enlistment_root%\bin\x64\Debug\Incubation\Evaluator\*.* %REEF_HOME%\reef-examples\dotnet  
 copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.StreamInsightActivity.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.StreamInsightTask.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.BlobSIAdapter.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.ServiceBusAdapter.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.Util.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.SqlAzureSIAdapter.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.DataPipeline.ComputeService.PayloadTypes.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.WindowsAzure.Storage.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.Practices.EnterpriseLibrary.Common.* %REEF_HOME%\reef-examples\dotnet
-copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightActivity\Microsoft.Practices.TransientFaultHandling.Core.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.DataPipeline.ComputeService.StreamInsightTask.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.DataPipeline.ComputeService.BlobSIAdapter.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.DataPipeline.ComputeService.ServiceBusAdapter.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.DataPipeline.ComputeService.Util.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.DataPipeline.ComputeService.SqlAzureSIAdapter.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.DataPipeline.ComputeService.PayloadTypes.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.WindowsAzure.Storage.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.Practices.EnterpriseLibrary.Common.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.Practices.TransientFaultHandling.Core.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\System.Reactive.* %REEF_HOME%\reef-examples\dotnet
 copy /y %enlistment_root%\bin\x64\Debug\Incubation\Streaming\StreamInsightTask\Microsoft.ComplexEventProcessing.* %REEF_HOME%\reef-examples\dotnet
-copy /z /y %REEF_HOME%\..\TANG\tang\src\test\cs\TangTest\bin\x64\Debug\task.bin  %REEF_HOME%\reef-examples\dotnet
-copy /z /y %REEF_HOME%\..\TANG\tang\src\test\cs\TangTest\bin\x64\Debug\com.microsoft.reef.task.*  %REEF_HOME%\reef-examples\dotnet
-copy /z /y %REEF_HOME%\..\TANG\tang\src\test\cs\TangTest\bin\x64\Debug\com.microsoft.reef.activityInterface.*  %REEF_HOME%\reef-examples\dotnet
+
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\ITask\Microsoft.Reef.Tasks.ITask.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\StreamingTask\Microsoft.Reef.Tasks.StreamingTask.* %REEF_HOME%\reef-examples\dotnet
+copy /y %enlistment_root%\bin\x64\Debug\Incubation\ClassHierarchyBuilder\task.bin %REEF_HOME%\reef-examples\dotnet
 
 
 
--- reef-examples/src/main/java/com/microsoft/reef/examples/helloStreaming/HelloDriver.java
+++ reef-examples/src/main/java/com/microsoft/reef/examples/helloStreaming/HelloDriver.java
@@ -15,6 +15,7 @@
  */
 package com.microsoft.reef.examples.helloStreaming;
 
+import com.microsoft.reef.driver.task.RunningTask;
 import com.microsoft.reef.driver.task.TaskConfiguration;
 import com.microsoft.reef.driver.context.ContextConfiguration;
 import com.microsoft.reef.driver.evaluator.AllocatedEvaluator;
@@ -38,6 +39,7 @@ import javax.inject.Inject;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -53,6 +55,7 @@ public final class HelloDriver {
 
   private int nJVMTasks = 0;  // guarded by this
   private int nCLRTasks = 2;  // guarded by this
+  private Hashtable<String, String> ipAddresses = new Hashtable<String, String>();
 
   /**
    * Job driver constructor - instantiated via TANG.
@@ -93,15 +96,36 @@ public final class HelloDriver {
                 //HelloDriver.this.onNextCLR(allocatedEvaluator);
                 HelloDriver.this.secondSiNode(allocatedEvaluator);
             } else if (nCLRTasks == 1) {
-                HelloDriver.this.firstSiNode(allocatedEvaluator);
-            }
+                try {
+                    String ip = ipAddresses.get("HelloStreaming2Context");
 
-          HelloDriver.this.nCLRTasks -= 1;
+                    while (ip == null) {
+                        Thread.sleep(1000);
+                        ip = ipAddresses.get("HelloStreaming2Context");
+                    }
+                    ip = ip.substring(ip.indexOf("/") + 1, ip.indexOf(":"));
+                    LOG.log(Level.INFO, "IP of the second node passed to first node: " + ip);
+                    HelloDriver.this.firstSiNode(allocatedEvaluator, ip);
+                }
+                catch(Exception e){
+
+                }
+            }
+            HelloDriver.this.nCLRTasks -= 1;
         }
       }
     }
   }
 
+    final class TaskRunningHandler implements EventHandler<RunningTask> {
+        @Override
+        public void onNext(final RunningTask runningActivity) {
+            String ip = runningActivity.getActiveContext().getEvaluatorDescriptor().getNodeDescriptor().getInetSocketAddress().toString();
+            String id =  runningActivity.getActiveContext().getId();
+            LOG.log(Level.INFO, "RunningActivity on address: " + ip + " for id " + id);
+            ipAddresses.put(id, ip);
+        }
+    }
   /**
    * Uses the AllocatedEvaluator to launch a CLR task.
    *
@@ -114,7 +138,7 @@ public final class HelloDriver {
           .set(ContextConfiguration.IDENTIFIER, "HelloREEFContext")
           .build();
 
-      final Configuration taskConfiguration = getCLRTaskConfiguration("Hello_From_CLR", "com.microsoft.reef.task.HelloTask");
+      final Configuration taskConfiguration = getCLRTaskConfiguration("Hello_From_CLR", "Microsoft.Reef.Tasks.HelloTask");
 
       allocatedEvaluator.submitContextAndTask(contextConfiguration, taskConfiguration);
     } catch (final BindException ex) {
@@ -124,14 +148,27 @@ public final class HelloDriver {
     }
   }
 
-    final void firstSiNode(final AllocatedEvaluator allocatedEvaluator) {
+    final void firstSiNode(final AllocatedEvaluator allocatedEvaluator, String ip) {
         try {
             allocatedEvaluator.setType(EvaluatorType.CLR);
             final Configuration contextConfiguration = ContextConfiguration.CONF
                     .set(ContextConfiguration.IDENTIFIER, "HelloStreaming1Context")
                     .build();
 
-            final Configuration taskConfiguration = getCLRTaskConfiguration("Hello_From_Streaming1", "com.microsoft.reef.task.StreamTask1");
+           // final Configuration taskConfiguration = getCLRTaskConfiguration("Hello_From_Streaming1", "com.microsoft.reef.task.StreamTask1");
+
+            final ConfigurationBuilder taskConfigurationBuilder = Tang.Factory.getTang()
+                    .newConfigurationBuilder(loadClassHierarchy());
+
+            taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.TaskConfigurationOptions.Identifier", "Hello_From_Streaming1");
+            taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.ITask", "Microsoft.Reef.Tasks.StreamTask1");
+
+            if (ip != null){
+                taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.StreamTask1.IpAddress", ip);
+            }
+
+            final Configuration taskConfiguration =  taskConfigurationBuilder.build();
+
             // final Configuration taskConfiguration = getCLRTaskConfiguration1("Hello_From_Streaming1");
 
             allocatedEvaluator.submitContextAndTask(contextConfiguration, taskConfiguration);
@@ -150,7 +187,7 @@ public final class HelloDriver {
                     .build();
 
             // final Configuration taskConfiguration = getCLRTaskConfiguration2("Hello_From_Streaming2");
-            final Configuration taskConfiguration = getCLRTaskConfiguration("Hello_From_Streaming2", "com.microsoft.reef.task.StreamTask2");
+            final Configuration taskConfiguration = getCLRTaskConfiguration("Hello_From_Streaming2", "Microsoft.Reef.Tasks.StreamTask2");
 
             allocatedEvaluator.submitContextAndTask(contextConfiguration, taskConfiguration);
         } catch (final BindException ex) {
@@ -199,8 +236,9 @@ public final class HelloDriver {
     final ConfigurationBuilder taskConfigurationBuilder = Tang.Factory.getTang()
         .newConfigurationBuilder(loadClassHierarchy());
 
-    taskConfigurationBuilder.bind("com.microsoft.reef.driver.task.TaskConfigurationOptions.Identifier", taskId);
-    taskConfigurationBuilder.bind("com.microsoft.reef.task.ITask", taskImplementationClassName);
+      taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.TaskConfigurationOptions.Identifier", taskId);
+      taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.ITask", taskImplementationClassName);
+
     return taskConfigurationBuilder.build();
   }
 
@@ -209,7 +247,7 @@ public final class HelloDriver {
    * @return
    */
   private static ClassHierarchy loadClassHierarchy() {
-    try (final InputStream chin = new FileInputStream(HelloCLR.CLASS_HIERARCHY_FILENAME)) {
+    try (final InputStream chin = new FileInputStream(HelloStreaming.CLASS_HIERARCHY_FILENAME)) {
       final ClassHierarchyProto.Node root = ClassHierarchyProto.Node.parseFrom(chin); // A
       final ClassHierarchy ch = new ProtocolBufferClassHierarchy(root);
       return ch;
