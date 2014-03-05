3b6bcc5dfae609b21ae7b3addd6bc3ffde356a76
Markus Weimer <mweimer@microsoft.com>
2014-03-05 15:31:39 -0800

    Added {} Synchronized all methods in EvaluatorContext

--- reef-common/src/main/java/com/microsoft/reef/runtime/common/driver/EvaluatorContext.java
+++ reef-common/src/main/java/com/microsoft/reef/runtime/common/driver/EvaluatorContext.java
@@ -47,7 +47,7 @@ public final class EvaluatorContext implements ActiveContext {
   }
 
   @Override
-  public void close() {
+  public synchronized void close() {
     if (this.closed) {
       throw new RuntimeException("Active context already closed");
     }
@@ -64,8 +64,10 @@ public final class EvaluatorContext implements ActiveContext {
   }
 
   @Override
-  public void sendMessage(final byte[] message) {
-    if (this.closed) throw new RuntimeException("Active context already closed");
+  public synchronized void sendMessage(final byte[] message) {
+    if (this.closed) {
+      throw new RuntimeException("Active context already closed");
+    }
     LOG.log(Level.FINEST, "Send message: RunningEvaluator id[" + getEvaluatorId() + "] context id[" + getId() + "]");
 
     final EvaluatorRuntimeProtocol.ContextControlProto contextControlProto =
@@ -79,8 +81,10 @@ public final class EvaluatorContext implements ActiveContext {
   }
 
   @Override
-  public void submitTask(Configuration taskConf) {
-    if (this.closed) throw new RuntimeException("Active context already closed");
+  public synchronized void submitTask(Configuration taskConf) {
+    if (this.closed) {
+      throw new RuntimeException("Active context already closed");
+    }
     LOG.log(Level.FINEST, "Submit task: RunningEvaluator id[" + getEvaluatorId() + "] context id[" + getId() + "]");
 
     final EvaluatorRuntimeProtocol.ContextControlProto contextControlProto =
@@ -94,8 +98,10 @@ public final class EvaluatorContext implements ActiveContext {
   }
 
   @Override
-  public void submitContext(final Configuration contextConfiguration) {
-    if (this.closed) throw new RuntimeException("Active context already closed");
+  public synchronized void submitContext(final Configuration contextConfiguration) {
+    if (this.closed) {
+      throw new RuntimeException("Active context already closed");
+    }
     LOG.log(Level.FINEST, "Submit new context: RunningEvaluator id[" + getEvaluatorId() + "] parent context id[" + getId() + "]");
 
     final EvaluatorRuntimeProtocol.ContextControlProto contextControlProto =
@@ -110,8 +116,10 @@ public final class EvaluatorContext implements ActiveContext {
   }
 
   @Override
-  public void submitContextAndService(final Configuration contextConfiguration, final Configuration serviceConfiguration) {
-    if (this.closed) throw new RuntimeException("Active context already closed");
+  public synchronized void submitContextAndService(final Configuration contextConfiguration, final Configuration serviceConfiguration) {
+    if (this.closed) {
+      throw new RuntimeException("Active context already closed");
+    }
     LOG.log(Level.FINEST, "Submit new context: RunningEvaluator id[" + getEvaluatorId() + "] parent context id[" + getId() + "]");
 
     final EvaluatorRuntimeProtocol.ContextControlProto contextControlProto =
