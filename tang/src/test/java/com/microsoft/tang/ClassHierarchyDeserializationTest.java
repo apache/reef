57f02fb06cbe57318dd85cf11c470796626ff723
Markus Weimer <mweimer@microsoft.com>
2014-03-18 13:54:39 -0700

    Added a test case

---
+++ tang/src/test/java/com/microsoft/tang/ClassHierarchyDeserializationTest.java
@@ -0,0 +1,15 @@
+package com.microsoft.tang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test case for class hierarchy deserialization.
+ */
+public class ClassHierarchyDeserializationTest {
+
+  @Test
+  public void testDeserialization() {
+    Assert.assertTrue(true);
+  }
+}
