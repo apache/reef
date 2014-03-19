18f39332165546ee9a18170592253b2208ed3b4d
beysims <beysims@hotmail.com>
2014-03-19 14:32:39 -0700

    improvement

---
+++ reef-bridge-project/reef-bridge-java/src/main/java/javabridge/EDriver.java
@@ -0,0 +1,7 @@
+package javabridge;
+
+/**
+ * Created by beysims on 3/18/14.
+ */
+public class EDriver {
+}
---
+++ reef-bridge-project/reef-bridge-java/src/main/java/javabridge/InteropLogger.java
@@ -0,0 +1,7 @@
+package javabridge;
+
+/**
+ * Created by beysims on 3/19/14.
+ */
+public class InteropLogger {
+}
