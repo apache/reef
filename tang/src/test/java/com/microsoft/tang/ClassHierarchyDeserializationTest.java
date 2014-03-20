000198a60f2fdc961506a6c5c66b2c51fafdc1f4
Julia Wang <jwang98052@yahoo.com>
2014-03-20 16:46:14 -0700

    Consolidate the two tests

--- tang/src/test/java/com/microsoft/tang/ClassHierarchyDeserializationTest.java
+++ tang/src/test/java/com/microsoft/tang/ClassHierarchyDeserializationTest.java
@@ -19,24 +19,34 @@ import java.util.logging.Level;
 public class ClassHierarchyDeserializationTest {
 
   @Test
-  public void testDeserializationForStreamTask() {
+  public void testDeserializationForTasks() {
       try (final InputStream chin = Thread.currentThread().getContextClassLoader()
               .getResourceAsStream("Task.bin")) {
           final ClassHierarchyProto.Node root = ClassHierarchyProto.Node.parseFrom(chin); // A
           final ClassHierarchy ch = new ProtocolBufferClassHierarchy(root);
-          Node n = ch.getNode("Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
-          Assert.assertTrue(n.getFullName().equals("Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+          Node n1 = ch.getNode("Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+          Assert.assertTrue(n1.getFullName().equals("Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
 
-          final ConfigurationBuilder taskConfigurationBuilder = Tang.Factory.getTang()
+          Node n2 = ch.getNode("Microsoft.Reef.Tasks.HelloTask, Microsoft.Reef.Tasks.HelloTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+          Assert.assertTrue(n2.getFullName().equals("Microsoft.Reef.Tasks.HelloTask, Microsoft.Reef.Tasks.HelloTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+
+          final ConfigurationBuilder taskConfigurationBuilder1 = Tang.Factory.getTang()
+                  .newConfigurationBuilder(ch);
+
+          final ConfigurationBuilder taskConfigurationBuilder2 = Tang.Factory.getTang()
                   .newConfigurationBuilder(ch);
           try {
-            taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.TaskConfigurationOptions+Identifier, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Hello_From_Streaming1");
-            taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.ITask, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            taskConfigurationBuilder1.bind("Microsoft.Reef.Tasks.TaskConfigurationOptions+Identifier, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Hello_From_Streaming1");
+            taskConfigurationBuilder1.bind("Microsoft.Reef.Tasks.ITask, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            taskConfigurationBuilder1.build();
+
+            taskConfigurationBuilder2.bind("Microsoft.Reef.Tasks.TaskConfigurationOptions+Identifier, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Hello_From_HelloTask");
+            taskConfigurationBuilder2.bind("Microsoft.Reef.Tasks.ITask, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Microsoft.Reef.Tasks.HelloTask, Microsoft.Reef.Tasks.HelloTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            taskConfigurationBuilder2.build();
           } catch (final BindException ex) {
             final String message = "Unable to setup Task or Context configuration.";
             throw new RuntimeException(message, ex);
           }
-
       } catch (final IOException e) {
           final String message = "Unable to load class hierarchy.";
           throw new RuntimeException(message, e);
@@ -45,35 +55,4 @@ public class ClassHierarchyDeserializationTest {
           throw new RuntimeException(message, e);
       }
   }
-
-    @Test
-    public void testDeserializationForHelloTask() {
-        try (final InputStream chin = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("Task.bin")) {
-            final ClassHierarchyProto.Node root = ClassHierarchyProto.Node.parseFrom(chin); // A
-            final ClassHierarchy ch = new ProtocolBufferClassHierarchy(root);
-            Node n = ch.getNode("Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
-            Assert.assertTrue(n.getFullName().equals("Microsoft.Reef.Tasks.StreamTask1, Microsoft.Reef.Tasks.StreamingTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
-
-            final ConfigurationBuilder taskConfigurationBuilder = Tang.Factory.getTang()
-                    .newConfigurationBuilder(ch);
-            try {
-                taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.TaskConfigurationOptions+Identifier, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Hello_From_HelloTask");
-                taskConfigurationBuilder.bind("Microsoft.Reef.Tasks.ITask, Microsoft.Reef.Tasks.ITask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Microsoft.Reef.Tasks.HelloTask, Microsoft.Reef.Tasks.HelloTask, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
-
-                taskConfigurationBuilder.build();
-            } catch (final BindException ex) {
-                final String message = "Unable to setup Task or Context configuration.";
-                throw new RuntimeException(message, ex);
-            }
-
-        } catch (final IOException e) {
-            final String message = "Unable to load class hierarchy.";
-            throw new RuntimeException(message, e);
-        }  catch (final NameResolutionException e) {
-            final String message = "Unable to get node from class hierarchy.";
-            throw new RuntimeException(message, e);
-        }
-    }
-
 }
