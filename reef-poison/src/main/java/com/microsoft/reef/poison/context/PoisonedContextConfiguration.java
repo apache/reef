34887dfbf8b9f1cee027ce376394b780cb583a8d
Markus Weimer <mweimer@microsoft.com>
2014-04-23 18:02:36 -0700

    Added REEF-Poison This project will host fault injection tools for REEF. So far, it contains a very simple failure injection for REEF contexts

---
+++ reef-poison/src/main/java/com/microsoft/reef/poison/context/PoisonedContextConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2014 Microsoft Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microsoft.reef.poison.context;
+
+import com.microsoft.reef.driver.context.ContextConfigurationOptions;
+import com.microsoft.reef.poison.context.params.CrashProbability;
+import com.microsoft.reef.poison.context.params.CrashTimeout;
+import com.microsoft.tang.formats.ConfigurationModule;
+import com.microsoft.tang.formats.ConfigurationModuleBuilder;
+import com.microsoft.tang.formats.OptionalParameter;
+
+/**
+ * Configure a Context with a lethal injection.
+ */
+public final class PoisonedContextConfiguration extends ConfigurationModuleBuilder {
+
+  /**
+   * The time window in seconds beginning at ContextStart during which the crash is to occur.
+   */
+  public static final OptionalParameter<Integer> CRASH_TIMEOUT = new OptionalParameter<>();
+
+  /**
+   * The probability with which a crash is to occur.
+   */
+  public static final OptionalParameter<Double> CRASH_PROBABILITY = new OptionalParameter<>();
+
+  public static final ConfigurationModule CONF = new PoisonedContextConfiguration()
+      .bindNamedParameter(CrashTimeout.class, CRASH_TIMEOUT)
+      .bindNamedParameter(CrashProbability.class, CRASH_PROBABILITY)
+      .bindSetEntry(ContextConfigurationOptions.StartHandlers.class, PoisonedStartHandler.class)
+      .build();
+
+
+}
