5ec43b348e9746bfde5f5d992b6f5a2a16a5a6c4
Julia Wang <jwang98052@yahoo.com>
2014-04-28 14:43:11 -0700

    HelloREEFHttp update and pom file change

--- reef-examples/src/main/java/com/microsoft/reef/examples/hello/HelloREEFHttp.java
+++ reef-examples/src/main/java/com/microsoft/reef/examples/hello/HelloREEFHttp.java
@@ -4,6 +4,7 @@ import com.microsoft.reef.client.DriverLauncher;
 import com.microsoft.reef.client.LauncherStatus;
 import com.microsoft.reef.runtime.local.client.LocalRuntimeConfiguration;
 import com.microsoft.reef.webserver.HttpHandlerConfiguration;
+import com.microsoft.reef.webserver.HttpServerReefEventHandler;
 import com.microsoft.tang.Configuration;
 import com.microsoft.tang.Configurations;
 import com.microsoft.tang.exceptions.BindException;
@@ -18,7 +19,9 @@ public final class HelloREEFHttp {
    * @return the driver-side configuration to be merged into the DriverConfiguration to enable the HTTP server.
    */
   public static Configuration getHTTPConfiguration() {
-    return HttpHandlerConfiguration.CONF.build();
+    return HttpHandlerConfiguration.CONF
+        .set(HttpHandlerConfiguration.HTTP_HANDLERS, HttpServerReefEventHandler.class)
+        .build();
   }
 
   public static LauncherStatus runHelloReef(final Configuration runtimeConf, final int timeOut)
@@ -26,7 +29,6 @@ public final class HelloREEFHttp {
     final Configuration driverConf = Configurations.merge(HelloREEF.getDriverConfiguration(), getHTTPConfiguration());
     return DriverLauncher.getLauncher(runtimeConf).run(driverConf, timeOut);
   }
-
 
   public static void main(final String[] args) throws InjectionException {
     final Configuration runtimeConfiguration = LocalRuntimeConfiguration.CONF
