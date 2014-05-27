99c6c7fedaf00784b78bd920ec6c772cd0c0af2e
tmajest <tmajest@microsoft.com>
2014-05-27 15:10:17 -0700

    Moved clr handler out of common

---
+++ reef-utils-hadoop/src/main/java/com/microsoft/reef/util/logging/CLRHandler.java
@@ -0,0 +1,40 @@
+package com.microsoft.reef.util.logging;
+
+import java.util.logging.*;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import javax.inject.Inject;
+import javabridge.*;
+
+
+public class CLRHandler extends Handler
+{
+    @Inject
+    public CLRHandler()
+    {
+        super();
+    }
+
+    @Override
+    public void close() throws SecurityException
+    {
+    }
+
+    @Override
+    public void flush()
+    {
+    }
+
+    @Override
+    public void publish(LogRecord record)
+    {
+        if (!isLoggable(record))
+            return;
+
+        if (record == null)
+            return;
+
+        //NativeInterop.ClrBufferedLog(3, getFormatter().format(record));
+    }
+}
