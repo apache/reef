f56498f790c262e1fa04ab1c78551681273e41b9
tmajest <tmajest@microsoft.com>
2014-05-28 10:45:05 -0700

    Got rid of clr handler in reef-commonm

--- reef-utils-hadoop/src/main/java/com/microsoft/reef/util/logging/CLRHandler.java
+++ reef-utils-hadoop/src/main/java/com/microsoft/reef/util/logging/CLRHandler.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import javax.inject.Inject;
-import javabridge.*;
+import com.microsoft.reef.javabridge.*;
 
 
 public class CLRHandler extends Handler
