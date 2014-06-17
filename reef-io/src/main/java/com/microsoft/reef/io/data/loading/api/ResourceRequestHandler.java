1d541cd9ee3e4cddf73a56ffd24e664fd9532794
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2014-06-16 18:06:59 -0700

    refactored resource request handler for the data loader

--- reef-io/src/main/java/com/microsoft/reef/io/data/loading/api/ResourceRequestHandler.java
+++ reef-io/src/main/java/com/microsoft/reef/io/data/loading/api/ResourceRequestHandler.java
@@ -15,46 +15,48 @@
  */
 package com.microsoft.reef.io.data.loading.api;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.microsoft.reef.driver.evaluator.EvaluatorRequest;
 import com.microsoft.reef.driver.evaluator.EvaluatorRequestor;
 import com.microsoft.wake.EventHandler;
 
-/**
- * 
- */
-public class ResourceRequestHandler implements EventHandler<EvaluatorRequest>{
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ResourceRequestHandler implements EventHandler<EvaluatorRequest> {
+
   private static final Logger LOG = Logger.getLogger(ResourceRequestHandler.class.getName());
+
   private final EvaluatorRequestor requestor;
+
   private CountDownLatch resourceRequestGate = new CountDownLatch(1);
 
-  public ResourceRequestHandler(EvaluatorRequestor requestor) {
-    super();
+  public ResourceRequestHandler(final EvaluatorRequestor requestor) {
     this.requestor = requestor;
   }
 
-  public void releaseResourceRequestGate(){
-    LOG.log(Level.FINE,"Releasing Gate");
-    resourceRequestGate.countDown();
+  public void releaseResourceRequestGate() {
+    LOG.log(Level.FINE, "Releasing Gate");
+    this.resourceRequestGate.countDown();
   }
 
   @Override
-  public void onNext(EvaluatorRequest request) {
-    try {
-      LOG.log(Level.FINE,"Processing a request with count: " + request.getNumber());
-      LOG.log(Level.FINE,"Waiting for gate to be released");
-      resourceRequestGate.await();
-      LOG.log(Level.FINE,"Gate released. Submitting request");
-      LOG.log(Level.FINE,"First resetting latch");
-      resourceRequestGate = new CountDownLatch(1);
-      requestor.submit(request);
-      LOG.log(Level.FINE,"End of request processing");
-    } catch (InterruptedException e) {
-      throw new RuntimeException("Interrupted while waiting for compute requests to be satisfied", e);
-    }
-  }
+  public void onNext(final EvaluatorRequest request) {
 
+    LOG.log(Level.FINE, "Processing a request with count: {0} - Waiting for gate to be released",
+        request.getNumber());
+
+    for (;;) {
+      try {
+        this.resourceRequestGate.await();
+        break;
+      } catch (final InterruptedException ex) {
+        LOG.log(Level.FINEST, "Interrupted", ex);
+      }
+    }
+
+    LOG.log(Level.FINE, "Gate released. Submitting request: {0}", request);
+    this.resourceRequestGate = new CountDownLatch(1);
+    this.requestor.submit(request);
+  }
 }
