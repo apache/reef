3939f67253adbe24724da26cf7a6ada96de593b8
yingdachen <ydchen@gmail.com>
2014-06-24 09:02:25 -0700

    to tirgger a build

--- reef-common/src/main/java/com/microsoft/reef/driver/context/ContextConfiguration.java
+++ reef-common/src/main/java/com/microsoft/reef/driver/context/ContextConfiguration.java
@@ -95,7 +95,7 @@ public class ContextConfiguration extends ConfigurationModuleBuilder {
       .bindSetEntry(TaskConfigurationOptions.StopHandlers.class, ON_TASK_STOP)
       .build();
 
-  @NamedParameter(doc = "house the implementation for re-connecting to driver after driver restart",
+  @NamedParameter(doc = "House the implementation for re-connecting to driver after driver restart",
           default_classes = DefaultDriverConnection.class)
   public static final class DriverReconnect implements Name<DriverConnection> {
   }
