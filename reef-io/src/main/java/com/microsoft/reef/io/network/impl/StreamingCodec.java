5b714ef6034e49b38377c1e84017d199576bf18b
Shravan Narayanamurthy <shravanmn@gmail.com>
2014-09-12 14:53:07 -0700

    Introducing the StreamingCodec interface
    Also includes some clean up and some utils

--- reef-io/src/main/java/com/microsoft/reef/io/network/impl/StreamingCodec.java
+++ reef-io/src/main/java/com/microsoft/reef/io/network/impl/StreamingCodec.java
@@ -1,11 +1,11 @@
-/**
- * Copyright (C) 2014 Microsoft Corporation
+/*
+ * Copyright 2013 Microsoft.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,18 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.microsoft.reef.io.network.group.operators;
+package com.microsoft.reef.io.network.impl;
 
-import com.microsoft.reef.io.network.exception.ParentDeadException;
-import com.microsoft.tang.annotations.Name;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 
-public interface GroupCommOperator {
+import com.microsoft.wake.remote.Codec;
 
-  Class<? extends Name<String>> getOperName();
+/**
+ * A codec that can make serialization more efficient when an object has to be
+ * codec'ed through a chain of codecs
+ */
+public interface StreamingCodec<T> extends Codec<T> {
 
-  Class<? extends Name<String>> getGroupName();
+  void encodeToStream (T obj, DataOutputStream stream);
 
-  void initialize() throws ParentDeadException;
-
-  int getVersion();
+  T decodeFromStream (DataInputStream stream);
 }
