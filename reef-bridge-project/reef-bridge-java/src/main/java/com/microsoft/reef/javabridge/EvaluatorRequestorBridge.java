0b7bc5774362308ac91fce31718f9e59dda8458a
Markus Weimer <github@weimo.de>
2014-10-03 15:10:37 -0700

    Minor cosmetic changes

--- reef-bridge-project/reef-bridge-java/src/main/java/com/microsoft/reef/javabridge/EvaluatorRequestorBridge.java
+++ reef-bridge-project/reef-bridge-java/src/main/java/com/microsoft/reef/javabridge/EvaluatorRequestorBridge.java
@@ -17,80 +17,62 @@
 package com.microsoft.reef.javabridge;
 
 import com.microsoft.reef.driver.catalog.RackDescriptor;
-import com.microsoft.reef.driver.catalog.ResourceCatalog;
 import com.microsoft.reef.driver.evaluator.EvaluatorRequest;
 import com.microsoft.reef.driver.evaluator.EvaluatorRequestor;
 import com.microsoft.reef.proto.DriverRuntimeProtocol;
 import com.microsoft.reef.runtime.common.driver.catalog.ResourceCatalogImpl;
-import com.microsoft.tang.Injector;
-import com.microsoft.tang.JavaConfigurationBuilder;
 import com.microsoft.tang.Tang;
 import com.microsoft.tang.exceptions.InjectionException;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class EvaluatorRequestorBridge extends NativeBridge {
+public final class EvaluatorRequestorBridge extends NativeBridge {
   private static final Logger LOG = Logger.getLogger(EvaluatorRequestorBridge.class.getName());
+  private static final String REQUEST_CONTAINERS_ON_RACK = "*"; // The rack used for all requests
+  private static final String REQUEST_CONTAINER_ON_HOST = "HostName"; // The hostname used for all requests.
 
   // accumulate how many evaluators have been submitted through this instance
   // of EvaluatorRequestorBridge
   private int clrEvaluatorsNumber;
   private final boolean isBlocked;
 
-  private EvaluatorRequestor jevaluatorRequestor;
+  private final EvaluatorRequestor jevaluatorRequestor;
 
-  public EvaluatorRequestorBridge(EvaluatorRequestor evaluatorRequestor, boolean isBlocked)
-  {
+  public EvaluatorRequestorBridge(final EvaluatorRequestor evaluatorRequestor, final boolean isBlocked) {
     this.jevaluatorRequestor = evaluatorRequestor;
     this.clrEvaluatorsNumber = 0;
     this.isBlocked = isBlocked;
   }
 
-  public void submit( final int evaluatorsNumber, final int memory, final int virtualCore, String rack)
-
-  {
-    if (this.isBlocked)
-    {
+  public void submit(final int evaluatorsNumber, final int memory, final int virtualCore, final String rack) {
+    if (this.isBlocked) {
       throw new RuntimeException("Cannot request additional Evaluator, this is probably because the Driver has crashed and restarted, and cannot ask for new container due to YARN-2433.");
     }
-    clrEvaluatorsNumber += evaluatorsNumber;
-    final JavaConfigurationBuilder cb = Tang.Factory.getTang().newConfigurationBuilder();
-    cb.bindImplementation(ResourceCatalog.class, ResourceCatalogImpl.class);
-    Injector injector = Tang.Factory.getTang().newInjector(cb.build());
-    ResourceCatalog resourceCatalog;
-    try {
-      resourceCatalog = injector.getInstance(ResourceCatalog.class);
-    } catch (final InjectionException e) {
-      LOG.log(Level.SEVERE, "Cannot inject resource catalog", e);
-      throw new RuntimeException("Cannot inject resource catalog");
+
+    if (rack != null && !rack.isEmpty()) {
+      LOG.log(Level.WARNING, "Ignoring rack request and using [{0}] instead.", REQUEST_CONTAINERS_ON_RACK);
     }
 
-    ResourceCatalogImpl catalog;
-    try {
-      catalog = (ResourceCatalogImpl) resourceCatalog;
-    } catch (final ClassCastException e) {
-      LOG.log(Level.SEVERE, "Failed to cast to ResourceCatalogImpl.", e);
-      throw e;
-    }
-    if(rack == null || rack.isEmpty())
-    {
-      rack = "/default-rack";
-    }
+    clrEvaluatorsNumber += evaluatorsNumber;
+
+    final ResourceCatalogImpl catalog = getNewResourceCatalogInstance();
+
     catalog.handle(DriverRuntimeProtocol.NodeDescriptorProto.newBuilder()
-        .setRackName("*")
-        .setHostName("HostName")
+        .setRackName(REQUEST_CONTAINERS_ON_RACK)
+        .setHostName(REQUEST_CONTAINER_ON_HOST)
         .setPort(0)
         .setMemorySize(memory)
         .setIdentifier("clrBridgeRackCatalog")
         .build());
-    RackDescriptor rackDescriptor = (RackDescriptor)(catalog.getRacks().toArray())[0];
-    EvaluatorRequest request = EvaluatorRequest.newBuilder().fromDescriptor(rackDescriptor)
+    final RackDescriptor rackDescriptor = (RackDescriptor) (catalog.getRacks().toArray())[0];
+    final EvaluatorRequest request = EvaluatorRequest.newBuilder().fromDescriptor(rackDescriptor)
         .setNumber(evaluatorsNumber)
         .setMemory(memory)
         .setNumberOfCores(virtualCore)
         .build();
 
-    LOG.log(Level.FINE, "submitting evaluator request " + request);
+    LOG.log(Level.FINE, "submitting evaluator request {0}", request);
     jevaluatorRequestor.submit(request);
   }
 
@@ -98,8 +80,21 @@ public class EvaluatorRequestorBridge extends NativeBridge {
     return clrEvaluatorsNumber;
   }
 
+  /**
+   * Helper method that uses Tang to generate a new instance of ResourceCatalogImpl
+   *
+   * @return a new instance of ResourceCatalogImpl
+   */
+  private ResourceCatalogImpl getNewResourceCatalogInstance() {
+    try {
+      return Tang.Factory.getTang().newInjector().getInstance(ResourceCatalogImpl.class);
+    } catch (final InjectionException e) {
+      LOG.log(Level.SEVERE, "Cannot inject resource catalog", e);
+      throw new RuntimeException("Cannot inject resource catalog");
+    }
+  }
+
   @Override
-  public void close()
-  {
+  public void close() {
   }
 }
