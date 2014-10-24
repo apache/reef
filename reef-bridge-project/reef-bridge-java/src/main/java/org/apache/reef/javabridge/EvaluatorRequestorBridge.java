16dd52a353f3ac57695e50670440c85b5f385829
John Yang <johnyangk@gmail.com>
2014-10-24 15:36:24 +0900

    This address [REEF-6] and [REEF-25]. It changes the policy of sending requests to YARN RM from "one-request-at-a-time" to "one-kind-of-requests-at-a-time". Contributed by John Yang <johnyangk@apache.org> Closes #4

--- reef-bridge-project/reef-bridge-java/src/main/java/org/apache/reef/javabridge/EvaluatorRequestorBridge.java
+++ reef-bridge-project/reef-bridge-java/src/main/java/org/apache/reef/javabridge/EvaluatorRequestorBridge.java
@@ -18,21 +18,14 @@
  */
 package org.apache.reef.javabridge;
 
-import org.apache.reef.driver.catalog.RackDescriptor;
 import org.apache.reef.driver.evaluator.EvaluatorRequest;
 import org.apache.reef.driver.evaluator.EvaluatorRequestor;
-import org.apache.reef.proto.DriverRuntimeProtocol;
-import org.apache.reef.runtime.common.driver.catalog.ResourceCatalogImpl;
-import org.apache.reef.tang.Tang;
-import org.apache.reef.tang.exceptions.InjectionException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public final class EvaluatorRequestorBridge extends NativeBridge {
   private static final Logger LOG = Logger.getLogger(EvaluatorRequestorBridge.class.getName());
-  private static final String REQUEST_CONTAINERS_ON_RACK = "*"; // The rack used for all requests
-  private static final String REQUEST_CONTAINER_ON_HOST = "HostName"; // The hostname used for all requests.
   private final boolean isBlocked;
   private final EvaluatorRequestor jevaluatorRequestor;
   // accumulate how many evaluators have been submitted through this instance
@@ -51,22 +44,12 @@ public final class EvaluatorRequestorBridge extends NativeBridge {
     }
 
     if (rack != null && !rack.isEmpty()) {
-      LOG.log(Level.WARNING, "Ignoring rack request and using [{0}] instead.", REQUEST_CONTAINERS_ON_RACK);
+      LOG.log(Level.WARNING, "Ignoring rack preference.");
     }
 
     clrEvaluatorsNumber += evaluatorsNumber;
 
-    final ResourceCatalogImpl catalog = getNewResourceCatalogInstance();
-
-    catalog.handle(DriverRuntimeProtocol.NodeDescriptorProto.newBuilder()
-        .setRackName(REQUEST_CONTAINERS_ON_RACK)
-        .setHostName(REQUEST_CONTAINER_ON_HOST)
-        .setPort(0)
-        .setMemorySize(memory)
-        .setIdentifier("clrBridgeRackCatalog")
-        .build());
-    final RackDescriptor rackDescriptor = (RackDescriptor) (catalog.getRacks().toArray())[0];
-    final EvaluatorRequest request = EvaluatorRequest.newBuilder().fromDescriptor(rackDescriptor)
+    final EvaluatorRequest request = EvaluatorRequest.newBuilder()
         .setNumber(evaluatorsNumber)
         .setMemory(memory)
         .setNumberOfCores(virtualCore)
@@ -78,20 +61,6 @@ public final class EvaluatorRequestorBridge extends NativeBridge {
 
   public int getEvaluatorNumber() {
     return clrEvaluatorsNumber;
-  }
-
-  /**
-   * Helper method that uses Tang to generate a new instance of ResourceCatalogImpl
-   *
-   * @return a new instance of ResourceCatalogImpl
-   */
-  private ResourceCatalogImpl getNewResourceCatalogInstance() {
-    try {
-      return Tang.Factory.getTang().newInjector().getInstance(ResourceCatalogImpl.class);
-    } catch (final InjectionException e) {
-      LOG.log(Level.SEVERE, "Cannot inject resource catalog", e);
-      throw new RuntimeException("Cannot inject resource catalog");
-    }
   }
 
   @Override
