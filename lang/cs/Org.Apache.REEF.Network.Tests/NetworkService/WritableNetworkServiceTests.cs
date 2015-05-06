a42b83a3ac1fdfcbb5daca653650850a21c214a8
dkm2110 <dhruv.mahajan@gmail.com>
2015-05-06 14:10:57 -0700

    buggy code

---
+++ lang/cs/Org.Apache.REEF.Network.Tests/NetworkService/WritableNetworkServiceTests.cs
@@ -0,0 +1,235 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System;
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Org.Apache.REEF.Common.Io;
+using Org.Apache.REEF.Network.Naming;
+using Org.Apache.REEF.Network.NetworkService;
+using Org.Apache.REEF.Tang.Annotations;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Util;
+using Org.Apache.REEF.Wake;
+using Org.Apache.REEF.Wake.Impl;
+using Org.Apache.REEF.Wake.Remote;
+using Org.Apache.REEF.Wake.Remote.Impl;
+using Org.Apache.REEF.Wake.Util;
+
+namespace Org.Apache.REEF.Network.Tests.NetworkService
+{
+    /// <summary>
+    /// Tests for Writable Network Service
+    /// </summary>
+    [TestClass]
+    [Obsolete("Need to remove Iwritable and use IstreamingCodec. Please see Jira REEF-295 ", false)]
+    public class WritableNetworkServiceTests
+    {
+        /// <summary>
+        /// Tests one way communication between two network services
+        /// </summary>
+        [TestMethod]
+        public void TestWritableNetworkServiceOneWayCommunication()
+        {
+            int networkServicePort1 = NetworkUtils.GenerateRandomPort(6000, 7000);
+            int networkServicePort2 = NetworkUtils.GenerateRandomPort(7001, 8000);
+
+            BlockingCollection<WritableString> queue = new BlockingCollection<WritableString>();
+
+            using (INameServer nameServer = new NameServer(0))
+            {
+                IPEndPoint endpoint = nameServer.LocalEndpoint;
+                int nameServerPort = endpoint.Port;
+                string nameServerAddr = endpoint.Address.ToString();
+                using (INetworkService<WritableString> networkService1 = BuildNetworkService(networkServicePort1, nameServerPort, nameServerAddr, null))
+                using (INetworkService<WritableString> networkService2 = BuildNetworkService(networkServicePort2, nameServerPort, nameServerAddr, new MessageHandler(queue)))
+                {
+                    IIdentifier id1 = new StringIdentifier("service1");
+                    IIdentifier id2 = new StringIdentifier("service2");
+                    networkService1.Register(id1);
+                    networkService2.Register(id2);
+
+                    using (IConnection<WritableString> connection = networkService1.NewConnection(id2))
+                    {
+                        connection.Open();
+                        connection.Write(new WritableString("abc"));
+                        connection.Write(new WritableString("def"));
+                        connection.Write(new WritableString("ghi"));
+
+                        Assert.AreEqual("abc", queue.Take().Data);
+                        Assert.AreEqual("def", queue.Take().Data);
+                        Assert.AreEqual("ghi", queue.Take().Data);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests two way communication between two network services
+        /// </summary>
+        [TestMethod]
+        public void TestWritableNetworkServiceTwoWayCommunication()
+        {
+            int networkServicePort1 = NetworkUtils.GenerateRandomPort(6000, 7000);
+            int networkServicePort2 = NetworkUtils.GenerateRandomPort(7001, 8000);
+
+            BlockingCollection<WritableString> queue1 = new BlockingCollection<WritableString>();
+            BlockingCollection<WritableString> queue2 = new BlockingCollection<WritableString>();
+
+            using (INameServer nameServer = new NameServer(0))
+            {
+                IPEndPoint endpoint = nameServer.LocalEndpoint;
+                int nameServerPort = endpoint.Port;
+                string nameServerAddr = endpoint.Address.ToString();
+                using (INetworkService<WritableString> networkService1 = BuildNetworkService(networkServicePort1, nameServerPort, nameServerAddr, new MessageHandler(queue1)))
+                using (INetworkService<WritableString> networkService2 = BuildNetworkService(networkServicePort2, nameServerPort, nameServerAddr, new MessageHandler(queue2)))
+                {
+                    IIdentifier id1 = new StringIdentifier("service1");
+                    IIdentifier id2 = new StringIdentifier("service2");
+                    networkService1.Register(id1);
+                    networkService2.Register(id2);
+
+                    using (IConnection<WritableString> connection1 = networkService1.NewConnection(id2))
+                    using (IConnection<WritableString> connection2 = networkService2.NewConnection(id1))
+                    {
+                        connection1.Open();
+                        connection1.Write(new WritableString("abc"));
+                        connection1.Write(new WritableString("def"));
+                        connection1.Write(new WritableString("ghi"));
+
+                        connection2.Open();
+                        connection2.Write(new WritableString("jkl"));
+                        connection2.Write(new WritableString("mno"));
+
+                        Assert.AreEqual("abc", queue2.Take().Data);
+                        Assert.AreEqual("def", queue2.Take().Data);
+                        Assert.AreEqual("ghi", queue2.Take().Data);
+
+                        Assert.AreEqual("jkl", queue1.Take().Data);
+                        Assert.AreEqual("mno", queue1.Take().Data);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates an instance of network service.
+        /// </summary>
+        /// <param name="networkServicePort">The port that the NetworkService will listen on</param>
+        /// <param name="nameServicePort">The port of the NameServer</param>
+        /// <param name="nameServiceAddr">The ip address of the NameServer</param>
+        /// <param name="handler">The observer to handle incoming messages</param>
+        /// <returns></returns>
+        private INetworkService<WritableString> BuildNetworkService(
+            int networkServicePort,
+            int nameServicePort,
+            string nameServiceAddr,
+            IObserver<WritableNsMessage<WritableString>> handler)
+        {
+            // Test injection
+            if (handler == null)
+            {
+                var networkServiceConf = TangFactory.GetTang().NewConfigurationBuilder()
+                    .BindNamedParameter<NetworkServiceOptions.NetworkServicePort, int>(
+                        GenericType<NetworkServiceOptions.NetworkServicePort>.Class,
+                        networkServicePort.ToString(CultureInfo.CurrentCulture))
+                    .BindNamedParameter<NamingConfigurationOptions.NameServerPort, int>(
+                        GenericType<NamingConfigurationOptions.NameServerPort>.Class,
+                        nameServicePort.ToString(CultureInfo.CurrentCulture))
+                    .BindNamedParameter<NamingConfigurationOptions.NameServerAddress, string>(
+                        GenericType<NamingConfigurationOptions.NameServerAddress>.Class,
+                        nameServiceAddr)
+                    .BindImplementation(GenericType<INameClient>.Class, GenericType<NameClient>.Class)
+                    .BindImplementation(GenericType<IObserver<WritableNsMessage<WritableString>>>.Class, GenericType<NetworkMessageHandler>.Class)
+                    .Build();
+
+                return TangFactory.GetTang().NewInjector(networkServiceConf).GetInstance<WritableNetworkService<WritableString>>();
+            }
+
+            var nameserverConf = TangFactory.GetTang().NewConfigurationBuilder()
+             .BindNamedParameter<NamingConfigurationOptions.NameServerPort, int>(
+                 GenericType<NamingConfigurationOptions.NameServerPort>.Class,
+                 nameServicePort.ToString(CultureInfo.CurrentCulture))
+             .BindNamedParameter<NamingConfigurationOptions.NameServerAddress, string>(
+                 GenericType<NamingConfigurationOptions.NameServerAddress>.Class,
+                 nameServiceAddr)
+             .BindImplementation(GenericType<INameClient>.Class, GenericType<NameClient>.Class)
+             .Build();
+            var injector = TangFactory.GetTang().NewInjector(nameserverConf);
+            var nameClient = injector.GetInstance<NameClient>();
+            var remoteManager = injector.GetInstance<WritableRemoteManagerFactory>();
+            return new WritableNetworkService<WritableString>(networkServicePort,
+                handler, new StringIdentifierFactory(), nameClient, remoteManager);
+        }
+
+        /// <summary>
+        /// The observer to handle incoming messages for WritableString
+        /// </summary>
+        private class MessageHandler : IObserver<WritableNsMessage<WritableString>>
+        {
+            private readonly BlockingCollection<WritableString> _queue;
+
+            public MessageHandler(BlockingCollection<WritableString> queue)
+            {
+                _queue = queue;
+            }
+
+            public void OnNext(WritableNsMessage<WritableString> value)
+            {
+                _queue.Add(value.Data.First());
+            }
+
+            public void OnError(Exception error)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnCompleted()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// The network handler to handle incoming Writable NsMessages
+        /// </summary>
+        private class NetworkMessageHandler : IObserver<WritableNsMessage<WritableString>>
+        {
+            [Inject]
+            public NetworkMessageHandler()
+            {
+            }
+
+            public void OnNext(WritableNsMessage<WritableString> value)
+            {
+            }
+
+            public void OnError(Exception error)
+            {
+            }
+
+            public void OnCompleted()
+            {
+            }
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.Network/NetworkService/WritableNsMessage.cs
@@ -0,0 +1,165 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Hadoop.Avro;
+using Org.Apache.REEF.Network.Group.Driver.Impl;
+using Org.Apache.REEF.Wake;
+using Org.Apache.REEF.Wake.Remote;
+
+namespace Org.Apache.REEF.Network.NetworkService
+{
+    /// <summary>
+    /// Writable Message sent between NetworkServices.</summary>
+    /// <typeparam name="T">The type of data being sent. It is assumed to be Writable</typeparam>
+    [Obsolete("Need to remove Iwritable and use IstreamingCodec. Please see Jira REEF-295 ", false)]
+    public class WritableNsMessage<T> : IWritable where T : IWritable
+    {
+        private readonly IIdentifierFactory _factory;
+        private IIdentifier _sourceId;
+        private IIdentifier _destId;
+
+        /// <summary>
+        /// Constructor to allow instantiation by reflection
+        /// </summary>
+        public WritableNsMessage(IIdentifierFactory factory)
+        {
+            _factory = factory;
+        }
+        
+        /// <summary>
+        /// Create a new Writable NsMessage with no data.
+        /// </summary>
+        /// <param name="sourceId">The identifier of the sender</param>
+        /// <param name="destId">The identifier of the receiver</param>
+        public WritableNsMessage(IIdentifier sourceId, IIdentifier destId)
+        {
+            _sourceId = sourceId;
+            _destId = destId;
+            Data = new List<T>();
+        }
+
+        /// <summary>
+        /// Create a new Writable NsMessage with data.
+        /// </summary>
+        /// <param name="sourceId">The identifier of the sender</param>
+        /// <param name="destId">The identifier of the receiver</param>
+        /// <param name="message">The message to send</param>
+        public WritableNsMessage(IIdentifier sourceId, IIdentifier destId, T message)
+        {
+            _sourceId = sourceId;
+            _destId = destId;
+            Data = new List<T> {message};
+        }
+
+        /// <summary>
+        /// A list of data being sent in the message.
+        /// </summary>
+        public List<T> Data { get; set; }
+
+        /// <summary>
+        /// Read the class fields.
+        /// </summary>
+        /// <param name="reader">The reader from which to read </param>
+        public void Read(IDataReader reader)
+        {
+            _sourceId = _factory.Create(reader.ReadString());
+            _destId = _factory.Create(reader.ReadString());
+            int messageCount = reader.ReadInt32();
+
+            Data = new List<T>();
+
+            for (int index = 0; index < messageCount; index++)
+            {
+                Data.Add(Activator.CreateInstance<T>());
+
+                if (Data[index] == null)
+                {
+                    throw new Exception("T type instance cannot be created from the stream data in Network Service Message");
+                }
+
+                Data[index].Read(reader);
+            }
+        }
+
+        /// <summary>
+        /// Writes the class fields.
+        /// </summary>
+        /// <param name="writer">The writer to which to write</param>
+        public void Write(IDataWriter writer)
+        {
+            writer.WriteString(_sourceId.ToString());
+            writer.WriteString(_destId.ToString());
+            writer.WriteInt32(Data.Count);
+
+            foreach (var data in Data)
+            {
+                data.Write(writer);
+            }
+        }
+
+        /// <summary>
+        /// Read the class fields.
+        /// </summary>
+        /// <param name="reader">The reader from which to read </param>
+        /// <param name="token">The cancellation token</param>
+        public async Task ReadAsync(IDataReader reader, CancellationToken token)
+        {
+            _sourceId = _factory.Create(await reader.ReadStringAsync(token));
+            _destId = _factory.Create(await reader.ReadStringAsync(token));
+            int messageCount = await reader.ReadInt32Async(token);
+
+            Data = new List<T>();
+
+            for (int index = 0; index < messageCount; index++)
+            {
+                Data.Add(Activator.CreateInstance<T>());
+
+                if (Data[index] == null)
+                {
+                    throw new Exception("T type instance cannot be created from the stream data in Network Service Message");
+                }
+
+                await Data[index].ReadAsync(reader, token);
+            }
+        }
+
+        /// <summary>
+        /// Writes the class fields.
+        /// </summary>
+        /// <param name="writer">The writer to which to write</param>
+        /// <param name="token">The cancellation token</param>
+        public async Task WriteAsync(IDataWriter writer, CancellationToken token)
+        {
+            await writer.WriteStringAsync(_sourceId.ToString(), token);
+            await writer.WriteStringAsync(_destId.ToString(), token);
+            await writer.WriteInt32Async(Data.Count, token);
+
+            foreach (var data in Data)
+            {
+                data.Write(writer);
+            }
+        }
+    }
+}
