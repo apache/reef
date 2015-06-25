15dd786988d9cfb5d456a0c67fb3d92ab9e02661
Kijung Shin <kijung.shin89@gmail.com>
2015-06-25 17:02:51 +0900

    [REEF-393]: OutputService for writing files to filesystem
    This addressed the issue by
      * implementing OutputService.
      * implementing an example application using OutputService.
    
    JIRA:
      [REEF-393](https://issues.apache.org/jira/browse/REEF-393)
    
    Pull Request:
      This closes #242

---
+++ lang/java/reef-examples/src/main/java/org/apache/reef/examples/data/output/OutputServiceTask.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.reef.examples.data.output;
+
+import org.apache.reef.io.data.output.OutputStreamProvider;
+import org.apache.reef.task.Task;
+
+import javax.inject.Inject;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * The Task code for the output service demo app.
+ * This task receives an output stream from the output service
+ * and writes "Hello REEF!" on it.
+ */
+public final class OutputServiceTask implements Task {
+
+  /**
+   * Output stream provider object through which tasks create output streams.
+   */
+  private final OutputStreamProvider outputStreamProvider;
+
+  /**
+   * Task constructor - instantiated via TANG.
+   *
+   * @param outputStreamProvider Output stream provider object through which tasks create output streams.
+   */
+  @Inject
+  public OutputServiceTask(final OutputStreamProvider outputStreamProvider) {
+    this.outputStreamProvider = outputStreamProvider;
+  }
+
+  /**
+   * Receives an output stream from the output service and writes "Hello REEF!" on it.
+   *
+   * @param memento the memento objected passed down by the driver.
+   * @return null
+   * @throws java.io.IOException
+   */
+  @Override
+  public byte[] call(final byte[] memento) throws IOException {
+    try (final DataOutputStream outputStream = outputStreamProvider.create("hello")) {
+      outputStream.writeBytes("Hello REEF!");
+    }
+    return null;
+  }
+}
---
+++ lang/java/reef-io/src/main/java/org/apache/reef/io/data/output/TaskOutputStreamProviderLocal.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.reef.io.data.output;
+
+import org.apache.reef.tang.annotations.Parameter;
+
+import javax.inject.Inject;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Implementation of {@link TaskOutputStreamProvider}.
+ * It provides FileOutputStreams on the local file system.
+ */
+public final class TaskOutputStreamProviderLocal extends TaskOutputStreamProvider {
+
+  /**
+   * Path of the output directory on the local disk to write outputs.
+   */
+  private final String outputPath;
+
+  /**
+   * Constructor - instantiated via TANG.
+   *
+   * @param outputPath path of the output directory on the local disk to write outputs.
+   */
+  @Inject
+  private TaskOutputStreamProviderLocal(
+      @Parameter(TaskOutputService.OutputPath.class) final String outputPath) {
+    this.outputPath = outputPath;
+  }
+
+  /**
+   * create a file output stream using the given name.
+   * The path of the file on the local file system is 'outputPath/name/taskId'.
+   *
+   * @param name name of the created output stream
+   *             It is used as the name of the directory if the created output stream is a file output stream
+   * @return OutputStream to a file on local file system. The path of the file is 'outputPath/name/taskId'
+   * @throws java.io.IOException
+   */
+  @Override
+  public DataOutputStream create(final String name) throws IOException {
+    final String directoryPath = outputPath + File.separator + name;
+    final File directory = new File(directoryPath);
+
+    synchronized (TaskOutputStreamProviderLocal.class) {
+      if (!directory.exists()) {
+        directory.mkdirs();
+      }
+    }
+
+    final File file = new File(directoryPath + File.separator + getTaskId());
+    return new DataOutputStream(new FileOutputStream(file));
+  }
+
+  @Override
+  public void close() throws IOException {
+  }
+}
