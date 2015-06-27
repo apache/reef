2656f91ecf710e96e69b475213e67bf4d5ef1cb3
Julia Wang <jwang98052@yahoo.com>
2015-06-26 18:36:56 -0700

    [REEF-396]  Implement ClassHierarchySerializer using Avro on the .Net side This PR includes
    *AvroClassHierarchy
    *AvroClassHierarchySerializer
    *Avro data contract for ClassHierarchy
    *Test cases for the conversion between AvroNode and ClassHierarchy
    
    There are some issues in Avro library for serialization. That portion will be tested after the bug from Avro team is resolved.
    
    JIRA: REEF-396(https://issues.apache.org/jira/browse/REEF-396)
    
    This closes #
    
    Author: Julia Wang  Email: jwang98052@yahoo.com

---
+++ lang/cs/Org.Apache.REEF.Tang/Implementations/ClassHierarchy/AvroClassHierarchy.cs
@@ -0,0 +1,291 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Org.Apache.REEF.Tang.Exceptions;
+using Org.Apache.REEF.Tang.Implementations.ClassHierarchy.AvroDataContract;
+using Org.Apache.REEF.Tang.Interface;
+using Org.Apache.REEF.Tang.Types;
+using Org.Apache.REEF.Utilities.Logging;
+
+namespace Org.Apache.REEF.Tang.Implementations.ClassHierarchy
+{
+    /// <summary>
+    /// Deserialized Class Hierarchy 
+    /// </summary>
+    internal class AvroClassHierarchy : IClassHierarchy
+    {
+        private static readonly Logger LOGGER = Logger.GetLogger(typeof(AvroClassHierarchy));
+
+        private readonly IPackageNode _rootNode;
+        private readonly IDictionary<string, INode> _lookupTable = new Dictionary<string, INode>();
+
+        /// <summary>
+        /// create a AvroClassHierarchy with empty nodes and lookup table. It can be used to merge other class hierarchy to it
+        /// </summary>
+        internal AvroClassHierarchy()
+        {
+            _rootNode = new PackageNodeImpl();
+        }
+
+        /// <summary>
+        /// Convert AvroNode into AvroClassHierarchy object
+        /// </summary>
+        /// <param name="root"></param>
+        internal AvroClassHierarchy(AvroNode root)
+        {
+            _rootNode = new PackageNodeImpl();
+            if (root.packageNode == null)
+            {
+                Utilities.Diagnostics.Exceptions.Throw(new ArgumentException("Expected a package node.  Got: " + root), LOGGER); 
+            }
+
+            foreach (AvroNode child in root.children)
+            {
+                ParseSubHierarchy(_rootNode, child);
+            }
+            
+            BuildHashTable(_rootNode);
+
+            foreach (AvroNode child in root.children)
+            {
+                WireUpInheritanceRelationships(child);
+            }
+        }
+
+        private void ParseSubHierarchy(INode parent, AvroNode n)
+        {
+            INode parsed = null;
+            if (n.packageNode != null && !n.packageNode.Equals(""))
+            {
+                parsed = new PackageNodeImpl(parent, n.name, n.fullName);
+            }
+            else if (n.namedParameterNode != null && !n.namedParameterNode.Equals(""))
+            {
+                AvroNamedParameterNode np = (AvroNamedParameterNode)n.namedParameterNode;
+                parsed = new NamedParameterNodeImpl(parent, n.name,
+                    n.fullName, np.fullArgClassName, np.simpleArgClassName,
+                    np.isSet, np.isList, np.documentation, np.shortName,
+                    np.instanceDefault.ToArray());
+            }
+            else if (n.classNode != null && !n.classNode.Equals(""))
+            {
+                AvroClassNode cn = (AvroClassNode)n.classNode;
+                IList<IConstructorDef> injectableConstructors = new List<IConstructorDef>();
+                IList<IConstructorDef> allConstructors = new List<IConstructorDef>();
+
+                foreach (AvroConstructorDef injectable in cn.injectableConstructors)
+                {
+                    IConstructorDef def = ParseConstructorDef(injectable, true);
+                    injectableConstructors.Add(def);
+                    allConstructors.Add(def);
+                }
+                foreach (AvroConstructorDef other in cn.otherConstructors)
+                {
+                    IConstructorDef def = ParseConstructorDef(other, false);
+                    allConstructors.Add(def);
+                }
+
+                parsed = new ClassNodeImpl(parent, n.name, n.fullName,
+                cn.isUnit, cn.isInjectionCandidate,
+                cn.isExternalConstructor, injectableConstructors,
+                allConstructors, cn.defaultImplementation);
+            }
+            else
+            {
+                Utilities.Diagnostics.Exceptions.Throw(new IllegalStateException("Bad protocol buffer: got abstract node" + n), LOGGER);
+            }
+
+            foreach (AvroNode child in n.children)
+            {
+                ParseSubHierarchy(parsed, child);
+            }
+        }
+
+        private void WireUpInheritanceRelationships(AvroNode n)
+        {
+            if (n.classNode != null && !n.classNode.Equals(""))
+            {
+                AvroClassNode cn = (AvroClassNode)n.classNode;
+                IClassNode iface = null;
+
+                try
+                {
+                    iface = (IClassNode)GetNode(n.fullName);
+                }
+                catch (NameResolutionException e)
+                {
+                    Utilities.Diagnostics.Exceptions.Caught(e, Level.Error, LOGGER);
+                    var ex = new IllegalStateException("When reading protocol buffer node "
+                        + n.fullName + " does not exist.  Full record is " + n, e);
+                    Utilities.Diagnostics.Exceptions.Throw(ex, LOGGER);
+                }
+
+                foreach (string impl in cn.implFullNames)
+                {
+                    try
+                    {
+                        iface.PutImpl((IClassNode)GetNode(impl));
+                    }
+                    catch (NameResolutionException e)
+                    {
+                        Utilities.Diagnostics.Exceptions.Caught(e, Level.Error, LOGGER);
+                        var ex = new IllegalStateException("When reading protocol buffer node "
+                            + n + " refers to non-existent implementation:" + impl);
+                        Utilities.Diagnostics.Exceptions.Throw(ex, LOGGER);
+
+                    }
+                    catch (InvalidCastException e)
+                    {
+                        Utilities.Diagnostics.Exceptions.Caught(e, Level.Error, LOGGER);
+                        try
+                        {
+                            var ex = new IllegalStateException(
+                                "When reading protocol buffer node " + n
+                                + " found implementation" + GetNode(impl)
+                                + " which is not a ClassNode!");
+                            Utilities.Diagnostics.Exceptions.Throw(ex, LOGGER);
+                        }
+                        catch (NameResolutionException ne)
+                        {
+                            Utilities.Diagnostics.Exceptions.Caught(ne, Level.Error, LOGGER);
+                            var ex = new IllegalStateException(
+                                "Got 'cant happen' exception when producing error message for " + e);
+                            Utilities.Diagnostics.Exceptions.Throw(ex, LOGGER);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Build hashtable to index the node
+        /// </summary>
+        /// <param name="n"></param>
+        public void BuildHashTable(INode n)
+        {
+            foreach (INode child in n.GetChildren())
+            {
+                _lookupTable.Add(child.GetFullName(), child);
+                BuildHashTable(child);
+            }
+        }
+
+        /// <summary>
+        /// Get a Node from the class hierarchy 
+        /// </summary>
+        /// <param name="fullName"></param>
+        /// <returns></returns>
+        public INode GetNode(string fullName)
+        {
+            INode ret;
+            _lookupTable.TryGetValue(fullName, out ret);
+            if (ret == null)
+            {
+                var ex = new NameResolutionException(fullName, "Cannot resolve the name from the class hierarchy during deserialization: " + fullName);
+                Utilities.Diagnostics.Exceptions.Throw(ex, LOGGER);
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// return root node
+        /// </summary>
+        /// <returns></returns>
+        public INode GetNamespace()
+        {
+            return _rootNode;
+        }
+
+        /// <summary>
+        /// Check if impl is an implementation of inter
+        /// </summary>
+        /// <param name="inter"></param>
+        /// <param name="impl"></param>
+        /// <returns></returns>
+        public bool IsImplementation(IClassNode inter, IClassNode impl)
+        {
+            return impl.IsImplementationOf(inter);
+        }
+
+        /// <summary>
+        /// Merge two Class Hierarchy 
+        /// </summary>
+        /// <param name="ch"></param>
+        /// <returns></returns>
+        public IClassHierarchy Merge(IClassHierarchy ch)
+        {
+            if (this == ch)
+            {
+                return this;
+            }
+
+            if (!(ch is AvroClassHierarchy))
+            {
+                Utilities.Diagnostics.Exceptions.Throw(new NotSupportedException(
+                                                            "Cannot merge ExternalClassHierarchies yet!"), LOGGER);
+            }
+
+            AvroClassHierarchy ach = (AvroClassHierarchy)ch;
+            foreach (var pair in ach._lookupTable)
+            {
+                if (!this._lookupTable.ContainsKey(pair.Key))
+                {
+                    this._lookupTable.Add(pair);
+                }
+            }
+
+            foreach (INode n in ch.GetNamespace().GetChildren())
+            {
+                if (!_rootNode.Contains(n.GetFullName()))
+                {
+                    if (n is INamedParameterNode)
+                    {
+                        INamedParameterNode np = (INamedParameterNode)n;
+                        new NamedParameterNodeImpl(this._rootNode, np.GetName(),
+                                                   np.GetFullName(), np.GetFullArgName(), np.GetSimpleArgName(),
+                                                   np.IsSet(), np.IsList(), np.GetDocumentation(), np.GetShortName(),
+                                                   np.GetDefaultInstanceAsStrings().ToArray());
+                    }
+                    else if (n is IClassNode)
+                    {
+                        IClassNode cn = (IClassNode)n;
+                        new ClassNodeImpl(_rootNode, cn.GetName(), cn.GetFullName(),
+                                          cn.IsUnit(), cn.IsInjectionCandidate(),
+                                          cn.IsExternalConstructor(), cn.GetInjectableConstructors(),
+                                          cn.GetAllConstructors(), cn.GetDefaultImplementation());
+                    }
+                }
+            }
+            return this;
+        }
+
+        private IConstructorDef ParseConstructorDef(AvroConstructorDef def, bool isInjectable)
+        {
+            IList<IConstructorArg> args = new List<IConstructorArg>();
+            foreach (AvroConstructorArg arg in def.constructorArgs)
+            {
+                args.Add(new ConstructorArgImpl(arg.fullArgClassName, arg.namedParameterName, arg.isInjectionFuture));
+            }
+            return new ConstructorDefImpl(def.fullClassName, args.ToArray(), isInjectable);
+        }
+    }
+}
