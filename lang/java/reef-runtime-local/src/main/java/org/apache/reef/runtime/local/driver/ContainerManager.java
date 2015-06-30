1fc0f91d0b6aad07b80063d7730429a691d53b6a
Ignacio Cano <nachoacano@gmail.com>
2015-06-30 13:32:59 -0700

    [REEF-416] Create API to specify racks in EvaluatorRequests
    With this commit, evaluators can now be asked to run in different nodes or racks.
    Nodes should be node names. Racks can be fully qualified names. Also the star wildcard can
    be used at the end. For example, in order to ask for evaluators in any rack in datacenter 2, you can do /dc2/*
    It includes a new EvaluatorRequest constructor, and it deprecates the descriptor interface.
    Updated UTs (which also helped in fixing a bug)
    
    JIRA: [REEF-416](https://issues.apache.org/jira/browse/REEF-416)
    
    This Closes

--- lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/driver/ContainerManager.java
+++ lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/driver/ContainerManager.java
@@ -161,7 +161,7 @@ final class ContainerManager implements AutoCloseable {
       }
     });
 
-    init(rackNames);
+    init(availableRacks);
 
     LOG.log(Level.FINE, "Initialized Container Manager with {0} containers", capacity);
   }
@@ -202,7 +202,7 @@ final class ContainerManager implements AutoCloseable {
     return normalizedRackNames;
   }
 
-  private void init(final Set<String> rackNames) {
+  private void init(final Collection<String> rackNames) {
     // evenly distribute the containers among the racks
     // if rack names are not specified, the default rack will be used, so the denominator will always be > 0
     final int capacityPerRack = capacity / rackNames.size();
