78d5efa4212aae2f0876446951587494e8c80ffc
Markus Weimer <weimer@apache.org>
2015-07-02 11:17:53 -0700

    Allow the tests project access to internal APIs

--- lang/cs/Org.Apache.REEF.IMRU/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.REEF.IMRU/Properties/AssemblyInfo.cs
@@ -17,6 +17,7 @@
  * under the License.
  */
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Org.Apache.REEF.IMRU")]
@@ -31,3 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("138891df-d331-4a5e-8514-775611c06f6c")]
 [assembly: AssemblyVersion("0.12.0.0")]
 [assembly: AssemblyFileVersion("0.12.0.0")]
+// Allow the tests project access to `internal` APIs
+[assembly: InternalsVisibleTo("Org.Apache.REEF.IMRU.Tests")]
