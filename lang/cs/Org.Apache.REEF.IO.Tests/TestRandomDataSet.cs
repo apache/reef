28e5ec030c3fab0118224a6ad147ffa05de1ead8
Markus Weimer <weimer@apache.org>
2015-07-07 16:16:07 -0700

    [REEF-450] Introduce Org.Apache.REEF.IO.PartitionedData
    This change adds `Org.Apache.REEF.IO.PartitionedData` which is a minimal
    API to represent partitioned data sets.
    
    This change also introduces `Org.Apache.REEF.IO.PartitionedData.Random`,
    which is an implementation of `Org.Apache.REEF.IO.PartitionedData` for a
    given number of partitions with a fixed number of random bytes per
    partition.
    
    Lastly, this introduces `Org.Apache.REEF.IO.Tests` as the test project
    for `Org.Apache.REEF.IO`. Unit Tests are included for
    `Org.Apache.REEF.IO.PartitionedData.Random`.
    
    JIRA:
      [REEF-450](https://issues.apache.org/jira/browse/REEF-450)
    
    Pull Request:
      This closes #285
    
    Author:    Markus Weimer <weimer@apache.org>

---
+++ lang/cs/Org.Apache.REEF.IO.Tests/TestRandomDataSet.cs
@@ -0,0 +1,108 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Org.Apache.REEF.IO.PartitionedData;
+using Org.Apache.REEF.IO.PartitionedData.Random;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+
+namespace Org.Apache.REEF.IO.Tests
+{
+    /// <summary>
+    /// Tests for Org.Apache.REEF.IO.PartitionedData.Random.
+    /// </summary>
+    [TestClass]
+    public sealed class TestRandomDataSet
+    {
+        /// <summary>
+        /// The number of partitions the tested dataset will have.
+        /// </summary>
+        private const int NumberOfPartitions = 3;
+
+        /// <summary>
+        /// The number of doubles the stream of each partition will return.
+        /// </summary>
+        private const int NumberOfDoublesPerPartition = 7;
+
+        /// <summary>
+        /// Number of bytes per double in the stream.
+        /// </summary>
+        private const int BytesPerDouble = 8;
+
+        /// <summary>
+        /// The number of bytes we expect each partition's stream to return.
+        /// </summary>
+        private const int ExpectedNumberOfBytesPerPartition = NumberOfDoublesPerPartition*BytesPerDouble;
+
+        /// <summary>
+        /// Test for the driver side APIs of RandomDataSet.
+        /// </summary>
+        [TestMethod]
+        public void TestDriverSide()
+        {
+            var dataSet = MakeRandomDataSet();
+            Assert.IsNotNull(dataSet);
+            Assert.IsNotNull(dataSet.Id);
+            Assert.IsNotNull(dataSet.GetEnumerator());
+            Assert.AreEqual(NumberOfPartitions, dataSet.Count);
+            Assert.IsNull(dataSet.GetPartitionDescriptorForId("THIS_IS_AN_ID_THAT_CANNOT_BE_IN_THE_DATASET"));
+        }
+
+        /// <summary>
+        /// Tests the Evaluator side of the IPartionedDataSet.
+        /// </summary>
+        /// <remarks>
+        /// This instantiates each IPartition using the IConfiguration provided by the IPartitionDescriptor.
+        /// </remarks>
+        [TestMethod]
+        public void TestEvaluatorSide()
+        {
+            var dataSet = MakeRandomDataSet();
+            foreach (var partitionDescriptor in dataSet)
+            {
+                var partition =
+                    TangFactory.GetTang()
+                        .NewInjector(partitionDescriptor.GetPartitionConfiguration())
+                        .GetInstance<IPartition>();
+                Assert.IsNotNull(partition);
+                Assert.IsNotNull(partition.Id);
+
+                using (var partitionStream = partition.Open())
+                {
+                    Assert.IsNotNull(partitionStream);
+                    Assert.IsTrue(partitionStream.CanRead);
+                    Assert.IsFalse(partitionStream.CanWrite);
+                    Assert.AreEqual(ExpectedNumberOfBytesPerPartition, partitionStream.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Make a DataSet instance using the RandomDataConfiguration.
+        /// </summary>
+        /// <returns></returns>
+        private IPartitionedDataSet MakeRandomDataSet()
+        {
+            return TangFactory.GetTang().NewInjector(RandomDataConfiguration.ConfigurationModule
+                .Set(RandomDataConfiguration.NumberOfDoublesPerPartition, NumberOfDoublesPerPartition.ToString())
+                .Set(RandomDataConfiguration.NumberOfPartitions, NumberOfPartitions.ToString())
+                .Build()).GetInstance<IPartitionedDataSet>();
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.IO/PartitionedData/Random/RandomDataSet.cs
@@ -0,0 +1,79 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using Org.Apache.REEF.IO.PartitionedData.Random.Parameters;
+using Org.Apache.REEF.Tang.Annotations;
+
+namespace Org.Apache.REEF.IO.PartitionedData.Random
+{
+    /// <summary>
+    /// An implementation of IPartitionedDataSet that produces a given number of partitions containing a given number of random
+    /// doubles.
+    /// </summary>
+    internal sealed class RandomDataSet : IPartitionedDataSet
+    {
+        private readonly Dictionary<string, IPartitionDescriptor> _partitions;
+
+        [Inject]
+        private RandomDataSet([Parameter(typeof(NumberOfPartitions))] int numberOfPartitions,
+            [Parameter(typeof(NumberOfDoublesPerPartition))] int numberOfDoublesPerPartition)
+        {
+            _partitions = new Dictionary<string, IPartitionDescriptor>(numberOfPartitions);
+            for (var i = 0; i < numberOfPartitions; ++i)
+            {
+                var id = "RandomPartition-" + i;
+                _partitions[id] = new RandomPartitionDescriptor(id, numberOfDoublesPerPartition);
+            }
+        }
+
+        public IEnumerator<IPartitionDescriptor> GetEnumerator()
+        {
+            return _partitions.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _partitions.Values.GetEnumerator();
+        }
+
+        public int Count
+        {
+            get { return _partitions.Count; }
+        }
+
+        public string Id
+        {
+            get { return "RandomDataSet"; }
+        }
+
+        public IPartitionDescriptor GetPartitionDescriptorForId(string partitionId)
+        {
+            try
+            {
+                return _partitions[partitionId];
+            }
+            catch (KeyNotFoundException)
+            {
+                return null;
+            }
+        }
+    }
+}
