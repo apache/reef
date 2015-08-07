deedbe3cdb74d387bafc7de3972da17ac43193d0
Dhruv <dhruv.mahajan@gmail.com>
2015-08-06 22:32:36 -0700

    [REEF-556] Implement MapTaskHost, UpdateTaskHost and ConfigurationManager required by IMRUDriver
    This addressed the issue by implementing following classes
    
      * `MapTaskHost` - Runs mapper iterations: receives input using group
        communication, does map update, sends results back to update
        function using group communications
    
      * `UpdateTaskHost` - Runs update iterations: sends input to mappers
        using group communications, receive aggregated output message from
        mappers and then apply update function.
    
      * `ConfigurationManager` - Takes serialized configuration specified by
        client, deserializes it to get Map, update, codecs configuration
        etc. from user. This class will be passed to IMRUDriver constructor.
    
    JIRA:
      [REEF-556](https://issues.apache.org/jira/browse/REEF-556)
    
    Pull Request:
      This closes #349

---
+++ lang/cs/Org.Apache.REEF.IMRU/OnREEF/IMRUTasks/MapTaskHost.cs
@@ -0,0 +1,81 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Common.Tasks;
+using Org.Apache.REEF.IMRU.API;
+using Org.Apache.REEF.IMRU.OnREEF.Driver;
+using Org.Apache.REEF.IMRU.OnREEF.MapInputWithControlMessage;
+using Org.Apache.REEF.Network.Group.Operators;
+using Org.Apache.REEF.Network.Group.Task;
+using Org.Apache.REEF.Tang.Annotations;
+using Org.Apache.REEF.Utilities.Logging;
+
+namespace Org.Apache.REEF.IMRU.OnREEF.IMRUTasks
+{
+    /// <summary>
+    /// Hosts the IMRU MapTask in a REEF Task.
+    /// </summary>
+    /// <typeparam name="TMapInput">Map input</typeparam>
+    /// <typeparam name="TMapOutput">Map output</typeparam>
+    internal sealed class MapTaskHost<TMapInput, TMapOutput> : ITask
+    {
+        private static readonly Logger Logger = Logger.GetLogger(typeof(MapTaskHost<TMapInput, TMapOutput>));
+
+        private readonly IBroadcastReceiver<MapInputWithControlMessage<TMapInput>> _dataAndMessageReceiver;
+        private readonly IReduceSender<TMapOutput> _dataReducer;
+        private readonly IMapFunction<TMapInput, TMapOutput> _mapTask;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="mapTask">The MapTask hosted in this REEF Task.</param>
+        /// <param name="groupCommunicationsClient">Used to setup the communications.</param>
+        [Inject]
+        private MapTaskHost(IMapFunction<TMapInput, TMapOutput> mapTask, IGroupCommClient groupCommunicationsClient)
+        {
+            _mapTask = mapTask;
+            var cg = groupCommunicationsClient.GetCommunicationGroup(IMRUConstants.CommunicationGroupName);
+            _dataAndMessageReceiver = cg.GetBroadcastReceiver<MapInputWithControlMessage<TMapInput>>(IMRUConstants.BroadcastOperatorName);
+            _dataReducer = cg.GetReduceSender<TMapOutput>(IMRUConstants.ReduceOperatorName);
+        }
+
+        /// <summary>
+        /// Performs IMRU iterations on map side
+        /// </summary>
+        /// <param name="memento"></param>
+        /// <returns></returns>
+        public byte[] Call(byte[] memento)
+        {
+            MapInputWithControlMessage<TMapInput> mapInput = _dataAndMessageReceiver.Receive();
+
+            while (mapInput.ControlMessage == MapControlMessage.AnotherRound)
+            {
+                var result = _mapTask.Map(mapInput.Message);
+                _dataReducer.Send(result);
+                mapInput = _dataAndMessageReceiver.Receive();
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Dispose function 
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.IMRU/OnREEF/IMRUTasks/UpdateTaskHost.cs
@@ -0,0 +1,98 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Common.Tasks;
+using Org.Apache.REEF.IMRU.API;
+using Org.Apache.REEF.IMRU.OnREEF.Driver;
+using Org.Apache.REEF.IMRU.OnREEF.MapInputWithControlMessage;
+using Org.Apache.REEF.Network.Group.Operators;
+using Org.Apache.REEF.Network.Group.Task;
+using Org.Apache.REEF.Tang.Annotations;
+using Org.Apache.REEF.Utilities.Logging;
+
+namespace Org.Apache.REEF.IMRU.OnREEF.IMRUTasks
+{
+    /// <summary>
+    /// Hosts the IMRU UpdateTask in a REEF task
+    /// </summary>
+    /// <typeparam name="TMapInput">Map input</typeparam>
+    /// <typeparam name="TMapOutput">Map output</typeparam>
+    /// <typeparam name="TResult">Final result</typeparam>
+    public sealed class UpdateTaskHost<TMapInput, TMapOutput, TResult> : ITask
+    {
+        private static readonly Logger Logger = Logger.GetLogger(typeof(UpdateTaskHost<TMapInput, TMapOutput, TResult>));
+
+        private readonly IReduceReceiver<TMapOutput> _dataReceiver;
+        private readonly IBroadcastSender<MapInputWithControlMessage<TMapInput>> _dataAndControlMessageSender;
+        private readonly IUpdateFunction<TMapInput, TMapOutput, TResult> _updateTask;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="updateTask">The UpdateTask hosted in this REEF Task.</param>
+        /// <param name="groupCommunicationsClient">Used to setup the communications.</param>
+        [Inject]
+        private UpdateTaskHost(
+            IUpdateFunction<TMapInput, TMapOutput, TResult> updateTask,
+            IGroupCommClient groupCommunicationsClient)
+        {
+            _updateTask = updateTask;
+            var cg = groupCommunicationsClient.GetCommunicationGroup(IMRUConstants.CommunicationGroupName);
+            _dataAndControlMessageSender = cg.GetBroadcastSender<MapInputWithControlMessage<TMapInput>>(IMRUConstants.BroadcastOperatorName);
+            _dataReceiver = cg.GetReduceReceiver<TMapOutput>(IMRUConstants.ReduceOperatorName);
+        }
+
+        /// <summary>
+        /// Performs IMRU iterations on update side
+        /// </summary>
+        /// <param name="memento"></param>
+        /// <returns></returns>
+        public byte[] Call(byte[] memento)
+        {
+            var updateResult = _updateTask.Initialize();
+            MapInputWithControlMessage<TMapInput> message =
+                new MapInputWithControlMessage<TMapInput>(MapControlMessage.AnotherRound);
+
+            while (updateResult.HasMapInput)
+            {
+                message.Message = updateResult.MapInput;
+                _dataAndControlMessageSender.Send(message);
+                updateResult = _updateTask.Update(_dataReceiver.Reduce());
+                if (updateResult.HasResult)
+                {
+                    // TODO[REEF-576]: Emit output somewhere.
+                }
+            }
+
+            message.ControlMessage = MapControlMessage.Stop;
+            _dataAndControlMessageSender.Send(message);
+
+            if (updateResult.HasResult)
+            {
+                // TODO[REEF-576]: Emit output somewhere.
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Dispose function
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+}
