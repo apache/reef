cc372c1275abd9c5750b275995f2094507f81e52
Markus Weimer <weimer@apache.org>
2015-08-17 11:35:29 -0700

    Updated HDFSCommandRunner to use a for() loop. Also, it now creates the `Process` class a bit more safely. Still, the process needs to be `.Start()`ed before the redirect can happen...

--- lang/cs/Org.Apache.REEF.IO/FileSystem/Hadoop/HDFSCommandRunner.cs
+++ lang/cs/Org.Apache.REEF.IO/FileSystem/Hadoop/HDFSCommandRunner.cs
@@ -81,7 +81,8 @@ namespace Org.Apache.REEF.IO.FileSystem.Hadoop
                 else
                 {
                     // The user provided folder does not exist. Try the environment variable.
-                    Logger.Log(Level.Warning, "The provided hadoop home folder {0} doesn't exist, trying environment variable {1} instead",
+                    Logger.Log(Level.Warning,
+                        "The provided hadoop home folder {0} doesn't exist, trying environment variable {1} instead",
                         hadoopHome, HadoopHomeEnvironmentVariableName);
                     _hdfsCommandPath = GetFullPathToHdfsCommandBasedOnEnvironmentVariable();
                 }
@@ -101,97 +102,29 @@ namespace Org.Apache.REEF.IO.FileSystem.Hadoop
 
         internal CommandResult Run(string hdfsCommandLineArguments)
         {
-            var outList = new List<string>();
-            var errList = new List<string>();
-            var tries = _numberOfRetries;
-            var origTries = _numberOfRetries;
-            Process process;
-            do
+            var processStartInfo = new ProcessStartInfo
             {
-                outList.Clear();
-                Logger.Log(Level.Info, "Trial {0} Timeout in {1} secs Executing: {2}", origTries - tries,
-                    _timeOutInMilliSeconds/1000,
-                    _hdfsCommandPath + " " + hdfsCommandLineArguments);
-                var startInfo = new ProcessStartInfo
-                {
-                    FileName = _hdfsCommandPath,
-                    Arguments = hdfsCommandLineArguments,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
-                };
-
-                process = Process.Start(startInfo);
-
-                if (process == null)
-                {
-                    throw new Exception(string.Format("HDFS cmd {0} {1} process didn't start.", _hdfsCommandPath,
-                        hdfsCommandLineArguments));
-                }
-
-                process.OutputDataReceived += delegate(object sender, DataReceivedEventArgs e)
-                {
-                    if (!string.IsNullOrWhiteSpace(e.Data))
-                    {
-                        outList.Add(e.Data.Trim());
-                    }
-                };
-
-                process.ErrorDataReceived += delegate(object sender, DataReceivedEventArgs e)
-                {
-                    if (!string.IsNullOrWhiteSpace(e.Data))
-                    {
-                        errList.Add(e.Data.Trim());
-                    }
-                };
-                process.BeginErrorReadLine();
-                process.BeginOutputReadLine();
-
-                if (process.WaitForExit(_timeOutInMilliSeconds))
-                {
-                    break;
-                }
-
-                Logger.Log(Level.Warning, "Process has not exited after a timeout of {0} secs. So killing it",
-                    _timeOutInMilliSeconds);
-                process.Kill();
-            } while (--tries > 0);
-
-            if (!process.HasExited)
+                FileName = _hdfsCommandPath,
+                Arguments = hdfsCommandLineArguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+            for (var attemptNumber = 0; attemptNumber < _numberOfRetries; ++attemptNumber)
             {
-                process.Kill();
-                throw new Exception(
-                    string.Format("HDFS Cmd {0} {1} could not be executed in the specified timeout & retry settings",
-                        _hdfsCommandPath, hdfsCommandLineArguments));
-            }
-            process.WaitForExit();
-
-            #region CommandOutputLogging
-
-            if (Logger.IsLoggable(Level.Verbose))
-            {
-                using (var messageBuilder = new StringWriter())
+                var processName = String.Format("HDFS_Attempt_{0}_of_{1}", attemptNumber, _numberOfRetries);
+                var result = RunAttempt(processStartInfo, _timeOutInMilliSeconds, processName);
+                if (null != result)
                 {
-                    messageBuilder.WriteLine("OUTPUT:");
-                    messageBuilder.WriteLine("----------------------------------------");
-                    foreach (var stdOut in outList)
-                    {
-                        messageBuilder.WriteLine("Out:    " + stdOut);
-                    }
-
-                    messageBuilder.WriteLine("----------------------------------------");
-                    foreach (var stdErr in errList)
-                    {
-                        messageBuilder.WriteLine("Err:    " + stdErr);
-                    }
-                    messageBuilder.WriteLine("----------------------------------------");
-                    Logger.Log(Level.Verbose, messageBuilder.ToString());
+                    LogCommandOutput(result);
+                    return result;
                 }
             }
 
-            #endregion
-
-            return new CommandResult(outList, errList, process.ExitCode);
+            // If we reached here, we ran out of retries.
+            throw new Exception(
+                string.Format("HDFS Cmd {0} {1} could not be executed in the specified timeout & retry settings",
+                    _hdfsCommandPath, hdfsCommandLineArguments));
         }
 
         /// <summary>
@@ -218,6 +151,91 @@ namespace Org.Apache.REEF.IO.FileSystem.Hadoop
                                     " not set and no path to the hadoop installation provided.");
             }
             return GetFullPathToHdfsCommand(hadoopHomeFromEnv);
+        }
+
+        /// <summary>
+        /// Helper method to log the command result.
+        /// </summary>
+        /// <param name="result"></param>
+        private static void LogCommandOutput(CommandResult result)
+        {
+            using (var messageBuilder = new StringWriter())
+            {
+                messageBuilder.WriteLine("OUTPUT:");
+                messageBuilder.WriteLine("----------------------------------------");
+                foreach (var stdOut in result.StdOut)
+                {
+                    messageBuilder.WriteLine("Out:    " + stdOut);
+                }
+
+                messageBuilder.WriteLine("----------------------------------------");
+                foreach (var stdErr in result.StdErr)
+                {
+                    messageBuilder.WriteLine("Err:    " + stdErr);
+                }
+                messageBuilder.WriteLine("----------------------------------------");
+                Logger.Log(Level.Verbose, messageBuilder.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Attempts to run a process with a timeout.
+        /// </summary>
+        /// <returns>The result of the attempt or null in case of timeout.</returns>
+        /// <param name="processStartInfo">The process start information.</param>
+        /// <param name="timeOutInMilliSeconds">Timeout for the process.</param>
+        /// <param name="processName">A human readable name used for logging purposes.</param>
+        private static CommandResult RunAttempt(ProcessStartInfo processStartInfo, int timeOutInMilliSeconds,
+            string processName)
+        {
+            // Setup the process.
+            var outList = new List<string>();
+            var errList = new List<string>();
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardOutput = true;
+            var process = new Process
+            {
+                StartInfo = processStartInfo
+            };
+
+            process.OutputDataReceived += delegate(object sender, DataReceivedEventArgs e)
+            {
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                {
+                    outList.Add(e.Data.Trim());
+                }
+            };
+
+            process.ErrorDataReceived += delegate(object sender, DataReceivedEventArgs e)
+            {
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                {
+                    errList.Add(e.Data.Trim());
+                }
+            };
+            // Start it
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+            Logger.Log(Level.Verbose, "Waiting for {0}ms for process `{1}` to finish", timeOutInMilliSeconds,
+                processName);
+
+            // Deal with timeouts
+            process.WaitForExit(timeOutInMilliSeconds);
+
+            if (process.HasExited)
+            {
+                // The happy path: Assemble an output
+                return new CommandResult(outList, errList, process.ExitCode);
+            }
+
+            // If we didn't return above, the process timed out.
+            Logger.Log(Level.Info, "The process `{0}` took longer than {1}ms to exit. Killing it.", processName,
+                timeOutInMilliSeconds);
+            process.Kill();
+            process.WaitForExit();
+            Logger.Log(Level.Info, "Killed process `{0}`.", processName);
+            return null;
         }
     }
 }
