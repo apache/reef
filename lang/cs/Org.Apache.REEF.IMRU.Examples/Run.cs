22b6a24748ba37ce723c973f1645da9134603348
Dhruv <dhruv.mahajan@gmail.com>
2015-08-19 15:06:42 -0700

    [REEF-609] Create empty REEF.IMRU.Examples project This addressed the issue by * creating an empty project
    JIRA:
    [REEF-609](https://issues.apache.org/jira/browse/REEF-609)

---
+++ lang/cs/Org.Apache.REEF.IMRU.Examples/Run.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System;
+using System.Globalization;
+using System.Linq;
+using Org.Apache.REEF.IMRU.OnREEF;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Wake.Remote.Parameters;
+using Org.Apache.REEF.IMRU.Examples.MapperCount;
+using Org.Apache.REEF.Tang.Interface;
+
+namespace Org.Apache.REEF.IMRU.Examples
+{
+    /// <summary>
+    /// Runs IMRU for mapper count either in localruntime or on cluster.
+    /// </summary>
+    public class Run
+    {
+        private static void Main(string[] args)
+        {      
+        }
+    }
+}
