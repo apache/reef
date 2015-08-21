d92ab9885f6cd955b5f9098e6e47164fa216b6c2
Dhruv <dhruv.mahajan@gmail.com>
2015-08-21 14:19:54 -0700

    [REEF-628] A Launcher for MapperCount and BroadcastReduce examples
    This adds a launcher for MapperCount and BroadcastReduce examples with
    the option for user to specify - example to run, number of nodes,
    runtime - local or yarn, port specification and some other example
    specific parameters
    
    JIRA:
      [REEF-628](https://issues.apache.org/jira/browse/REEF-628)
    
    Pull Request:
      This closes #401

--- lang/cs/Org.Apache.REEF.IMRU.Examples/Run.cs
+++ lang/cs/Org.Apache.REEF.IMRU.Examples/Run.cs
@@ -20,10 +20,11 @@
 using System;
 using System.Globalization;
 using System.Linq;
-using Org.Apache.REEF.IMRU.OnREEF;
+using Org.Apache.REEF.IMRU.Examples.PipelinedBroadcastReduce;
 using Org.Apache.REEF.Tang.Implementations.Tang;
-using Org.Apache.REEF.Wake.Remote.Parameters;
 using Org.Apache.REEF.Tang.Interface;
+using Org.Apache.REEF.Wake.Remote.Parameters;
+using Org.Apache.REEF.Utilities.Logging;
 
 namespace Org.Apache.REEF.IMRU.Examples
 {
@@ -32,8 +33,129 @@ namespace Org.Apache.REEF.IMRU.Examples
     /// </summary>
     public class Run
     {
+        private static readonly Logger Logger = Logger.GetLogger(typeof(Run));
+
+        private static void RunMapperTest(IConfiguration tcpPortConfig, bool runOnYarn, int numNodes)
+        {
+            IInjector injector;
+
+            if (!runOnYarn)
+            {
+                injector =
+                    TangFactory.GetTang()
+                        .NewInjector(OnREEFIMRURunTimeConfiguration<int, int, int>.GetLocalIMRUConfiguration(numNodes), tcpPortConfig);
+            }
+            else
+            {
+                injector = TangFactory.GetTang()
+                    .NewInjector(OnREEFIMRURunTimeConfiguration<int, int, int>.GetYarnIMRUConfiguration(), tcpPortConfig);
+            }
+
+            var mapperCountExample = injector.GetInstance<MapperCount.MapperCount>();
+            mapperCountExample.Run(numNodes - 1);
+        }
+
+        private static void RunBroadcastReduceTest(IConfiguration tcpPortConfig, bool runOnYarn, int numNodes, string[] args)
+        {
+            int chunkSize = 2;
+            int dims = 10;
+            int iterations = 10;
+
+            if (args.Length > 0)
+            {
+                dims = Convert.ToInt32(args[0]);
+            }
+
+            if (args.Length > 1)
+            {
+                chunkSize = Convert.ToInt32(args[1]);
+            }
+
+            if (args.Length > 2)
+            {
+                iterations = Convert.ToInt32(args[2]);
+            }
+
+            IInjector injector;
+
+            if (!runOnYarn)
+            {
+                injector =
+                    TangFactory.GetTang()
+                        .NewInjector(OnREEFIMRURunTimeConfiguration<int[], int[], int[]>.GetLocalIMRUConfiguration(numNodes), tcpPortConfig);
+            }
+            else
+            {
+                injector = TangFactory.GetTang()
+                    .NewInjector(OnREEFIMRURunTimeConfiguration<int, int, int>.GetYarnIMRUConfiguration(), tcpPortConfig);
+            }
+            var broadcastReduceExample = injector.GetInstance<PipelinedBroadcastAndReduce>();
+            broadcastReduceExample.Run(numNodes - 1, chunkSize, iterations, dims);
+        }
+
         private static void Main(string[] args)
-        {      
+        {
+            Logger.Log(Level.Info, "start running client: " + DateTime.Now);
+            string methodName = "MapperCount";
+            bool runOnYarn = false;
+            int numNodes = 2;
+            int startPort = 8900;
+            int portRange = 1000;
+
+            if (args != null)
+            {
+                if (args.Length > 0)
+                {
+                    runOnYarn = bool.Parse(args[0].ToLower());
+                }
+
+                if (args.Length > 1)
+                {
+                    numNodes = int.Parse(args[1]);
+                }
+
+                if (args.Length > 2)
+                {
+                    startPort = int.Parse(args[2]);
+                }
+
+                if (args.Length > 3)
+                {
+                    portRange = int.Parse(args[3]);
+                }
+
+                if (args.Length > 4)
+                {
+                    methodName = args[4];
+                }
+            }
+
+            var tcpPortConfig = TangFactory.GetTang().NewConfigurationBuilder()
+                .BindNamedParameter(typeof(TcpPortRangeStart),
+                    startPort.ToString(CultureInfo.InvariantCulture))
+                .BindNamedParameter(typeof(TcpPortRangeCount),
+                    portRange.ToString(CultureInfo.InvariantCulture))
+                .Build();
+
+            switch (methodName.ToLower())
+            {
+                case "mappercount":
+                    Logger.Log(Level.Info, "Running Mapper count");
+                    RunMapperTest(tcpPortConfig, runOnYarn, numNodes);
+                    Logger.Log(Level.Info, "Done Running Mapper count");
+                    return;
+
+                case "broadcastandreduce":
+                    Logger.Log(Level.Info, "Running Broadcast and Reduce");
+                    RunBroadcastReduceTest(tcpPortConfig, runOnYarn, numNodes, args.Skip(5).ToArray());
+                    Logger.Log(Level.Info, "Done Running Broadcast and Reduce");
+                    return;
+
+                default:
+                    Logger.Log(Level.Info, "wrong test name");
+                    return;
+
+            }
         }
     }
 }
