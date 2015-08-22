4aeef3e9e4be2c2e60ffb735b6bdd91d040c0642
Andrew Chung <afchung90@gmail.com>
2015-08-21 23:29:05 -0700

    [REEF-632] Register containers with YarnContainerManager on restart
    This adds `YarnContainerManager.onContainersRecovered()` and calls it on
    recovering containers in YarnDriverRuntimeRestartManager.
    
    JIRA:
      [REEF-632](https://issues.apache.org/jira/browse/REEF-632)
    
    Pull Request:
      This closes #405

--- lang/java/reef-runtime-yarn/src/main/java/org/apache/reef/runtime/yarn/driver/YarnDriverRuntimeRestartManager.java
+++ lang/java/reef-runtime-yarn/src/main/java/org/apache/reef/runtime/yarn/driver/YarnDriverRuntimeRestartManager.java
@@ -55,16 +55,20 @@ public final class YarnDriverRuntimeRestartManager implements DriverRuntimeResta
   private final EvaluatorPreserver evaluatorPreserver;
   private final ApplicationMasterRegistration registration;
   private final REEFEventHandlers reefEventHandlers;
+  private final YarnContainerManager yarnContainerManager;
+
   private Set<Container> previousContainers;
 
   @Inject
   private YarnDriverRuntimeRestartManager(@Parameter(YarnEvaluatorPreserver.class)
                                           final EvaluatorPreserver evaluatorPreserver,
                                           final REEFEventHandlers reefEventHandlers,
-                                          final ApplicationMasterRegistration registration){
+                                          final ApplicationMasterRegistration registration,
+                                          final YarnContainerManager yarnContainerManager) {
     this.registration = registration;
     this.evaluatorPreserver = evaluatorPreserver;
     this.reefEventHandlers = reefEventHandlers;
+    this.yarnContainerManager = yarnContainerManager;
     this.previousContainers = null;
   }
 
@@ -132,12 +136,17 @@ public final class YarnDriverRuntimeRestartManager implements DriverRuntimeResta
    */
   private synchronized void initializeListOfPreviousContainers() {
     if (this.previousContainers == null) {
-      this.previousContainers = new HashSet<>(this.registration.getRegistration().getContainersFromPreviousAttempts());
+      final List<Container> yarnPrevContainers =
+          this.registration.getRegistration().getContainersFromPreviousAttempts();
 
       // If it's still null, create an empty list to indicate that it's not a restart.
-      if (this.previousContainers == null) {
-        this.previousContainers = new HashSet<>();
+      if (yarnPrevContainers == null) {
+        this.previousContainers = Collections.unmodifiableSet(new HashSet<Container>());
+      } else {
+        this.previousContainers = Collections.unmodifiableSet(new HashSet<>(yarnPrevContainers));
       }
+
+      yarnContainerManager.onContainersRecovered(this.previousContainers);
     }
   }
 
