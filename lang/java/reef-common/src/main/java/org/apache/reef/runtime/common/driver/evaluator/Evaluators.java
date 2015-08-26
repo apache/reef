f795b19056465d0686fbde932816b4dc29b64cdb
Andrew Chung <afchung90@gmail.com>
2015-08-26 15:32:52 -0700

    [REEF-617] Enable creation of EvaluatorManager on restarted evaluators
    This addressed the issue by
      * Adding helper functions for ``EvaluatorManagerFactory`` to create EvaluatorManagers for recovered evaluators.
      * Recover ``EvaluatorManager` on first heartbeat of an expected Evaluator on restart.
      * Fix a bug in ``DriverRestartManager`` where the boolean check is reversed.
    
    JIRA:
      [REEF-617](https://issues.apache.org/jira/browse/REEF-617)

--- lang/java/reef-common/src/main/java/org/apache/reef/runtime/common/driver/evaluator/Evaluators.java
+++ lang/java/reef-common/src/main/java/org/apache/reef/runtime/common/driver/evaluator/Evaluators.java
@@ -107,7 +107,7 @@ public final class Evaluators implements AutoCloseable {
   public synchronized void put(
       final EvaluatorManagerFactory evaluatorManagerFactory,
       final ResourceAllocationEvent evaluatorMsg) {
-    this.put(evaluatorManagerFactory.getNewEvaluatorManagerForNewlyAllocatedEvaluator(evaluatorMsg));
+    this.put(evaluatorManagerFactory.getNewEvaluatorManagerForNewEvaluator(evaluatorMsg));
   }
 
   /**
