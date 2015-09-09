95b182cb72a70890707d48c08881848fe89df3d6
Markus Weimer <weimer@apache.org>
2015-09-09 13:43:52 -0700

    Addressed review comments

--- lang/cs/Org.Apache.REEF.Driver/Evaluator/EvaluatorException.cs
+++ lang/cs/Org.Apache.REEF.Driver/Evaluator/EvaluatorException.cs
@@ -1,19 +1,21 @@
-﻿// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-// 
-//   http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 using System;
 using Org.Apache.REEF.Driver.Task;
@@ -29,43 +31,36 @@ namespace Org.Apache.REEF.Driver.Evaluator
         private readonly string _evaluatorId;
         private readonly IRunningTask _runningTask;
 
-        internal EvaluatorException(string evaluatorId)
+        internal EvaluatorException(string evaluatorId, IRunningTask runningTask = null)
         {
             _evaluatorId = evaluatorId;
-            _runningTask = null;
+            _runningTask = runningTask;
         }
 
-        internal EvaluatorException(string evaluatorId, string message, Exception cause)
+        internal EvaluatorException(string evaluatorId, string message, Exception cause, IRunningTask runningTask = null)
             : base(message, cause)
         {
             _evaluatorId = evaluatorId;
-            _runningTask = null;
+            _runningTask = runningTask;
         }
 
-        internal EvaluatorException(string evaluatorId, string message)
-            : this(evaluatorId, message, (IRunningTask) null)
-        {
-        }
-
-        internal EvaluatorException(string evaluatorId, string message, IRunningTask runningTask)
+        internal EvaluatorException(string evaluatorId, string message, IRunningTask runningTask = null)
             : base(message)
         {
             _evaluatorId = evaluatorId;
             _runningTask = runningTask;
         }
 
-        internal EvaluatorException(string evaluatorId, Exception cause)
-            : this(evaluatorId, cause, null)
-        {
-        }
-
-        internal EvaluatorException(string evaluatorId, Exception cause, IRunningTask runningTask)
+        internal EvaluatorException(string evaluatorId, Exception cause, IRunningTask runningTask = null)
             : base(string.Empty, cause)
         {
             _evaluatorId = evaluatorId;
             _runningTask = runningTask;
         }
 
+        /// <summary>
+        /// The task that was running on the Evaluator or null if none was running exists.
+        /// </summary>
         public IRunningTask RunningTask
         {
             get { return _runningTask; }
@@ -74,7 +69,6 @@ namespace Org.Apache.REEF.Driver.Evaluator
         public string Id
         {
             get { return _evaluatorId; }
-            set { }
         }
     }
 }
