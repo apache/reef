deab46bfe76d634560067c6c0c06b140992b3baf
Anupam <anupam128@gmail.com>
2015-11-13 11:57:22 -0800

    [REEF-940] Create .NET job resource uploader using IFileSystem
    JIRA:
     [REEF-940](https://issues.apache.org/jira/browse/REEF-940)

---
+++ lang/cs/Org.Apache.REEF.Client.Tests/JobResourceUploaderTests.cs
@@ -0,0 +1,130 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Org.Apache.REEF.Client.Common;
+using Org.Apache.REEF.Client.Yarn;
+using Org.Apache.REEF.Client.YARN.RestClient;
+using Org.Apache.REEF.IO.FileSystem;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Util;
+
+namespace Org.Apache.REEF.Client.Tests
+{
+    [TestClass]
+    public class JobResourceUploaderTests
+    {
+        private const string AnyDriverLocalFolderPath = @"Any\Local\Folder\Path\";
+        private const string AnyDriverResourceUploadPath = "/vol1/tmp/";
+        private const string AnyUploadedResourcePath = "/vol1/tmp/Path.zip";
+        private const string AnyHost = "host";
+        private const string AnyScheme = "hdfs://";
+        private const string AnyUploadedResourceAbsoluteUri = AnyScheme + AnyHost + AnyUploadedResourcePath;
+        private const string AnyLocalArchivePath = @"Any\Local\Archive\Path.zip";
+        private const long AnyModificationTime = 1447413621;
+        private const long AnyResourceSize = 53092;
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        [TestMethod]
+        public void JobResourceUploaderCanInstantiateWithDefaultBindings()
+        {
+            TangFactory.GetTang().NewInjector().GetInstance<JobResourceUploader>();
+        }
+
+        [TestMethod]
+        public void UploadJobResourceCreatesResourceArchive()
+        {
+            var testContext = new TestContext();
+            var jobResourceUploader = testContext.GetJobResourceUploader();
+
+            jobResourceUploader.UploadJobResource(AnyDriverLocalFolderPath);
+
+            // Archive file generator recieved exactly one call with correct driver local folder path
+            testContext.ResourceArchiveFileGenerator.Received(1).CreateArchiveToUpload(AnyDriverLocalFolderPath);
+        }
+
+        [TestMethod]
+        public void UploadJobResourceReturnsJobResourceDetails()
+        {
+            var testContext = new TestContext();
+            var jobResourceUploader = testContext.GetJobResourceUploader();
+
+            var jobResource = jobResourceUploader.UploadJobResource(AnyDriverLocalFolderPath);
+
+            Assert.AreEqual(AnyModificationTime, jobResource.LastModificationUnixTimestamp);
+            Assert.AreEqual(AnyResourceSize, jobResource.ResourceSize);
+            Assert.AreEqual(AnyUploadedResourceAbsoluteUri, jobResource.RemoteUploadPath);
+        }
+
+        [TestMethod]
+        public void UploadJobResourceMakesCorrectFileSystemCalls()
+        {
+            var testContext = new TestContext();
+            var jobResourceUploader = testContext.GetJobResourceUploader();
+
+            jobResourceUploader.UploadJobResource(AnyDriverLocalFolderPath);
+
+            testContext.FileSystem.Received(1).CreateUriForPath(AnyDriverResourceUploadPath);
+            testContext.FileSystem.Received(1).CreateUriForPath(AnyUploadedResourcePath);
+            testContext.FileSystem.Received(1)
+                .CopyFromLocal(AnyLocalArchivePath, new Uri(AnyUploadedResourceAbsoluteUri));
+            testContext.FileSystem.Received(1)
+                .CreateDirectory(new Uri(AnyScheme + AnyHost + AnyDriverResourceUploadPath));
+        }
+
+        [TestMethod]
+        public void UploadJobResourceCallsJobSubmissionDirProvider()
+        {
+            var testContext = new TestContext();
+            var jobResourceUploader = testContext.GetJobResourceUploader();
+
+            jobResourceUploader.UploadJobResource(AnyDriverLocalFolderPath);
+
+            testContext.JobSubmissionDirectoryProvider.Received(1).GetJobSubmissionDirectory();
+        }
+
+        private class TestContext
+        {
+
+            public readonly IJobSubmissionDirectoryProvider JobSubmissionDirectoryProvider =
+                Substitute.For<IJobSubmissionDirectoryProvider>();
+            public readonly IResourceArchiveFileGenerator ResourceArchiveFileGenerator =
+                Substitute.For<IResourceArchiveFileGenerator>();
+            public readonly IFileSystem FileSystem = Substitute.For<IFileSystem>();
+
+            public JobResourceUploader GetJobResourceUploader()
+            {
+                var injector = TangFactory.GetTang().NewInjector();
+                JobSubmissionDirectoryProvider.GetJobSubmissionDirectory().Returns(AnyDriverResourceUploadPath);
+                FileSystem.GetFileStatus(new Uri(AnyUploadedResourceAbsoluteUri))
+                    .Returns(new FileStatus(Epoch + TimeSpan.FromSeconds(AnyModificationTime), AnyResourceSize));
+                ResourceArchiveFileGenerator.CreateArchiveToUpload(AnyDriverLocalFolderPath)
+                    .Returns(AnyLocalArchivePath);
+                FileSystem.CreateUriForPath(AnyDriverResourceUploadPath)
+                    .Returns(new Uri(AnyScheme + AnyHost + AnyDriverResourceUploadPath));
+                FileSystem.CreateUriForPath(AnyUploadedResourcePath)
+                    .Returns(new Uri(AnyUploadedResourceAbsoluteUri));
+                injector.BindVolatileInstance(GenericType<IJobSubmissionDirectoryProvider>.Class, JobSubmissionDirectoryProvider);
+                injector.BindVolatileInstance(GenericType<IResourceArchiveFileGenerator>.Class, ResourceArchiveFileGenerator);
+                injector.BindVolatileInstance(GenericType<IFileSystem>.Class, FileSystem);
+                return injector.GetInstance<JobResourceUploader>();
+            }
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.Client/YARN/RESTClient/JobResourceUploader.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.IO;
+using Org.Apache.REEF.Client.Common;
+using Org.Apache.REEF.Client.Yarn;
+using Org.Apache.REEF.IO.FileSystem;
+using Org.Apache.REEF.Tang.Annotations;
+using Org.Apache.REEF.Utilities.Logging;
+
+namespace Org.Apache.REEF.Client.YARN.RestClient
+{
+    public sealed class JobResourceUploader : IJobResourceUploader
+    {
+        private static readonly Logger Log = Logger.GetLogger(typeof(LegacyJobResourceUploader));
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        private readonly IJobSubmissionDirectoryProvider _jobSubmissionDirectoryProvider;
+        private readonly IResourceArchiveFileGenerator _resourceArchiveFileGenerator;
+        private readonly IFileSystem _fileSystem;
+
+        [Inject]
+        private JobResourceUploader(
+            IJobSubmissionDirectoryProvider jobSubmissionDirectoryProvider,
+            IResourceArchiveFileGenerator resourceArchiveFileGenerator,
+            IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+            _resourceArchiveFileGenerator = resourceArchiveFileGenerator;
+            _jobSubmissionDirectoryProvider = jobSubmissionDirectoryProvider;
+        }
+
+        public JobResource UploadJobResource(string driverLocalFolderPath)
+        {
+            driverLocalFolderPath = driverLocalFolderPath.TrimEnd('\\') + @"\";
+            var driverUploadPath = _jobSubmissionDirectoryProvider.GetJobSubmissionDirectory().TrimEnd('/') + @"/";
+            Log.Log(Level.Info, "DriverFolderPath: {0} DriverUploadPath: {1}", driverLocalFolderPath, driverUploadPath);
+            var archivePath = _resourceArchiveFileGenerator.CreateArchiveToUpload(driverLocalFolderPath);
+
+            var destinationPath = Path.Combine(driverUploadPath, Path.GetFileName(archivePath));
+            var remoteFileUri = _fileSystem.CreateUriForPath(destinationPath);
+            Log.Log(Level.Info, @"Copy {0} to {1}", archivePath, remoteFileUri);
+
+            var parentDirectoryUri = _fileSystem.CreateUriForPath(driverUploadPath);
+            _fileSystem.CreateDirectory(parentDirectoryUri);
+            _fileSystem.CopyFromLocal(archivePath, remoteFileUri);
+            var fileStatus = _fileSystem.GetFileStatus(remoteFileUri);
+
+            return new JobResource
+            {
+                LastModificationUnixTimestamp = DateTimeToUnixTimestamp(fileStatus.ModificationTime),
+                RemoteUploadPath = remoteFileUri.AbsoluteUri,
+                ResourceSize = fileStatus.LengthBytes
+            };
+        }
+
+        private long DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            return (long) (dateTime - Epoch).TotalSeconds;
+        }
+    }
+}
