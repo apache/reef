d10ab5b85ce2b98715d2963ab0539b597be9927e
Andrew Chung <afchung90@gmail.com>
2015-11-12 16:50:46 -0800

    [REEF-941] IFileSystem for Azure Blob Storage
    This addressed the issue by
      * Adding Azure Storage dependency to REEF.IO.
      * Implementing and testing on "Fakes" and actual storage.
      * Creating proxy classes for "Fakes."
    
    JIRA:
      [REEF-941](https://issues.apache.org/jira/browse/REEF-941)

---
+++ lang/cs/Org.Apache.REEF.IO.Tests/TestAzureBlobFileSystem.cs
@@ -0,0 +1,146 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NSubstitute;
+using Org.Apache.REEF.IO.FileSystem;
+using Org.Apache.REEF.IO.FileSystem.AzureBlob;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+
+namespace Org.Apache.REEF.IO.Tests
+{
+    /// <summary>
+    /// Test class for unit testing the AzureBlobFileSystem.
+    /// Some methods are currently not unit tested due to the complexity of "faking out"
+    /// the methods. They are instead tested E2E in TestAzureBlobFileSystemE2E
+    /// </summary>
+    [TestClass]
+    public sealed class TestAzureBlobFileSystem
+    {
+        private readonly static Uri FakeUri = new Uri("http://fake.com");
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestCreateNotSupported()
+        {
+            new TestContext().GetAzureFileSystem().Create(FakeUri);
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestOpenNotSupported()
+        {
+            new TestContext().GetAzureFileSystem().Open(FakeUri);
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            var testContext = new TestContext();
+            testContext.GetAzureFileSystem().Delete(new Uri(FakeUri, "container/file"));
+            testContext.TestCloudBlob.Received(1).Delete();
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestDeleteAtContainer()
+        {
+            new TestContext().GetAzureFileSystem().Delete(new Uri(FakeUri, "container"));
+        }
+
+        [TestMethod]
+        public void TestExists()
+        {
+            var testContext = new TestContext();
+            testContext.GetAzureFileSystem().Exists(FakeUri);
+            testContext.TestCloudBlob.Received(1).Exists();
+        }
+
+        [TestMethod]
+        public void TestCopyToLocal()
+        {
+            var testContext = new TestContext();
+            testContext.GetAzureFileSystem().CopyToLocal(FakeUri, "local");
+            testContext.TestCloudBlob.Received(1).DownloadToFile("local", FileMode.CreateNew);
+        }
+
+        [TestMethod]
+        public void TestCreateDirectory()
+        {
+            var testContext = new TestContext();
+            testContext.GetAzureFileSystem().CreateDirectory(FakeUri);
+            testContext.TestCloudBlobClient.DidNotReceiveWithAnyArgs();
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestDeleteDirectoryFails()
+        {
+            new TestContext().GetAzureFileSystem().DeleteDirectory(FakeUri);
+        }
+
+        [TestMethod]
+        public void TestDeleteDirectoryAtContainer()
+        {
+            var testContext = new TestContext();
+            testContext.GetAzureFileSystem().DeleteDirectory(new Uri("http://test.com/test"));
+            testContext.TestCloudBlobClient.Received(1).GetContainerReference("test");
+            testContext.TestCloudBlobContainer.Received(1).DeleteIfExists();
+        }
+
+        [TestMethod]
+        public void TestDeleteDirectoryRecursive()
+        {
+            var testContext = new TestContext();
+            testContext.TestCloudBlobDirectory.ListBlobs(true).ReturnsForAnyArgs(Enumerable.Repeat(testContext.TestCloudBlob, 5));
+            testContext.GetAzureFileSystem().DeleteDirectory(new Uri("http://test.com/container/directory/directory"));
+            testContext.TestCloudBlobClient.Received(1).GetContainerReference("container");
+            testContext.TestCloudBlobContainer.Received(1).GetDirectoryReference("directory");
+            testContext.TestCloudBlobDirectory.Received(1).GetDirectoryReference("directory");
+            testContext.TestCloudBlobDirectory.Received(1).ListBlobs(true);
+            testContext.TestCloudBlob.Received(5).DeleteIfExists();
+        }
+
+        [TestMethod]
+        public void TestCreateUriForPath()
+        {
+            var testContext = new TestContext();
+            const string DirStructure = "container/directory";
+            Assert.AreEqual(new Uri(FakeUri, DirStructure), testContext.GetAzureFileSystem().CreateUriForPath(DirStructure));
+        }
+
+        private sealed class TestContext
+        {
+            public readonly ICloudBlobClient TestCloudBlobClient = Substitute.For<ICloudBlobClient>();
+            public readonly ICloudBlob TestCloudBlob = Substitute.For<ICloudBlob>();
+            public readonly ICloudBlobContainer TestCloudBlobContainer = Substitute.For<ICloudBlobContainer>();
+            public readonly ICloudBlobDirectory TestCloudBlobDirectory = Substitute.For<ICloudBlobDirectory>();
+
+            public IFileSystem GetAzureFileSystem()
+            {
+                var injector = TangFactory.GetTang().NewInjector();
+                injector.BindVolatileInstance(TestCloudBlobClient);
+                TestCloudBlobClient.BaseUri.ReturnsForAnyArgs(FakeUri);
+                TestCloudBlobClient.GetBlobReferenceFromServer(FakeUri).ReturnsForAnyArgs(TestCloudBlob);
+                TestCloudBlobClient.GetContainerReference("container").ReturnsForAnyArgs(TestCloudBlobContainer);
+                TestCloudBlobContainer.GetDirectoryReference("directory").ReturnsForAnyArgs(TestCloudBlobDirectory);
+                TestCloudBlobDirectory.GetDirectoryReference("directory").ReturnsForAnyArgs(TestCloudBlobDirectory);
+                return injector.GetInstance<AzureBlobFileSystem>();
+            }
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.IO/FileSystem/AzureBlob/AzureCloudBlobClient.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Org.Apache.REEF.IO.FileSystem.AzureBlob
+{
+    /// <summary>
+    /// A proxy class for CloudBlobClient, mainly in order to fake for testing.
+    /// </summary>
+    internal sealed class AzureCloudBlobClient : ICloudBlobClient
+    {
+        private readonly CloudBlobClient _client;
+
+        public StorageCredentials Credentials { get { return _client.Credentials; } }
+
+        public AzureCloudBlobClient(Uri baseUri)
+        {
+            _client = new CloudBlobClient(baseUri);
+        }
+
+        public AzureCloudBlobClient(Uri baseUri, StorageCredentials storageCredentials)
+        {
+            _client = new CloudBlobClient(baseUri, storageCredentials);
+        }
+
+        public AzureCloudBlobClient(StorageUri storageUri, StorageCredentials storageCredentials)
+        {
+            _client = new CloudBlobClient(storageUri, storageCredentials);
+        }
+
+        public AzureCloudBlobClient(string connectionString)
+        {
+            _client = CloudStorageAccount.Parse(connectionString).CreateCloudBlobClient();
+        }
+
+        public Uri BaseUri
+        {
+            get { return _client.BaseUri; }
+        }
+
+        public ICloudBlob GetBlobReferenceFromServer(Uri blobUri)
+        {
+            return _client.GetBlobReferenceFromServer(blobUri);
+        }
+
+        public ICloudBlobContainer GetContainerReference(string containerName)
+        {
+            return new AzureCloudBlobContainer(_client.GetContainerReference(containerName));
+        }
+
+        public BlobResultSegment ListBlobsSegmented(string prefix, bool useFlatListing, BlobListingDetails blobListingDetails,
+            int? maxResults, BlobContinuationToken continuationToken, BlobRequestOptions blobRequestOptions,
+            OperationContext operationContext)
+        {
+            return _client.ListBlobsSegmented(prefix, useFlatListing, blobListingDetails, maxResults, continuationToken,
+                blobRequestOptions, operationContext);
+        }
+    }
+}
