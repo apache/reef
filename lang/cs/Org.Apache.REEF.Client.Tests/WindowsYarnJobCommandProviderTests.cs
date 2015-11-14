4daf55645de7db5477a05ca19e70d7a7824183ff
Anupam <anupam128@gmail.com>
2015-11-13 18:03:36 -0800

    [REEF-946] Implement YARN REST submission command builder for REEF .NET client
    JIRA:
     [REEF-946](https://issues.apache.org/jira/browse/REEF-946)
    
    Pull Request:
      Closes #639

---
+++ lang/cs/Org.Apache.REEF.Client.Tests/WindowsYarnJobCommandProviderTests.cs
@@ -0,0 +1,186 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Org.Apache.REEF.Client.Yarn;
+using Org.Apache.REEF.Client.YARN;
+using Org.Apache.REEF.Client.YARN.Parameters;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Util;
+
+namespace Org.Apache.REEF.Client.Tests
+{
+    [TestClass]
+    public class WindowsYarnJobCommandProviderTests
+    {
+        private static readonly List<string> AnyClassPathItems = new List<string>
+        {
+            "%HADOOP_CONF_DIR%",
+            "%HADOOP_HOME%/*",
+            "%HADOOP_HOME%/lib/*",
+            "%HADOOP_COMMON_HOME%/*",
+            "%HADOOP_COMMON_HOME%/lib/*",
+            "%HADOOP_YARN_HOME%/*",
+            "%HADOOP_YARN_HOME%/lib/*",
+            "%HADOOP_HDFS_HOME%/*",
+            "%HADOOP_HDFS_HOME%/lib/*",
+            "%HADOOP_MAPRED_HOME%/*",
+            "%HADOOP_MAPRED_HOME%/lib/*",
+            "%HADOOP_HOME%/etc/hadoop",
+            "%HADOOP_HOME%/share/hadoop/common/*",
+            "%HADOOP_HOME%/share/hadoop/common/lib/*",
+            "%HADOOP_HOME%/share/hadoop/yarn/*",
+            "%HADOOP_HOME%/share/hadoop/yarn/lib/*",
+            "%HADOOP_HOME%/share/hadoop/hdfs/*",
+            "%HADOOP_HOME%/share/hadoop/hdfs/lib/*",
+            "%HADOOP_HOME%/share/hadoop/mapreduce/*",
+            "%HADOOP_HOME%/share/hadoop/mapreduce/lib/*",
+            "reef/local/*",
+            "reef/global/*"
+        };
+
+        [TestMethod]
+        public void YarnJobCommandBuilderIsDefaultImplementationOfIYarnJobCommandBuilder()
+        {
+            Assert.IsInstanceOfType(TangFactory.GetTang().NewInjector().GetInstance<IYarnJobCommandProvider>(), typeof(WindowsYarnJobCommandProvider));
+        }
+
+        [TestMethod]
+        public void GetJobSubmissionCommandGeneratesCorrectCommand()
+        {
+            var testContext = new TestContext();
+            const string expectedCommand = @"reef\Org.Apache.REEF.Bridge.exe" +
+                                           " %JAVA_HOME%/bin/java -XX:PermSize=128m -XX:MaxPermSize=128m -Xmx512m " +
+                                           "-classpath %HADOOP_CONF_DIR%;%HADOOP_HOME%/*;%HADOOP_HOME%/lib/*;%HADO" +
+                                           "OP_COMMON_HOME%/*;%HADOOP_COMMON_HOME%/lib/*;%HADOOP_YARN_HOME%/*;%HAD" +
+                                           "OOP_YARN_HOME%/lib/*;%HADOOP_HDFS_HOME%/*;%HADOOP_HDFS_HOME%/lib/*;%HA" +
+                                           "DOOP_MAPRED_HOME%/*;%HADOOP_MAPRED_HOME%/lib/*;%HADOOP_HOME%/etc/hadoo" +
+                                           "p;%HADOOP_HOME%/share/hadoop/common/*;%HADOOP_HOME%/share/hadoop/commo" +
+                                           "n/lib/*;%HADOOP_HOME%/share/hadoop/yarn/*;%HADOOP_HOME%/share/hadoop/y" +
+                                           "arn/lib/*;%HADOOP_HOME%/share/hadoop/hdfs/*;%HADOOP_HOME%/share/hadoop" +
+                                           "/hdfs/lib/*;%HADOOP_HOME%/share/hadoop/mapreduce/*;%HADOOP_HOME%/share" +
+                                           "/hadoop/mapreduce/lib/*;reef/local/*;reef/global/* -Dproc_reef org.apa" +
+                                           "che.reef.bridge.client.YarnBootstrapREEFLauncher reef/local/job-submis" +
+                                           "sion-params.json 1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+
+            var commandBuilder = testContext.GetCommandBuilder();
+            var jobSubmissionCommand = commandBuilder.GetJobSubmissionCommand();
+            Assert.AreEqual(expectedCommand, jobSubmissionCommand);
+        }
+
+        [TestMethod]
+        public void GetJobSubmissionCommandLoggingEnabledGeneratesCorrectCommand()
+        {
+            var testContext = new TestContext();
+            const string expectedCommand = @"reef\Org.Apache.REEF.Bridge.exe" +
+                                           " %JAVA_HOME%/bin/java -XX:PermSize=128m -XX:MaxPermSize=128m -Xmx512m " +
+                                           "-classpath %HADOOP_CONF_DIR%;%HADOOP_HOME%/*;%HADOOP_HOME%/lib/*;%HADO" +
+                                           "OP_COMMON_HOME%/*;%HADOOP_COMMON_HOME%/lib/*;%HADOOP_YARN_HOME%/*;%HAD" +
+                                           "OOP_YARN_HOME%/lib/*;%HADOOP_HDFS_HOME%/*;%HADOOP_HDFS_HOME%/lib/*;%HA" +
+                                           "DOOP_MAPRED_HOME%/*;%HADOOP_MAPRED_HOME%/lib/*;%HADOOP_HOME%/etc/hadoo" +
+                                           "p;%HADOOP_HOME%/share/hadoop/common/*;%HADOOP_HOME%/share/hadoop/commo" +
+                                           "n/lib/*;%HADOOP_HOME%/share/hadoop/yarn/*;%HADOOP_HOME%/share/hadoop/y" +
+                                           "arn/lib/*;%HADOOP_HOME%/share/hadoop/hdfs/*;%HADOOP_HOME%/share/hadoop" +
+                                           "/hdfs/lib/*;%HADOOP_HOME%/share/hadoop/mapreduce/*;%HADOOP_HOME%/share" +
+                                           "/hadoop/mapreduce/lib/*;reef/local/*;reef/global/* -Dproc_reef -Djava." +
+                                           "util.logging.config.class=org.apache.reef.util.logging.Config org.apac" +
+                                           "he.reef.bridge.client.YarnBootstrapREEFLauncher reef/local/job-submiss" +
+                                           "ion-params.json 1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+            var commandBuilder = testContext.GetCommandBuilder(true);
+            var jobSubmissionCommand = commandBuilder.GetJobSubmissionCommand();
+            Assert.AreEqual(expectedCommand, jobSubmissionCommand);
+        }
+
+        [TestMethod]
+        public void GetJobSubmissionCommandSetsCorrectDriverMemoryAllocationSize()
+        {
+            var testContext = new TestContext();
+            const int sizeMB = 256;
+            const string expectedCommandFormat = @"reef\Org.Apache.REEF.Bridge.exe" +
+                                                 " %JAVA_HOME%/bin/java -XX:PermSize=128m -XX:MaxPermSize=128m -Xmx{0}m " +
+                                                 "-classpath %HADOOP_CONF_DIR%;%HADOOP_HOME%/*;%HADOOP_HOME%/lib/*;%HADO" +
+                                                 "OP_COMMON_HOME%/*;%HADOOP_COMMON_HOME%/lib/*;%HADOOP_YARN_HOME%/*;%HAD" +
+                                                 "OOP_YARN_HOME%/lib/*;%HADOOP_HDFS_HOME%/*;%HADOOP_HDFS_HOME%/lib/*;%HA" +
+                                                 "DOOP_MAPRED_HOME%/*;%HADOOP_MAPRED_HOME%/lib/*;%HADOOP_HOME%/etc/hadoo" +
+                                                 "p;%HADOOP_HOME%/share/hadoop/common/*;%HADOOP_HOME%/share/hadoop/commo" +
+                                                 "n/lib/*;%HADOOP_HOME%/share/hadoop/yarn/*;%HADOOP_HOME%/share/hadoop/y" +
+                                                 "arn/lib/*;%HADOOP_HOME%/share/hadoop/hdfs/*;%HADOOP_HOME%/share/hadoop" +
+                                                 "/hdfs/lib/*;%HADOOP_HOME%/share/hadoop/mapreduce/*;%HADOOP_HOME%/share" +
+                                                 "/hadoop/mapreduce/lib/*;reef/local/*;reef/global/* -Dproc_reef org.apa" +
+                                                 "che.reef.bridge.client.YarnBootstrapREEFLauncher reef/local/job-submis" +
+                                                 "sion-params.json 1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+            string expectedCommand = string.Format(expectedCommandFormat, sizeMB);
+            var commandBuilder = testContext.GetCommandBuilder(maxMemAllocPoolSize: sizeMB);
+            var jobSubmissionCommand = commandBuilder.GetJobSubmissionCommand();
+            Assert.AreEqual(expectedCommand, jobSubmissionCommand);
+        }
+
+        [TestMethod]
+        public void GetJobSubmissionCommandSetsCorrectMaxPermSize()
+        {
+            var testContext = new TestContext();
+            const int sizeMB = 256;
+            const string expectedCommandFormat = @"reef\Org.Apache.REEF.Bridge.exe" +
+                                                 " %JAVA_HOME%/bin/java -XX:PermSize=128m -XX:MaxPermSize={0}m -Xmx512m " +
+                                                 "-classpath %HADOOP_CONF_DIR%;%HADOOP_HOME%/*;%HADOOP_HOME%/lib/*;%HADO" +
+                                                 "OP_COMMON_HOME%/*;%HADOOP_COMMON_HOME%/lib/*;%HADOOP_YARN_HOME%/*;%HAD" +
+                                                 "OOP_YARN_HOME%/lib/*;%HADOOP_HDFS_HOME%/*;%HADOOP_HDFS_HOME%/lib/*;%HA" +
+                                                 "DOOP_MAPRED_HOME%/*;%HADOOP_MAPRED_HOME%/lib/*;%HADOOP_HOME%/etc/hadoo" +
+                                                 "p;%HADOOP_HOME%/share/hadoop/common/*;%HADOOP_HOME%/share/hadoop/commo" +
+                                                 "n/lib/*;%HADOOP_HOME%/share/hadoop/yarn/*;%HADOOP_HOME%/share/hadoop/y" +
+                                                 "arn/lib/*;%HADOOP_HOME%/share/hadoop/hdfs/*;%HADOOP_HOME%/share/hadoop" +
+                                                 "/hdfs/lib/*;%HADOOP_HOME%/share/hadoop/mapreduce/*;%HADOOP_HOME%/share" +
+                                                 "/hadoop/mapreduce/lib/*;reef/local/*;reef/global/* -Dproc_reef org.apa" +
+                                                 "che.reef.bridge.client.YarnBootstrapREEFLauncher reef/local/job-submis" +
+                                                 "sion-params.json 1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+            string expectedCommand = string.Format(expectedCommandFormat, sizeMB);
+            
+            var commandBuilder = testContext.GetCommandBuilder(maxPermSize: sizeMB);
+            var jobSubmissionCommand = commandBuilder.GetJobSubmissionCommand();
+            
+            Assert.AreEqual(expectedCommand, jobSubmissionCommand);
+        }
+
+        private class TestContext
+        {
+            public WindowsYarnJobCommandProvider GetCommandBuilder(bool enableLogging = false, 
+                int? maxPermSize = null,
+                int? maxMemAllocPoolSize = null)
+            {
+                var injector = TangFactory.GetTang().NewInjector();
+         
+                IYarnCommandLineEnvironment yarnCommandLineEnvironment = Substitute.For<IYarnCommandLineEnvironment>();
+                yarnCommandLineEnvironment.GetYarnClasspathList().Returns(AnyClassPathItems);
+                
+                injector.BindVolatileInstance(GenericType<IYarnCommandLineEnvironment>.Class, yarnCommandLineEnvironment);
+                injector.BindVolatileParameter(GenericType<EnableDebugLogging>.Class, enableLogging);
+                if (maxPermSize.HasValue)
+                {
+                    injector.BindVolatileParameter<DriverMaxPermSizeMB, int>(maxPermSize.Value);
+                }
+                if (maxMemAllocPoolSize.HasValue)
+                {
+                    injector.BindVolatileParameter<DriverMaxMemoryAllicationPoolSizeMB, int>(maxMemAllocPoolSize.Value);
+                }
+
+                return injector.GetInstance<WindowsYarnJobCommandProvider>();
+            }
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.Client/YARN/WindowsYarnJobCommandProvider.cs
@@ -0,0 +1,96 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System.Text;
+using Org.Apache.REEF.Client.Yarn;
+using Org.Apache.REEF.Client.YARN.Parameters;
+using Org.Apache.REEF.Common.Files;
+using Org.Apache.REEF.Tang.Annotations;
+
+namespace Org.Apache.REEF.Client.YARN
+{
+    /// <summary>
+    /// WindowsYarnJobCommandProvider is .NET implementation of `org.apache.reef.runtime.common.launch.JavaLaunchCommandBuilder`
+    /// This class provides the command to be submitted to RM for execution of .NET driver running on Windows environment.
+    /// </summary>
+    internal sealed class WindowsYarnJobCommandProvider : IYarnJobCommandProvider
+    {
+        private static readonly string JavaExe = @"%JAVA_HOME%/bin/java";
+
+        private static readonly string JvmOptionsPermSize = @"-XX:PermSize=128m";
+        private static readonly string JvmOptionsMaxPermSizeFormat = @"-XX:MaxPermSize={0}m";
+        private static readonly string JvmOptionsMaxMemoryAllocationPoolSizeFormat = @"-Xmx{0}m";
+        private static readonly string ClassPathToken = @"-classpath";
+
+        private static readonly string ProcReefProperty = @"-Dproc_reef";
+        private static readonly string JavaLoggingProperty =
+            @"-Djava.util.logging.config.class=org.apache.reef.util.logging.Config";
+        private static readonly string LauncherClassName = @"org.apache.reef.bridge.client.YarnBootstrapREEFLauncher";
+        private readonly REEFFileNames _fileNames;
+        private readonly bool _enableDebugLogging;
+        private readonly IYarnCommandLineEnvironment _yarnCommandLineEnvironment;
+        private readonly int _driverMaxMemoryAllocationPoolSizeMB;
+        private readonly int _driverMaxPermSizeMB;
+
+
+        [Inject]
+        private WindowsYarnJobCommandProvider(
+            [Parameter(typeof(EnableDebugLogging))] bool enableDebugLogging,
+            [Parameter(typeof(DriverMaxMemoryAllicationPoolSizeMB))] int driverMaxMemoryAllocationPoolSizeMB,
+            [Parameter(typeof(DriverMaxPermSizeMB))] int driverMaxPermSizeMB,
+            IYarnCommandLineEnvironment yarnCommandLineEnvironment,
+            REEFFileNames fileNames)
+        {
+            _yarnCommandLineEnvironment = yarnCommandLineEnvironment;
+            _enableDebugLogging = enableDebugLogging;
+            _fileNames = fileNames;
+            _driverMaxMemoryAllocationPoolSizeMB = driverMaxMemoryAllocationPoolSizeMB;
+            _driverMaxPermSizeMB = driverMaxPermSizeMB;
+        }
+
+        /// <summary>
+        /// Builds the command to be submitted to YARNRM
+        /// </summary>
+        /// <returns>Command string</returns>
+        public string GetJobSubmissionCommand()
+        {
+            var sb = new StringBuilder();
+            sb.Append(_fileNames.GetBridgeExePath());
+            sb.Append(" " + JavaExe);
+            sb.Append(" " + JvmOptionsPermSize);
+            sb.Append(" " + string.Format(JvmOptionsMaxPermSizeFormat, _driverMaxPermSizeMB));
+            sb.Append(" " +
+                      string.Format(JvmOptionsMaxMemoryAllocationPoolSizeFormat, _driverMaxMemoryAllocationPoolSizeMB));
+            sb.Append(" " + ClassPathToken);
+            sb.Append(" " + string.Join(";", _yarnCommandLineEnvironment.GetYarnClasspathList()));
+            sb.Append(" " + ProcReefProperty);
+            if (_enableDebugLogging)
+            {
+                sb.Append(" " + JavaLoggingProperty);
+            }
+
+            sb.Append(" " + LauncherClassName);
+            sb.Append(" " +
+                      string.Format("{0}/{1}/{2}",
+                          _fileNames.GetReefFolderName(),
+                          _fileNames.GetLocalFolderName(),
+                          _fileNames.GetJobSubmissionParametersFile()));
+            sb.Append(" " + _fileNames.GetDriverLoggingConfigCommand());
+            return sb.ToString();
+        }
+    }
+}
