155eda21fa0797e2a64158cde44cef79e39f685b
Anupam <anupam128@gmail.com>
2015-11-13 18:03:36 -0800

    [REEF-946] Implement YARN REST submission command builder for REEF .NET client
    JIRA:
     [REEF-946](https://issues.apache.org/jira/browse/REEF-946)

---
+++ lang/cs/Org.Apache.REEF.Client.Tests/YarnJobCommandBuilderTests.cs
@@ -0,0 +1,81 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Org.Apache.REEF.Client.YARN;
+using Org.Apache.REEF.Client.YARN.Parameters;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Util;
+
+namespace Org.Apache.REEF.Client.Tests
+{
+    [TestClass]
+    public class YarnJobCommandBuilderTests
+    {
+        [TestMethod]
+        public void YarnJobCommandBuilderIsDefaultImplementationOfIYarnJobCommandBuilder()
+        {
+            Assert.IsInstanceOfType(TangFactory.GetTang().NewInjector().GetInstance<IYarnJobCommandBuilder>(), typeof(YarnJobCommandBuilder));
+        }
+
+        [TestMethod]
+        public void GetJobSubmissionCommandGeneratesCorrectCommand()
+        {
+            const string expectedCommand = @"reef\Org.Apache.REEF.Bridge.exe" +
+                                           " %JAVA_HOME%/bin/java -XX:PermSize=128m -XX:MaxPermSize=128m -Xmx512m " +
+                                           "-classpath %HADOOP_CONF_DIR%;%HADOOP_HOME%/*;%HADOOP_HOME%/lib/*;%HADO" +
+                                           "OP_COMMON_HOME%/*;%HADOOP_COMMON_HOME%/lib/*;%HADOOP_YARN_HOME%/*;%HAD" +
+                                           "OOP_YARN_HOME%/lib/*;%HADOOP_HDFS_HOME%/*;%HADOOP_HDFS_HOME%/lib/*;%HA" +
+                                           "DOOP_MAPRED_HOME%/*;%HADOOP_MAPRED_HOME%/lib/*;%HADOOP_HOME%/etc/hadoo" +
+                                           "p;%HADOOP_HOME%/share/hadoop/common/*;%HADOOP_HOME%/share/hadoop/commo" +
+                                           "n/lib/*;%HADOOP_HOME%/share/hadoop/yarn/*;%HADOOP_HOME%/share/hadoop/y" +
+                                           "arn/lib/*;%HADOOP_HOME%/share/hadoop/hdfs/*;%HADOOP_HOME%/share/hadoop" +
+                                           "/hdfs/lib/*;%HADOOP_HOME%/share/hadoop/mapreduce/*;%HADOOP_HOME%/share" +
+                                           "/hadoop/mapreduce/lib/*;reef/local/*;reef/global/* -Dproc_reef org.apa" +
+                                           "che.reef.bridge.client.YarnBootstrapREEFLauncher reef/local/yarnparame" +
+                                           "ters.json 1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+
+            var commandBuilder = TangFactory.GetTang().NewInjector().GetInstance<YarnJobCommandBuilder>();
+            var jobSubmissionCommand = commandBuilder.GetJobSubmissionCommand();
+            Assert.AreEqual(expectedCommand, jobSubmissionCommand);
+        }
+
+        [TestMethod]
+        public void GetJobSubmissionCommandLoggingEnabledGeneratesCorrectCommand()
+        {
+            const string expectedCommand = @"reef\Org.Apache.REEF.Bridge.exe" +
+                                           " %JAVA_HOME%/bin/java -XX:PermSize=128m -XX:MaxPermSize=128m -Xmx512m " +
+                                           "-classpath %HADOOP_CONF_DIR%;%HADOOP_HOME%/*;%HADOOP_HOME%/lib/*;%HADO" +
+                                           "OP_COMMON_HOME%/*;%HADOOP_COMMON_HOME%/lib/*;%HADOOP_YARN_HOME%/*;%HAD" +
+                                           "OOP_YARN_HOME%/lib/*;%HADOOP_HDFS_HOME%/*;%HADOOP_HDFS_HOME%/lib/*;%HA" +
+                                           "DOOP_MAPRED_HOME%/*;%HADOOP_MAPRED_HOME%/lib/*;%HADOOP_HOME%/etc/hadoo" +
+                                           "p;%HADOOP_HOME%/share/hadoop/common/*;%HADOOP_HOME%/share/hadoop/commo" +
+                                           "n/lib/*;%HADOOP_HOME%/share/hadoop/yarn/*;%HADOOP_HOME%/share/hadoop/y" +
+                                           "arn/lib/*;%HADOOP_HOME%/share/hadoop/hdfs/*;%HADOOP_HOME%/share/hadoop" +
+                                           "/hdfs/lib/*;%HADOOP_HOME%/share/hadoop/mapreduce/*;%HADOOP_HOME%/share" +
+                                           "/hadoop/mapreduce/lib/*;reef/local/*;reef/global/* -Dproc_reef -Djava." +
+                                           "util.logging.config.class=org.apache.reef.util.logging.Config org.apac" +
+                                           "he.reef.bridge.client.YarnBootstrapREEFLauncher reef/local/yarnparamet" +
+                                           "ers.json 1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+            var injector = TangFactory.GetTang().NewInjector();
+            injector.BindVolatileParameter(GenericType<EnableDebugLogging>.Class, true);
+            var commandBuilder = injector.GetInstance<YarnJobCommandBuilder>();
+            var jobSubmissionCommand = commandBuilder.GetJobSubmissionCommand();
+            Assert.AreEqual(expectedCommand, jobSubmissionCommand);
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.Client/YARN/YarnJobCommandBuilder.cs
@@ -0,0 +1,107 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System.Collections.Generic;
+using System.Text;
+using Org.Apache.REEF.Client.YARN.Parameters;
+using Org.Apache.REEF.Common.Files;
+using Org.Apache.REEF.Tang.Annotations;
+
+namespace Org.Apache.REEF.Client.YARN
+{
+    /// <summary>
+    /// YarnJobCommandBuilder is .NET implementation of `org.apache.reef.runtime.common.launch.JavaLaunchCommandBuilder`
+    /// This class provides the command to be submitted to RM for execution of .NET driver.
+    /// </summary>
+    public sealed class YarnJobCommandBuilder : IYarnJobCommandBuilder
+    {
+        private static readonly string JavaExe = @"%JAVA_HOME%/bin/java";
+
+        // keeping static values for now; make dynamic when needed
+        private static readonly string JvmOptions = @"-XX:PermSize=128m -XX:MaxPermSize=128m -Xmx512m";
+        private static readonly string ClassPathToken = @"-classpath";
+
+        private static readonly List<string> ClassPathItems = new List<string>
+        {
+            "%HADOOP_CONF_DIR%",
+            "%HADOOP_HOME%/*",
+            "%HADOOP_HOME%/lib/*",
+            "%HADOOP_COMMON_HOME%/*",
+            "%HADOOP_COMMON_HOME%/lib/*",
+            "%HADOOP_YARN_HOME%/*",
+            "%HADOOP_YARN_HOME%/lib/*",
+            "%HADOOP_HDFS_HOME%/*",
+            "%HADOOP_HDFS_HOME%/lib/*",
+            "%HADOOP_MAPRED_HOME%/*",
+            "%HADOOP_MAPRED_HOME%/lib/*",
+            "%HADOOP_HOME%/etc/hadoop",
+            "%HADOOP_HOME%/share/hadoop/common/*",
+            "%HADOOP_HOME%/share/hadoop/common/lib/*",
+            "%HADOOP_HOME%/share/hadoop/yarn/*",
+            "%HADOOP_HOME%/share/hadoop/yarn/lib/*",
+            "%HADOOP_HOME%/share/hadoop/hdfs/*",
+            "%HADOOP_HOME%/share/hadoop/hdfs/lib/*",
+            "%HADOOP_HOME%/share/hadoop/mapreduce/*",
+            "%HADOOP_HOME%/share/hadoop/mapreduce/lib/*",
+            "reef/local/*",
+            "reef/global/*"
+        };
+
+        private static readonly string ProcReefProperty = @"-Dproc_reef";
+        private static readonly string LoggingProperty =
+            @"-Djava.util.logging.config.class=org.apache.reef.util.logging.Config";
+        private static readonly string LauncherClassName = @"org.apache.reef.bridge.client.YarnBootstrapREEFLauncher";
+        private static readonly string ParametersFilePath = @"reef/local/yarnparameters.json";
+        private static readonly string DriverLoggingConfig = "1> <LOG_DIR>/driver.stdout 2> <LOG_DIR>/driver.stderr";
+        private readonly REEFFileNames _fileNames;
+        private readonly bool _enableDebugLogging;
+
+
+        [Inject]
+        private YarnJobCommandBuilder(
+            [Parameter(typeof(EnableDebugLogging))] bool enableDebugLogging,
+            REEFFileNames fileNames)
+        {
+            _enableDebugLogging = enableDebugLogging;
+            _fileNames = fileNames;
+        }
+
+        /// <summary>
+        /// Builds the command to be submitted to YARNRM
+        /// </summary>
+        /// <returns>Command string</returns>
+        public string GetJobSubmissionCommand()
+        {
+            var sb = new StringBuilder();
+            sb.Append(_fileNames.GetBridgeExePath());
+            sb.Append(" " + JavaExe);
+            sb.Append(" " + JvmOptions);
+            sb.Append(" " + ClassPathToken);
+            sb.Append(" " + string.Join(";", ClassPathItems));
+            sb.Append(" " + ProcReefProperty);
+            if (_enableDebugLogging)
+            {
+                sb.Append(" " + LoggingProperty);
+            }
+
+            sb.Append(" " + LauncherClassName);
+            sb.Append(" " + ParametersFilePath);
+            sb.Append(" " + DriverLoggingConfig);
+            return sb.ToString();
+        }
+    }
+}
