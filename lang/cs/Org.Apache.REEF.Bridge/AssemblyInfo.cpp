9edfd33d1b40c31d5a78fa1d39119f1c5ae5f264
Dongjoon Hyun <dongjoon@apache.org>
2016-01-06 15:16:16 -0800

    [REEF-1126] Update copyright year and fix obsolete assembly version
    This change:
     * updates the year of copyright statements.
     * fixes the obsolete version number in O.A.R.ClrDriver/AssemblyInfo.cpp.
     * fixes change_version.py the missing AssemblyInfo.cpp.
    
    JIRA:
      [REEF-1126](https://issues.apache.org/jira/browse/REEF-1126)
    
    Pull request:
      This closes #759

--- lang/cs/Org.Apache.REEF.Bridge/AssemblyInfo.cpp
+++ lang/cs/Org.Apache.REEF.Bridge/AssemblyInfo.cpp
@@ -35,7 +35,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
 [assembly:AssemblyProductAttribute(L"OrgApacheREEFJavaClrBridge")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2015")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2016")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
