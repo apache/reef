0b91e52a4b0574d89cc167e0cba217d9ea4502d0
Dongjoon Hyun <dongjoon@apache.org>
2016-02-05 12:03:32 -0800

    [REEF-1190] Fix mismatched AssemblyTitle and AssemblyProduct info
    JIRA:
      [REEF-1190](https://issues.apache.org/jira/browse/REEF-1190)
    
    Pull request:
      This closes #829

--- lang/cs/Org.Apache.REEF.Evaluator/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.REEF.Evaluator/Properties/AssemblyInfo.cs
@@ -19,36 +19,16 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Evaluator")]
+[assembly: AssemblyTitle("Org.Apache.REEF.Evaluator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Evaluator")]
+[assembly: AssemblyProduct("Org.Apache.REEF.Evaluator")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a64dc535-9b1e-41a4-8303-117f8b28c8c0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.14.0.0")]
 [assembly: AssemblyFileVersion("0.14.0.0")]
 
