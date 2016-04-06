dkm2110/Microsoft-cisl

[REEF-1292] Link and StreamingLink classes should have retry logic for setting up remote connection
This addressed the issue by

* introducing RemoteConnectionRetryHandler that handles the retry logic for connecting to remote endpoint
* allowing user to specify the value of connection retries and sleep times bewteen retries for NameClient, DefaultRemoteManagerFactory and StreamingRemoteManagerFactory.

JIRA:
  [REEF-1292](https://issues.apache.org/jira/browse/REEF-1292)

lang/cs/Org.Apache.REEF.Wake.Tests/TransportTest.cs
@@ -21,6 +21,7 @@ using System.Net;
 using System.Reactive;
 using System.Threading.Tasks;
 using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Util;
 using Org.Apache.REEF.Wake.Remote;
 using Org.Apache.REEF.Wake.Remote.Impl;
 using Org.Apache.REEF.Wake.Remote.Parameters;
@@ -33,6 +34,8 @@ namespace Org.Apache.REEF.Wake.Tests
     {
         private readonly IPAddress _localIpAddress = IPAddress.Parse("127.0.0.1");
         private readonly ITcpPortProvider _tcpPortProvider = GetTcpProvider(8900, 8940);
+        private readonly ITcpClientFactory _tcpClientFactory = GetTcpClientFactory(5, 500);
+
         [Fact]
         public void TestTransportServer()
         {
@@ -49,7 +52,7 @@ namespace Org.Apache.REEF.Wake.Tests
                 server.Run();
 
                 IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                using (var client = new TransportClient<string>(remoteEndpoint, codec))
+                using (var client = new TransportClient<string>(remoteEndpoint, codec, _tcpClientFactory))
                 {
                     client.Send("Hello");
                     client.Send(", ");
@@ -83,7 +86,7 @@ namespace Org.Apache.REEF.Wake.Tests
                 server.Run();
 
                 IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                using (var client = new TransportClient<TestEvent>(remoteEndpoint, codec))
+                using (var client = new TransportClient<TestEvent>(remoteEndpoint, codec, _tcpClientFactory))
                 {
                     client.Send(new TestEvent("Hello"));
                     client.Send(new TestEvent(", "));
@@ -119,7 +122,7 @@ namespace Org.Apache.REEF.Wake.Tests
 
                 var clientHandler = Observer.Create<TransportEvent<string>>(tEvent => queue.Add(tEvent.Data));
                 IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                using (var client = new TransportClient<string>(remoteEndpoint, codec, clientHandler))
+                using (var client = new TransportClient<string>(remoteEndpoint, codec, clientHandler, _tcpClientFactory))
                 {
                     client.Send("Hello");
                     client.Send(", ");
@@ -158,7 +161,7 @@ namespace Org.Apache.REEF.Wake.Tests
                     Task.Run(() =>
                     {
                         IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                        using (var client = new TransportClient<string>(remoteEndpoint, codec))
+                        using (var client = new TransportClient<string>(remoteEndpoint, codec, _tcpClientFactory))
                         {
                             client.Send("Hello");
                             client.Send(", ");
@@ -212,6 +215,17 @@ namespace Org.Apache.REEF.Wake.Tests
                 .BindIntNamedParam<TcpPortRangeCount>((portRangeEnd - portRangeStart + 1).ToString())
                 .Build();
             return TangFactory.GetTang().NewInjector(configuration).GetInstance<ITcpPortProvider>();
+        }
+
+        private static ITcpClientFactory GetTcpClientFactory(int connectionRetryCount, int sleepTimeInMs)
+        {
+            var config =
+                TangFactory.GetTang()
+                    .NewConfigurationBuilder()
+                    .BindIntNamedParam<ConnectionRetryCount>(connectionRetryCount.ToString())
+                    .BindIntNamedParam<SleepTimeInMs>(sleepTimeInMs.ToString())
+                    .Build();
+            return TangFactory.GetTang().NewInjector(config).GetInstance<ITcpClientFactory>();
         }
     }
 }

lang/cs/Org.Apache.REEF.Wake/Remote/Impl/DefaultRemoteManagerFactory.cs
@@ -28,25 +28,49 @@ namespace Org.Apache.REEF.Wake.Impl
     internal sealed class DefaultRemoteManagerFactory : IRemoteManagerFactory
     {
         private readonly ITcpPortProvider _tcpPortProvider;
+        private readonly ITcpClientFactory _tcpClientFactory;
+
         [Inject]
-        private DefaultRemoteManagerFactory(ITcpPortProvider tcpPortProvider)
+        private DefaultRemoteManagerFactory(ITcpPortProvider tcpPortProvider, ITcpClientFactory tcpClientFactory)
         {
             _tcpPortProvider = tcpPortProvider;
+            _tcpClientFactory = tcpClientFactory;
         }
 
+        /// <summary>
+        /// Gives DefaultRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Type of Message</typeparam>
+        /// <param name="localAddress">Local IP address</param>
+        /// <param name="port">Port number</param>
+        /// <param name="codec">Codec for message type T</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(IPAddress localAddress, int port, ICodec<T> codec)
         {
-            return new DefaultRemoteManager<T>(localAddress, port, codec, _tcpPortProvider);
+            return new DefaultRemoteManager<T>(localAddress, port, codec, _tcpPortProvider, _tcpClientFactory);
         }
 
+        /// <summary>
+        /// Gives DefaultRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Message type</typeparam>
+        /// <param name="localAddress">Local IP address</param>
+        /// <param name="codec">Codec for message type</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(IPAddress localAddress, ICodec<T> codec)
         {
-            return new DefaultRemoteManager<T>(localAddress, 0, codec, _tcpPortProvider);
+            return new DefaultRemoteManager<T>(localAddress, 0, codec, _tcpPortProvider, _tcpClientFactory);
         }
 
+        /// <summary>
+        /// Gives DefaultRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Message type</typeparam>
+        /// <param name="codec">Codec for message type</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(ICodec<T> codec)
         {
-            return new DefaultRemoteManager<T>(codec);
+            return new DefaultRemoteManager<T>(codec, _tcpClientFactory);
         }
     }
 }

lang/cs/Org.Apache.REEF.Wake/Remote/Impl/StreamingRemoteManagerFactory.cs
@@ -28,18 +28,29 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
     public sealed class StreamingRemoteManagerFactory
     {
         private readonly ITcpPortProvider _tcpPortProvider;
+        private readonly ITcpClientFactory _tcpClientFactory;
         private readonly IInjector _injector;
 
-        [Inject]  
-        private StreamingRemoteManagerFactory(ITcpPortProvider tcpPortProvider, IInjector injector)
+        [Inject]
+        private StreamingRemoteManagerFactory(ITcpPortProvider tcpPortProvider,
+            ITcpClientFactory tcpClientFactory,
+            IInjector injector)
         {
             _tcpPortProvider = tcpPortProvider;
+            _tcpClientFactory = tcpClientFactory;
             _injector = injector;
         }
 
+        /// <summary>
+        /// Gives StreamingRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Type of message</typeparam>
+        /// <param name="localAddress">local IP address</param>
+        /// <param name="codec">codec for message type T</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(IPAddress localAddress, IStreamingCodec<T> codec)
         {
-            return new StreamingRemoteManager<T>(localAddress, _tcpPortProvider, codec);
+            return new StreamingRemoteManager<T>(localAddress, _tcpPortProvider, codec, _tcpClientFactory);
         }
     }
 }

lang/cs/Org.Apache.REEF.Wake/Remote/Impl/TransportClient.cs
@@ -38,7 +38,8 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
         /// </summary>
         /// <param name="remoteEndpoint">The endpoint of the remote server to connect to</param>
         /// <param name="codec">Codec to decode/encode</param>
-        public TransportClient(IPEndPoint remoteEndpoint, ICodec<T> codec) 
+        /// <param name="clientFactory">TcpClient factory</param>
+        public TransportClient(IPEndPoint remoteEndpoint, ICodec<T> codec, ITcpClientFactory clientFactory) 
         {
             if (remoteEndpoint == null)
             {
@@ -48,8 +49,12 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
             {
                 throw new ArgumentNullException("codec");
             }
+            if (clientFactory == null)
+            {
+                throw new ArgumentNullException("clientFactory");
+            }
 
-            _link = new Link<T>(remoteEndpoint, codec);
+            _link = new Link<T>(remoteEndpoint, codec, clientFactory);
             _cancellationSource = new CancellationTokenSource();
             _disposed = false;
         }
@@ -61,10 +66,12 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
         /// <param name="remoteEndpoint">The endpoint of the remote server to connect to</param>
         /// <param name="codec">Codec to decode/encodec</param>
         /// <param name="observer">Callback used when receiving responses from remote host</param>
-        public TransportClient(IPEndPoint remoteEndpoint, 
-                               ICodec<T> codec, 
-                               IObserver<TransportEvent<T>> observer) 
-                                   : this(remoteEndpoint, codec)
+        /// <param name="clientFactory">TcpClient factory</param>
+        public TransportClient(IPEndPoint remoteEndpoint,
+            ICodec<T> codec,
+            IObserver<TransportEvent<T>> observer,
+            ITcpClientFactory clientFactory)
+            : this(remoteEndpoint, codec, clientFactory)
         {
             _observer = observer;
             Task.Run(() => ResponseLoop());

apache/reef

lang/cs/Org.Apache.REEF.Wake.Tests/TransportTest.cs
@@ -21,6 +21,7 @@ using System.Net;
 using System.Reactive;
 using System.Threading.Tasks;
 using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Util;
 using Org.Apache.REEF.Wake.Remote;
 using Org.Apache.REEF.Wake.Remote.Impl;
 using Org.Apache.REEF.Wake.Remote.Parameters;
@@ -33,6 +34,8 @@ namespace Org.Apache.REEF.Wake.Tests
     {
         private readonly IPAddress _localIpAddress = IPAddress.Parse("127.0.0.1");
         private readonly ITcpPortProvider _tcpPortProvider = GetTcpProvider(8900, 8940);
+        private readonly ITcpClientConnectionFactory _tcpClientFactory = GetTcpClientFactory(5, 500);
+
         [Fact]
         public void TestTransportServer()
         {
@@ -49,7 +52,7 @@ namespace Org.Apache.REEF.Wake.Tests
                 server.Run();
 
                 IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                using (var client = new TransportClient<string>(remoteEndpoint, codec))
+                using (var client = new TransportClient<string>(remoteEndpoint, codec, _tcpClientFactory))
                 {
                     client.Send("Hello");
                     client.Send(", ");
@@ -83,7 +86,7 @@ namespace Org.Apache.REEF.Wake.Tests
                 server.Run();
 
                 IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                using (var client = new TransportClient<TestEvent>(remoteEndpoint, codec))
+                using (var client = new TransportClient<TestEvent>(remoteEndpoint, codec, _tcpClientFactory))
                 {
                     client.Send(new TestEvent("Hello"));
                     client.Send(new TestEvent(", "));
@@ -119,7 +122,7 @@ namespace Org.Apache.REEF.Wake.Tests
 
                 var clientHandler = Observer.Create<TransportEvent<string>>(tEvent => queue.Add(tEvent.Data));
                 IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                using (var client = new TransportClient<string>(remoteEndpoint, codec, clientHandler))
+                using (var client = new TransportClient<string>(remoteEndpoint, codec, clientHandler, _tcpClientFactory))
                 {
                     client.Send("Hello");
                     client.Send(", ");
@@ -158,7 +161,7 @@ namespace Org.Apache.REEF.Wake.Tests
                     Task.Run(() =>
                     {
                         IPEndPoint remoteEndpoint = new IPEndPoint(_localIpAddress, server.LocalEndpoint.Port);
-                        using (var client = new TransportClient<string>(remoteEndpoint, codec))
+                        using (var client = new TransportClient<string>(remoteEndpoint, codec, _tcpClientFactory))
                         {
                             client.Send("Hello");
                             client.Send(", ");
@@ -212,6 +215,17 @@ namespace Org.Apache.REEF.Wake.Tests
                 .BindIntNamedParam<TcpPortRangeCount>((portRangeEnd - portRangeStart + 1).ToString())
                 .Build();
             return TangFactory.GetTang().NewInjector(configuration).GetInstance<ITcpPortProvider>();
+        }
+
+        private static ITcpClientConnectionFactory GetTcpClientFactory(int connectionRetryCount, int sleepTimeInMs)
+        {
+            var config =
+                TangFactory.GetTang()
+                    .NewConfigurationBuilder()
+                    .BindIntNamedParam<ConnectionRetryCount>(connectionRetryCount.ToString())
+                    .BindIntNamedParam<SleepTimeInMs>(sleepTimeInMs.ToString())
+                    .Build();
+            return TangFactory.GetTang().NewInjector(config).GetInstance<ITcpClientConnectionFactory>();
         }
     }
 }

lang/cs/Org.Apache.REEF.Wake/Remote/Impl/DefaultRemoteManagerFactory.cs
@@ -28,25 +28,49 @@ namespace Org.Apache.REEF.Wake.Impl
     internal sealed class DefaultRemoteManagerFactory : IRemoteManagerFactory
     {
         private readonly ITcpPortProvider _tcpPortProvider;
+        private readonly ITcpClientConnectionFactory _tcpClientFactory;
+
         [Inject]
-        private DefaultRemoteManagerFactory(ITcpPortProvider tcpPortProvider)
+        private DefaultRemoteManagerFactory(ITcpPortProvider tcpPortProvider, ITcpClientConnectionFactory tcpClientFactory)
         {
             _tcpPortProvider = tcpPortProvider;
+            _tcpClientFactory = tcpClientFactory;
         }
 
+        /// <summary>
+        /// Gives DefaultRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Type of Message</typeparam>
+        /// <param name="localAddress">Local IP address</param>
+        /// <param name="port">Port number</param>
+        /// <param name="codec">Codec for message type T</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(IPAddress localAddress, int port, ICodec<T> codec)
         {
-            return new DefaultRemoteManager<T>(localAddress, port, codec, _tcpPortProvider);
+            return new DefaultRemoteManager<T>(localAddress, port, codec, _tcpPortProvider, _tcpClientFactory);
         }
 
+        /// <summary>
+        /// Gives DefaultRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Message type</typeparam>
+        /// <param name="localAddress">Local IP address</param>
+        /// <param name="codec">Codec for message type</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(IPAddress localAddress, ICodec<T> codec)
         {
-            return new DefaultRemoteManager<T>(localAddress, 0, codec, _tcpPortProvider);
+            return new DefaultRemoteManager<T>(localAddress, 0, codec, _tcpPortProvider, _tcpClientFactory);
         }
 
+        /// <summary>
+        /// Gives DefaultRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Message type</typeparam>
+        /// <param name="codec">Codec for message type</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(ICodec<T> codec)
         {
-            return new DefaultRemoteManager<T>(codec);
+            return new DefaultRemoteManager<T>(codec, _tcpClientFactory);
         }
     }
 }

lang/cs/Org.Apache.REEF.Wake/Remote/Impl/StreamingRemoteManagerFactory.cs
@@ -28,18 +28,29 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
     public sealed class StreamingRemoteManagerFactory
     {
         private readonly ITcpPortProvider _tcpPortProvider;
+        private readonly ITcpClientConnectionFactory _tcpClientFactory;
         private readonly IInjector _injector;
 
-        [Inject]  
-        private StreamingRemoteManagerFactory(ITcpPortProvider tcpPortProvider, IInjector injector)
+        [Inject]
+        private StreamingRemoteManagerFactory(ITcpPortProvider tcpPortProvider,
+            ITcpClientConnectionFactory tcpClientFactory,
+            IInjector injector)
         {
             _tcpPortProvider = tcpPortProvider;
+            _tcpClientFactory = tcpClientFactory;
             _injector = injector;
         }
 
+        /// <summary>
+        /// Gives StreamingRemoteManager instance
+        /// </summary>
+        /// <typeparam name="T">Type of message</typeparam>
+        /// <param name="localAddress">local IP address</param>
+        /// <param name="codec">codec for message type T</param>
+        /// <returns>IRemoteManager instance</returns>
         public IRemoteManager<T> GetInstance<T>(IPAddress localAddress, IStreamingCodec<T> codec)
         {
-            return new StreamingRemoteManager<T>(localAddress, _tcpPortProvider, codec);
+            return new StreamingRemoteManager<T>(localAddress, _tcpPortProvider, codec, _tcpClientFactory);
         }
     }
 }

lang/cs/Org.Apache.REEF.Wake/Remote/Impl/TransportClient.cs
@@ -38,7 +38,8 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
         /// </summary>
         /// <param name="remoteEndpoint">The endpoint of the remote server to connect to</param>
         /// <param name="codec">Codec to decode/encode</param>
-        public TransportClient(IPEndPoint remoteEndpoint, ICodec<T> codec) 
+        /// <param name="clientFactory">TcpClient factory</param>
+        public TransportClient(IPEndPoint remoteEndpoint, ICodec<T> codec, ITcpClientConnectionFactory clientFactory) 
         {
             if (remoteEndpoint == null)
             {
@@ -48,8 +49,12 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
             {
                 throw new ArgumentNullException("codec");
             }
+            if (clientFactory == null)
+            {
+                throw new ArgumentNullException("clientFactory");
+            }
 
-            _link = new Link<T>(remoteEndpoint, codec);
+            _link = new Link<T>(remoteEndpoint, codec, clientFactory);
             _cancellationSource = new CancellationTokenSource();
             _disposed = false;
         }
@@ -61,10 +66,12 @@ namespace Org.Apache.REEF.Wake.Remote.Impl
         /// <param name="remoteEndpoint">The endpoint of the remote server to connect to</param>
         /// <param name="codec">Codec to decode/encodec</param>
         /// <param name="observer">Callback used when receiving responses from remote host</param>
-        public TransportClient(IPEndPoint remoteEndpoint, 
-                               ICodec<T> codec, 
-                               IObserver<TransportEvent<T>> observer) 
-                                   : this(remoteEndpoint, codec)
+        /// <param name="clientFactory">TcpClient factory</param>
+        public TransportClient(IPEndPoint remoteEndpoint,
+            ICodec<T> codec,
+            IObserver<TransportEvent<T>> observer,
+            ITcpClientConnectionFactory clientFactory)
+            : this(remoteEndpoint, codec, clientFactory)
         {
             _observer = observer;
             Task.Run(() => ResponseLoop());