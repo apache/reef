63ae64fd672d0d00ef9e2ba4ddaa4974994f4b72
Julia Wang <juliaw@apache.org>
2016-04-06 13:14:49 -0700

    [REEF-1308] Enhance Tang duplicated named parameter entries error message
    Update COnfigurationBuilderImpl
    Add test case
    
    JIRA: [REEF-1308](https://issues.apache.org/jira/browse/REEF-1308)
    This closes #

---
+++ lang/cs/Org.Apache.REEF.Tang.Tests/Configuration/TestNegativeCases.cs
@@ -0,0 +1,52 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using Org.Apache.REEF.Tang.Annotations;
+using Org.Apache.REEF.Tang.Implementations.Configuration;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Xunit;
+
+namespace Org.Apache.REEF.Tang.Tests.Configuration
+{
+    public class TestNegativeCases
+    {
+        [Fact]
+        public void TestDuplicatedNamedParameterbinding()
+        {
+            string msg = null;
+            try
+            {
+                var c = TangFactory.GetTang().NewConfigurationBuilder()
+                    .BindStringNamedParam<NamedString>("abc")
+                    .BindStringNamedParam<NamedString>("def")
+                    .Build();
+            }
+            catch (ArgumentException e)
+            {
+                msg = e.Message;
+            }
+            Assert.NotNull(msg);
+            Assert.True(msg.Contains(ConfigurationBuilderImpl.DuplicatedEntryForNamedParamater));
+        }
+
+        [NamedParameter(Documentation = "test", ShortName = "NamedString")]
+        class NamedString : Name<string>
+        {
+        }
+    }
+}
