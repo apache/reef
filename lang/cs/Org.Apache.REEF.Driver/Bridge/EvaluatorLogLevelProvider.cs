762ea0a15bda0a7395a1bd4ef25ef020f5cf4371
Andrew Chung <afchung90@gmail.com>
2016-04-07 16:01:02 -0700

    [REEF-1315] Logs are shown with Verbose level irrespective of level set
    This adds a new EvaluatorLogLevelProvider to provide log level configuration
    for Evaluators.
    
    JIRA:
      [REEF-1315](https://issues.apache.org/jira/browse/REEF-1315)
    
    Pull Request:
      This closes #934

---
+++ lang/cs/Org.Apache.REEF.Driver/Bridge/EvaluatorLogLevelProvider.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Tang.Annotations;
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Interface;
+using Org.Apache.REEF.Tang.Util;
+
+namespace Org.Apache.REEF.Driver.Bridge
+{
+    /// <summary>
+    /// Provides the log level for the Evaluator, based on the log level specified in the Driver.
+    /// </summary>
+    internal sealed class EvaluatorLogLevelProvider : IConfigurationProvider
+    {
+        private readonly string _traceLevel;
+
+        [Inject]
+        private EvaluatorLogLevelProvider([Parameter(Value = typeof(DriverBridgeConfigurationOptions.TraceLevel))] string traceLevel)
+        {
+            _traceLevel = traceLevel;
+        }
+
+        public IConfiguration GetConfiguration()
+        {
+            return TangFactory.GetTang().NewConfigurationBuilder()
+                .BindNamedParameter<DriverBridgeConfigurationOptions.TraceLevel, string>(
+                    GenericType<DriverBridgeConfigurationOptions.TraceLevel>.Class, _traceLevel)
+                .Build();
+        }
+    }
+}
