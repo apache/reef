7430c77d8df8f04b6877ae5f17e0942ce2e3a2ab
Julia Wang <jwang98052@yahoo.com>
2016-04-28 14:26:24 -0700

    [REEF-1340] Creating Context manager for Fault Tolerant
    Adding ContextManager which manages IActiveContext Add/Remove, etc operations
    Adding test cases for it
    
    JIRA: [REEF-1340](https://issues.apache.org/jira/browse/REEF-1340)
    This closes

---
+++ lang/cs/Org.Apache.REEF.IMRU.Tests/TestContextManager.cs
@@ -0,0 +1,337 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.Collections.Generic;
+using Org.Apache.REEF.Driver.Context;
+using Org.Apache.REEF.Driver.Evaluator;
+using Org.Apache.REEF.IMRU.OnREEF.Driver;
+using Xunit;
+
+namespace Org.Apache.REEF.IMRU.Tests
+{
+    /// <summary>
+    /// Test methods in ContextManager
+    /// </summary>
+    public class TestContextManager
+    {
+        private const string EvaluatorIdPrefix = "EvaluatorId";
+        private const string ContextIdPrefix = "ContextId";
+
+        /// <summary>
+        /// Test add, Remove, RemovedFailedContextInFailedEvaluator, NumberOfMissingContexts and NumberOfActiveContext
+        /// in ContextManager
+        /// </summary>
+        [Fact]
+        public void TestContextmanager()
+        {
+            var contextManager = new ContextManager(5);
+            for (int i = 0; i < 5; i++)
+            {
+                var c = new MyActiveContext(ContextIdPrefix + i);
+                contextManager.Add(c);
+            }
+            Assert.True(contextManager.AllContextsReceived);
+            Assert.Equal(5, contextManager.NumberOfActiveContext);
+            var e = new MyFailedEvaluator(ContextIdPrefix + "3");
+            contextManager.RemovedFailedContextInFailedEvaluator(e);
+            Assert.Equal(1, contextManager.NumberOfMissingContexts);
+            contextManager.Remove(ContextIdPrefix + "4");
+            Assert.Equal(3, contextManager.NumberOfActiveContext);
+        }
+
+        /// <summary>
+        /// Test remove a failed evaluator which has two contexts associated.
+        /// In current IMRU driver, assume there is only one context associated to the IFailedEvalutor
+        /// </summary>
+        [Fact]
+        public void TestRemoveFailedEvaluatorWithTwoContexts()
+        {
+            var contextManager = new ContextManager(5);
+            for (int i = 0; i < 5; i++)
+            {
+                var c = new MyActiveContext(ContextIdPrefix + i);
+                contextManager.Add(c);
+            }
+
+            var e = new MyFailedEvaluatorWithTwoContexts();
+            Action remove = () => contextManager.RemovedFailedContextInFailedEvaluator(e);
+            Assert.Throws<IMRUSystemException>(remove);
+        }
+
+        /// <summary>
+        /// Test remove a failed evaluator which has a context but it doesn't exist.
+        /// </summary>
+        [Fact]
+        public void TestRemoveFailedEvaluatorWithNoExistsContexts()
+        {
+            var contextManager = new ContextManager(5);
+            for (int i = 0; i < 5; i++)
+            {
+                var c = new MyActiveContext(ContextIdPrefix + i);
+                contextManager.Add(c);
+            }
+
+            var e = new MyFailedEvaluator(ContextIdPrefix + "5");
+            Action remove = () => contextManager.RemovedFailedContextInFailedEvaluator(e);
+            Assert.Throws<SystemException>(remove);
+        }
+
+        /// <summary>
+        /// Test remove a failed evaluator which has no context associated.
+        /// The scenario may happen when an evaluator failed but context has not created yet. 
+        /// </summary>
+        [Fact]
+        public void TestRemoveFailedEvaluatorWithNoContext()
+        {
+            var contextManager = new ContextManager(5);
+            for (int i = 0; i < 5; i++)
+            {
+                var c = new MyActiveContext(ContextIdPrefix + i);
+                contextManager.Add(c);
+            }
+
+            var e = new MyFailedEvaluatorWithNoContext();
+            contextManager.RemovedFailedContextInFailedEvaluator(e);
+            Assert.Equal(5, contextManager.NumberOfActiveContext);
+        }
+
+        /// <summary>
+        /// An implementation of IFailedEvaluator for testing
+        /// </summary>
+        private class MyFailedEvaluator : IFailedEvaluator
+        {
+            private readonly string _contextId;
+
+            /// <summary>
+            /// Constructor of MyFailedEvaluator
+            /// The contextId is used in the associated IFailedContext 
+            /// </summary>
+            /// <param name="contextId"></param>
+            internal MyFailedEvaluator(string contextId)
+            {
+                _contextId = contextId;
+            }
+
+            public EvaluatorException EvaluatorException
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Have one failed context in the failed Evaluator
+            /// </summary>
+            public IList<IFailedContext> FailedContexts
+            {
+                get
+                {
+                    IList<IFailedContext> contexts = new List<IFailedContext>();
+                    contexts.Add(new MyFailedContext(_contextId));
+                    return contexts;
+                }
+            }
+
+            public Utilities.Optional<Driver.Task.IFailedTask> FailedTask
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Returns Evaluator id
+            /// </summary>
+            public string Id
+            {
+                get { return EvaluatorIdPrefix + _contextId; }
+            }
+        }
+
+        /// <summary>
+        /// An implementation of IFailedEvaluator for testing
+        /// with no context associated
+        /// </summary>
+        private class MyFailedEvaluatorWithNoContext : IFailedEvaluator
+        {
+            public EvaluatorException EvaluatorException
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Returns null FailedContexts
+            /// </summary>
+            public IList<IFailedContext> FailedContexts
+            {
+                get { return null; }
+            }
+
+            public Utilities.Optional<Driver.Task.IFailedTask> FailedTask
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Returns Evaluator id
+            /// </summary>
+            public string Id
+            {
+                get { return EvaluatorIdPrefix + "no"; }
+            }
+        }
+
+        /// <summary>
+        /// An implementation of IFailedEvaluator for testing
+        /// with two contexts associated
+        /// </summary>
+        private class MyFailedEvaluatorWithTwoContexts : IFailedEvaluator
+        {
+            public EvaluatorException EvaluatorException
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Returns two FailedContexts
+            /// </summary>
+            public IList<IFailedContext> FailedContexts
+            {
+                get
+                {
+                    IList<IFailedContext> contexts = new List<IFailedContext>();
+                    contexts.Add(new MyFailedContext(ContextIdPrefix + "1"));
+                    contexts.Add(new MyFailedContext(ContextIdPrefix + "2"));
+                    return contexts;
+                }
+            }
+
+            public Utilities.Optional<Driver.Task.IFailedTask> FailedTask
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Returns Evaluator id
+            /// </summary>
+            public string Id
+            {
+                get { return EvaluatorIdPrefix + "no"; }
+            }
+        }
+
+        private class MyFailedContext : IFailedContext
+        {
+            private readonly string _id;
+
+            internal MyFailedContext(string id)
+            {
+                _id = id;
+            }
+
+            public Utilities.Optional<IActiveContext> ParentContext
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string EvaluatorId
+            {
+                get { return EvaluatorIdPrefix + _id; }
+            }
+
+            public Utilities.Optional<string> ParentId
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IEvaluatorDescriptor EvaluatorDescriptor
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string Id
+            {
+                get { return _id; }
+            }
+        }
+
+        /// <summary>
+        /// An implementation of IActiveContext for testing
+        /// </summary>
+        private sealed class MyActiveContext : IActiveContext
+        {
+            private readonly string _id;
+
+            /// <summary>
+            /// Constructor which gets id of MyActiveContext
+            /// </summary>
+            /// <param name="id"></param>
+            internal MyActiveContext(string id)
+            {
+                _id = id;
+            }
+
+            public void SendMessage(byte[] message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// Returns Evaluator Id of the MyActiveContext
+            /// </summary>
+            public string EvaluatorId
+            {
+                get { return EvaluatorIdPrefix + _id; }
+            }
+
+            public Utilities.Optional<string> ParentId
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IEvaluatorDescriptor EvaluatorDescriptor
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            /// <summary>
+            /// Returns Id of the Active Context
+            /// </summary>
+            public string Id
+            {
+                get { return _id; }
+            }
+
+            public void SubmitTask(Tang.Interface.IConfiguration taskConf)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SubmitContext(Tang.Interface.IConfiguration contextConfiguration)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SubmitContextAndService(Tang.Interface.IConfiguration contextConfiguration, Tang.Interface.IConfiguration serviceConfiguration)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.IMRU/OnREEF/Driver/ContextManager.cs
@@ -0,0 +1,147 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Org.Apache.REEF.Driver.Context;
+using Org.Apache.REEF.Driver.Evaluator;
+using Org.Apache.REEF.Utilities.Attributes;
+using Org.Apache.REEF.Utilities.Diagnostics;
+using Org.Apache.REEF.Utilities.Logging;
+
+namespace Org.Apache.REEF.IMRU.OnREEF.Driver
+{
+    /// <summary>
+    /// Manages active contexts for the driver
+    /// </summary>
+    [NotThreadSafe]
+    internal sealed class ContextManager
+    {
+        private static readonly Logger Logger = Logger.GetLogger(typeof(ContextManager));
+        private readonly IDictionary<string, IActiveContext> _activeContexts = new Dictionary<string, IActiveContext>();
+        private readonly int _totalExpectedContexts;
+
+        /// <summary>
+        /// Constructor of ContextManager
+        /// totalContexts specify the total number of expected active contexts that driver needs
+        /// </summary>
+        /// <param name="totalContexts"></param>
+        internal ContextManager(int totalContexts)
+        {
+            _totalExpectedContexts = totalContexts;
+        }
+
+        /// <summary>
+        /// Returns the collection of IActiveContext
+        /// </summary>
+        internal ICollection<IActiveContext> ActiveContexts
+        {
+            get { return _activeContexts.Values; }
+        }
+
+        /// <summary>
+        /// Returns the difference between the number of expected IActiveContext and actually number of IActiveContext. 
+        /// </summary>
+        internal int NumberOfMissingContexts
+        {
+            get { return _totalExpectedContexts - _activeContexts.Count; }
+        }
+
+        /// <summary>
+        /// Checks if all the requested contexts are received. 
+        /// </summary>
+        internal bool AllContextsReceived
+        {
+            get { return NumberOfMissingContexts == 0; }
+        }
+
+        /// <summary>
+        /// Adding an IActiveContext to the ActiveContext collection
+        /// Throw ApplicationException if the IActiveContext already exists.
+        /// </summary>
+        /// <param name="activeContext"></param>
+        internal void Add(IActiveContext activeContext)
+        {
+            if (_activeContexts.ContainsKey(activeContext.Id))
+            {
+                var msg = string.Format(CultureInfo.InvariantCulture, "The context [{0}] received already exists.", activeContext.Id);
+                Exceptions.Throw(new ApplicationException(msg), Logger);
+            }
+            _activeContexts.Add(activeContext.Id, activeContext);
+        }
+
+        /// <summary>
+        /// Remove an IActiveContext from the ActiveContext collection
+        /// Throw ApplicationException if the IActiveContext doesn't exist.
+        /// </summary>
+        /// <param name="activeContextId"></param>
+        internal void Remove(string activeContextId)
+        {
+            if (!_activeContexts.ContainsKey(activeContextId))
+            {
+                var msg = string.Format(CultureInfo.InvariantCulture, "The context [{0}] to be removed does not exist.", activeContextId);
+                Exceptions.Throw(new ApplicationException(msg), Logger);
+            }
+            _activeContexts.Remove(activeContextId);
+        }
+
+        /// <summary>
+        /// Returns the current number of IActiveContext in the ActiveContext collection
+        /// </summary>
+        internal int NumberOfActiveContext
+        {
+            get { return _activeContexts.Count; }
+        }
+
+        /// <summary>
+        /// Given an IFailedEvaluator, remove associated IActiveContext from the collection
+        /// Throw SystemException if associated IActiveContext doesn't exist.
+        /// Throw IMRUSystemException if more than one IActiveContext are associated with the IFailedEvaluator
+        /// </summary>
+        /// <param name="value"></param>
+        internal void RemovedFailedContextInFailedEvaluator(IFailedEvaluator value)
+        {
+            if (value.FailedContexts != null && value.FailedContexts.Count > 0)
+            {
+                if (value.FailedContexts.Count == 1)
+                {
+                    var failedContextId = value.FailedContexts[0].Id;
+                    if (!_activeContexts.ContainsKey(failedContextId))
+                    {
+                        var msg = string.Format(CultureInfo.InvariantCulture,
+                            "The active context [{0}] attached in IFailedEvaluator [{1}] is not in the Active Contexts collection.",
+                            failedContextId,
+                            value.Id);
+                        Exceptions.Throw(new SystemException(msg), Logger);
+                    }
+                    else
+                    {
+                        _activeContexts.Remove(failedContextId);
+                    }
+                }
+                else
+                {
+                    var msg = string.Format(CultureInfo.InvariantCulture,
+                        "There are [{0}] contexts attached in the failed evaluator. Expected number is 1.",
+                        value.FailedContexts.Count);
+                    Exceptions.Throw(new IMRUSystemException(msg), Logger);
+                }
+            }
+        }
+    }
+}
