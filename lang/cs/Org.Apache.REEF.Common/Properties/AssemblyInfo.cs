d59049dfca1dd27292b579b7c7b67c97bb5231af
Boris Shulman <shulmanb@gmail.com>
2016-05-20 09:55:04 -0700

    [REEF-1398] Update version to 0.16.0-SNAPSHOT This addresses the issue by updating the versions to 0.16.0-SNAPSHOT JIRA:     [REEF-1398](https://issues.apache.org/jira/browse/REEF-1398)

--- lang/cs/Org.Apache.REEF.Common/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.REEF.Common/Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.15.0.0")]
-[assembly: AssemblyFileVersion("0.15.0.0")]
+[assembly: AssemblyVersion("0.16.0.0")]
+[assembly: AssemblyFileVersion("0.16.0.0")]
 
 // Common and Evaluator share APIs that should not be exposed to the user
 [assembly: InternalsVisibleTo("Org.Apache.REEF.Evaluator, publickey=" +
--- lang/cs/Org.Apache.REEF.IMRU/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.REEF.IMRU/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("138891df-d331-4a5e-8514-775611c06f6c")]
-[assembly: AssemblyVersion("0.15.0.0")]
-[assembly: AssemblyFileVersion("0.15.0.0")]
+[assembly: AssemblyVersion("0.16.0.0")]
+[assembly: AssemblyFileVersion("0.16.0.0")]
 
 // Allow the tests project access to `internal` APIs
 [assembly: InternalsVisibleTo("Org.Apache.REEF.IMRU.Tests, publickey=" +
