dce5b376f4caba5aa63d59b34afe2ab09181e64f
dhruv <dhruv@apache.org>
2016-06-30 20:14:27 -0700

    [REEF-1480] Increase the retry count for task registration to high value
    This addressed the issue by
      * increasing the default value.
    JIRA:
      [REEF-1480](https://issues.apache.org/jira/browse/REEF-1480)

--- lang/cs/Org.Apache.REEF.Network/Group/Config/GroupCommConfigurationOptions.cs
+++ lang/cs/Org.Apache.REEF.Network/Group/Config/GroupCommConfigurationOptions.cs
@@ -59,9 +59,9 @@ namespace Org.Apache.REEF.Network.Group.Config
         /// We don't want to set it too low in case some nodes are just slow, if we simply throw an exception, that is not right. 
         /// We don't want it to try endlessly in case some node is really dead, we should come out with exception. 
         /// We want it to return as soon as all nodes in the group are registered, So increasing retry count is better than increasing sleep time.
-        /// Current default sleep time is 500ms. Default retry is 500. Total is 250000ms, that is 250s, little bit more than 4 min
+        /// Current default sleep time is 500ms. Default retry is 7500. Total is a bit more than 1 hour.
         /// </remarks>
-        [NamedParameter("Retry times to wait for nodes to be registered", defaultValue: "500")]
+        [NamedParameter("Retry times to wait for nodes to be registered", defaultValue: "7500")]
         internal sealed class RetryCountWaitingForRegistration : Name<int>
         {
         }
