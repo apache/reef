bfd447f52c2b446a7c47c379fdf1271447dc7185
pchung13 <me@patrickchung.net>
2016-08-23 10:18:35 -0700

    [REEF-1539] Fix memory leak in InteropUtil.cpp
    This addressed the issue by
      * Release the bytes generated in ManagedByteArrayFromJavaByteArray.
    
    JIRA:
      [REEF-1539](https://issues.apache.org/jira/browse/REEF-1539)

--- lang/cs/Org.Apache.REEF.Bridge/InteropUtil.cpp
+++ lang/cs/Org.Apache.REEF.Bridge/InteropUtil.cpp
@@ -101,6 +101,7 @@ array<byte>^ ManagedByteArrayFromJavaByteArray(
     for (int i = 0; i < len; i++) {
       managedByteArray[i] = bytes[i];
     }
+	env->ReleaseByteArrayElements(javaByteArray, (jbyte*)bytes, JNI_ABORT);
     return managedByteArray;
   }
   return nullptr;
