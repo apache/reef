36b368da6b9cdada0f2b1fb33e17092c2881291f
Sergey Dudoladov <ssd@apache.org>
2016-11-14 20:39:34 +0100

    [DISCUSS][REEF-683] Add checkpointing Service for REEF.NET
      This is the first PR to discuss the interface of the checkpoint service
      to be
    
      JIRA:
        [REEF-683](https://issues.apache.org/jira/browse/REEF-683)

---
+++ lang/cs/Org.Apache.REEF.IO.Checkpoint/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Org.Apache.REEF.IO.Checkpoint")]
+[assembly: AssemblyDescription("Checkpointing Service for REEF.NET")]
+[assembly: AssemblyProduct("Org.Apache.REEF.IO.Checkpoint")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("627d6d27-96dd-42e5-8ae7-9a2a705b75d4")]
