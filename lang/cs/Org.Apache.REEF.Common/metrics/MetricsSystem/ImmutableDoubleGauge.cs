fd2a746ee7b71a9e95cf784a86fd3d241eaf558b
dhruv <dhruv@apache.org>
2016-11-17 10:46:01 -0800

    [REEF-1672] Define Immutable Counters and Gauges
    This addressed the issue by
      * introducing Immutable gauges and counter classes.
      * introducing corresponding tests.
    
    JIRA:
      [REEF-1672](https://issues.apache.org/jira/browse/REEF-1672)
    
    Pull request:
      This closes #1183

---
+++ lang/cs/Org.Apache.REEF.Common/metrics/MetricsSystem/ImmutableDoubleGauge.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Common.Metrics.Api;
+
+namespace Org.Apache.REEF.Common.Metrics.MetricsSystem
+{
+    /// <summary>
+    /// Immutable double gauge implementation. All the double gauges added in 
+    /// <see cref="IMetricsRecordBuilder"/> will be internally converted to 
+    /// this class instance in metrics system.
+    /// </summary>
+    internal sealed class ImmutableDoubleGauge : ImmutableMetricsImpl
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="info">Meta data of the gauge</param>
+        /// <param name="value">Value of the gauge</param>
+        public ImmutableDoubleGauge(IMetricsInfo info, double value)
+            : base(info, value, MetricType.Gauge, (visitor) => visitor.Gauge(info, value))
+        {
+        }
+    }
+}
