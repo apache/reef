367ee20d486088d4aac260a629bd075ebb8627f3
Sergiy Matusevych <motus@apache.org>
2017-01-23 17:07:06 -0800

    [REEF-1724] Refactor HelloREEF example for the local runtime
       * Make code more readable and idiomatic
       * Log all running threads at the end of the process
       * remove references to `BindException` from the example and from `DriverLauncher`
       * Minor cleanups to the code
    
    JIRA: [REEF-1724](https://issues.apache.org/jira/browse/REEF-1724)
    
    Closes #

--- lang/java/reef-examples/src/main/java/org/apache/reef/examples/hello/HelloREEF.java
+++ lang/java/reef-examples/src/main/java/org/apache/reef/examples/hello/HelloREEF.java
@@ -23,9 +23,9 @@ import org.apache.reef.client.DriverLauncher;
 import org.apache.reef.client.LauncherStatus;
 import org.apache.reef.runtime.local.client.LocalRuntimeConfiguration;
 import org.apache.reef.tang.Configuration;
-import org.apache.reef.tang.exceptions.BindException;
 import org.apache.reef.tang.exceptions.InjectionException;
 import org.apache.reef.util.EnvironmentUtils;
+import org.apache.reef.util.ThreadLogger;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,60 +34,42 @@ import java.util.logging.Logger;
  * The Client for Hello REEF example.
  */
 public final class HelloREEF {
+
   private static final Logger LOG = Logger.getLogger(HelloREEF.class.getName());
 
-  /**
-   * The upper limit on the number of Evaluators that the local resourcemanager will hand out concurrently.
-   */
-  private static final int MAX_NUMBER_OF_EVALUATORS = 2;
-
-  /**
-   * Number of milliseconds to wait for the job to complete.
-   */
+  /** Number of milliseconds to wait for the job to complete. */
   private static final int JOB_TIMEOUT = 10000; // 10 sec.
 
 
-  /**
-   * @return the configuration of the runtime
-   */
-  private static Configuration getRuntimeConfiguration() {
-    return LocalRuntimeConfiguration.CONF
-        .set(LocalRuntimeConfiguration.MAX_NUMBER_OF_EVALUATORS, MAX_NUMBER_OF_EVALUATORS)
-        .build();
-  }
+  /** Configuration of the runtime. */
+  private static final Configuration RUNTIME_CONFIG =
+      LocalRuntimeConfiguration.CONF
+          .set(LocalRuntimeConfiguration.MAX_NUMBER_OF_EVALUATORS, 2)
+          .build();
+
+  /** Configuration of the HelloREEF driver. */
+  private static final Configuration DRIVER_CONFIG =
+      DriverConfiguration.CONF
+          .set(DriverConfiguration.DRIVER_IDENTIFIER, "HelloREEF")
+          .set(DriverConfiguration.GLOBAL_LIBRARIES, EnvironmentUtils.getClassLocation(HelloDriver.class))
+          .set(DriverConfiguration.ON_DRIVER_STARTED, HelloDriver.StartHandler.class)
+          .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, HelloDriver.EvaluatorAllocatedHandler.class)
+          .build();
 
   /**
-   * @return the configuration of the HelloREEF driver.
-   */
-  private static Configuration getDriverConfiguration() {
-    return DriverConfiguration.CONF
-        .set(DriverConfiguration.GLOBAL_LIBRARIES, EnvironmentUtils.getClassLocation(HelloDriver.class))
-        .set(DriverConfiguration.DRIVER_IDENTIFIER, "HelloREEF")
-        .set(DriverConfiguration.ON_DRIVER_STARTED, HelloDriver.StartHandler.class)
-        .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, HelloDriver.EvaluatorAllocatedHandler.class)
-        .build();
-  }
-
-  /**
-   * Start Hello REEF job.
-   *
+   * Start Hello REEF job with local runtime.
    * @param args command line parameters.
-   * @throws BindException      configuration error.
    * @throws InjectionException configuration error.
    */
-  public static void main(final String[] args) throws BindException, InjectionException {
-    final Configuration runtimeConf = getRuntimeConfiguration();
-    final Configuration driverConf = getDriverConfiguration();
+  public static void main(final String[] args) throws InjectionException {
 
-    final LauncherStatus status = DriverLauncher
-        .getLauncher(runtimeConf)
-        .run(driverConf, JOB_TIMEOUT);
+    final LauncherStatus status = DriverLauncher.getLauncher(RUNTIME_CONFIG).run(DRIVER_CONFIG, JOB_TIMEOUT);
+
     LOG.log(Level.INFO, "REEF job completed: {0}", status);
+
+    ThreadLogger.logThreads(LOG, Level.FINE, "Threads running at the end of HelloREEF:");
   }
 
-  /**
-   * Empty private constructor to prohibit instantiation of utility class.
-   */
-  private HelloREEF() {
-  }
+  /** Empty private constructor to prohibit instantiation of utility class. */
+  private HelloREEF() { }
 }
