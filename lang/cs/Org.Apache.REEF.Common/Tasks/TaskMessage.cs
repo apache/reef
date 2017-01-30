2fd16b5fce2e19c611ce22a79409af353e90763e
Mariia Mykhailova <mariia@apache.org>
2017-01-30 09:45:09 -0800

    [REEF-1376] Delete empty setter in Common.Tasks.TaskMessage.Message (breaking)
    JIRA:
      [REEF-1376](https://issues.apache.org/jira/browse/REEF-1376)
    
    Pull request:
      This closes #1246

--- lang/cs/Org.Apache.REEF.Common/Tasks/TaskMessage.cs
+++ lang/cs/Org.Apache.REEF.Common/Tasks/TaskMessage.cs
@@ -17,48 +17,48 @@
 
 using System;
 using Org.Apache.REEF.Utilities;
-using Org.Apache.REEF.Utilities.Logging;
 
 namespace Org.Apache.REEF.Common.Tasks
 {
+    /// <summary>
+    /// Task-side TaskMessage.
+    /// </summary>
     public class TaskMessage : IMessage
     {
-        private static readonly Logger LOGGER = Logger.GetLogger(typeof(TaskMessage));
-        private readonly string _messageSourcId;
+        private readonly string _messageSourceId;
         private readonly byte[] _bytes;
 
         private TaskMessage(string messageSourceId, byte[] bytes)
         {
-            _messageSourcId = messageSourceId;
+            _messageSourceId = messageSourceId;
             _bytes = bytes;
         }
 
         public string MessageSourceId
         {
-            get { return _messageSourcId; }
+            get { return _messageSourceId; }
         }
 
         public byte[] Message
         {
             get { return _bytes; }
-            set { }
         }
 
         /// <summary>
-        ///  From byte[] message to a TaskMessage
+        /// From byte[] message to a TaskMessage
         /// </summary>
-        /// <param name="messageSourceId">messageSourceId The message's sourceID. This will be accessible in the Driver for routing</param>
+        /// <param name="messageSourceId">The message's sourceID. This will be accessible in the Driver for routing</param>
         /// <param name="message">The actual content of the message, serialized into a byte[]</param>
         /// <returns>a new TaskMessage with the given content</returns>
         public static TaskMessage From(string messageSourceId, byte[] message)
         {
             if (string.IsNullOrEmpty(messageSourceId))
             {
-                Org.Apache.REEF.Utilities.Diagnostics.Exceptions.Throw(new ArgumentNullException("messageSourceId"), LOGGER);
+                throw new ArgumentNullException("messageSourceId");
             }
             if (message == null)
             {
-                Org.Apache.REEF.Utilities.Diagnostics.Exceptions.Throw(new ArgumentNullException("bytes"), LOGGER);
+                throw new ArgumentNullException("message");
             }
             return new TaskMessage(messageSourceId, message);
         }
