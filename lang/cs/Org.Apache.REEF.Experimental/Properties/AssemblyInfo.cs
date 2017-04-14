b272f3e04f3ae709af9c9c52b60215bb44c62090
Shouheng Yi <shouyi@microsoft.com>
2017-04-14 13:24:42 -0700

    Updated assembly info for Org.Apache.REEF.Experimental and Org.Apache.REEF.Experimental.Tests.

--- lang/cs/Org.Apache.REEF.Experimental/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.REEF.Experimental/Properties/AssemblyInfo.cs
@@ -16,38 +16,14 @@
 // under the License.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Org.Apache.REEF.Experimental")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Experimental Projects for REEF")]
 [assembly: AssemblyProduct("Org.Apache.REEF.Experimental")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c8ab57a8-98c0-4e6e-a99e-c5b62c2b6997")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
