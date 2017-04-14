47856a86841f3068c725585a2d303cf0fea20ede
Shouheng Yi <shouyi@microsoft.com>
2017-04-14 10:48:39 -0700

    Fixed some comments in the #1287.

--- lang/cs/Org.Apache.REEF.Experimental/ParquetReader/ParquetReader.cs
+++ lang/cs/Org.Apache.REEF.Experimental/ParquetReader/ParquetReader.cs
@@ -28,17 +28,30 @@ using Org.Apache.REEF.Utilities.Logging;
 
 namespace Org.Apache.REEF.Experimental.ParquetReader
 {
-    public class ParquetReader
+    sealed public class ParquetReader
     {
         private static readonly Logger Logger = Logger.GetLogger(typeof(ParquetReader));
 
         private readonly string _parquetPath;
         private readonly string _avroPath;
         private readonly string _jarPath;
-        private readonly string _mainClass = "org.apache.reef.experimental.parquet.ParquetReader";
 
-        private readonly Process _proc = new Process();
+        private class JavaProcessFactory
+        {
+            public string fileName { get; set; }
+            public string parquetPath { get; set; }
+            public string avroPath { get; set; }
+            public string jarPath { get; set; }
+            public string mainClass { get; set; }
+            public Process p { get; set; }
+        }
 
+        /// <summary>
+        /// Constructor of ParquetReader for Tang Injection
+        /// </summary>
+        /// <param name="parquetPath">Path to input parquet file.</param>
+        /// <param name="avroPath">Path to temp avro file.</param>
+        /// <param name="jarPath">Path to jar file that contains Java parquet reader.</param>
         [Inject]
         private ParquetReader(
             [Parameter(typeof(ParquetPathString))] string parquetPath, 
@@ -48,15 +61,28 @@ namespace Org.Apache.REEF.Experimental.ParquetReader
             _parquetPath = parquetPath;
             _avroPath = avroPath;
             _jarPath = jarPath;
-            _proc.StartInfo.FileName = "java";
-            _proc.StartInfo.Arguments = 
-                @"-cp " + jarPath + " " + _mainClass+ " " + parquetPath + " " + avroPath;
         }
 
+        /// <summary>
+        /// Method to read the given parquet files.
+        /// </summary>
         public IEnumerable<T> read<T>()
         {
-            _proc.Start();
-            _proc.WaitForExit();
+            JavaProcessFactory f = new JavaProcessFactory
+            {
+                fileName = "java",
+                parquetPath = _parquetPath,
+                avroPath = _avroPath,
+                jarPath = _jarPath,
+                mainClass = "org.apache.reef.experimental.parquet.ParquetReader",
+                p = new Process()
+            };
+
+            f.p.StartInfo.FileName = f.fileName;
+            f.p.StartInfo.Arguments = $"-cp {f.jarPath} {f.mainClass} {f.parquetPath} {f.avroPath}";
+            f.p.Start();
+            f.p.WaitForExit();
+
             Stream stream = new FileStream(_avroPath, FileMode.Open);
             using (var reader = AvroContainer.CreateReader<T>(stream))
             {
