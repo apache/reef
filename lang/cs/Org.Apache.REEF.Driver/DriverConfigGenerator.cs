b37cc1f0a34b52ea30a627e3cc6914a118006e16
taegeonum <taegeonum@gmail.com>
2017-08-04 10:24:33 +0900

    [REEF-1848] Update version to 0.17.0-SNAPSHOT
      * Update versions to 0.17-SNAPSHOT for POM and all other files
      * Add build.DotNet.props and ReefOnSpark.scala in change_version.py
    
    JIRA:
      [REEF-1848](https://issues.apache.org/jira/browse/REEF-1848)
    
    Pull Request:
      This closes #1353

--- lang/cs/Org.Apache.REEF.Driver/DriverConfigGenerator.cs
+++ lang/cs/Org.Apache.REEF.Driver/DriverConfigGenerator.cs
@@ -42,7 +42,7 @@ namespace Org.Apache.REEF.Driver
         /// <summary>
         /// The bridge JAR name.
         /// </summary>
-        public const string JavaBridgeJarFileName = "reef-bridge-java-0.16.0-SNAPSHOT-shaded.jar";
+        public const string JavaBridgeJarFileName = "reef-bridge-java-0.17.0-SNAPSHOT-shaded.jar";
 
         private static readonly Logger Log = Logger.GetLogger(typeof(DriverConfigGenerator));
 
