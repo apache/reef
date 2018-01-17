2b45d763949448bf1d9b6f3ed3e495f3c2cdd75d
Sharath Malladi <msharath@live.com>
2018-01-17 14:06:44 -0800

    Fixed descriptions of azbatch parameters

--- lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/parameters/AzureBatchAccountName.java
+++ lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/parameters/AzureBatchAccountName.java
@@ -3,6 +3,6 @@ package org.apache.reef.runime.azbatch.parameters;
 import org.apache.reef.tang.annotations.Name;
 import org.apache.reef.tang.annotations.NamedParameter;
 
-@NamedParameter(doc = "The Azure Batch Account URL")
+@NamedParameter(doc = "The Azure Batch Account Name")
 public class AzureBatchAccountName implements Name<String> {
 }
--- lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/parameters/AzureBatchPoolId.java
+++ lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/parameters/AzureBatchPoolId.java
@@ -3,6 +3,6 @@ package org.apache.reef.runime.azbatch.parameters;
 import org.apache.reef.tang.annotations.Name;
 import org.apache.reef.tang.annotations.NamedParameter;
 
-@NamedParameter(doc = "The Azure Batch pool name")
+@NamedParameter(doc = "The Azure Batch Pool Id")
 public class AzureBatchPoolId implements Name<String> {
 }
