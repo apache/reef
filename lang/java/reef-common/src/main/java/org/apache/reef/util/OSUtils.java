2702748ea6f43c0fa01f6461f778f4b97462bfe2
Chenxi Zhao <33109414+chzhams@users.noreply.github.com>
2018-02-12 11:50:20 -0800

    Adding return value check for process.waitFor()

--- lang/java/reef-common/src/main/java/org/apache/reef/util/OSUtils.java
+++ lang/java/reef-common/src/main/java/org/apache/reef/util/OSUtils.java
@@ -77,7 +77,10 @@ public final class OSUtils {
         final Process process = new ProcessBuilder()
             .command("bash", "-c", "echo $PPID")
             .start();
-        process.waitFor();
+        if (process.waitFor() != 0) {
+          LOG.log(Level.SEVERE, "Unable to determine PID");
+          return -1;
+        }
         final byte[] returnBytes = new byte[128];
         if (process.getInputStream().read(returnBytes) == -1) {
           LOG.log(Level.FINE, "No data read because end of stream was reached");
@@ -95,7 +98,10 @@ public final class OSUtils {
             .command("powershell.exe", "-NoProfile", "-Command",
                 "wmic process where processid=$pid get parentprocessid")
             .start();
-        process.waitFor();
+        if (process.waitFor() != 0) {
+          LOG.log(Level.SEVERE, "Unable to determine PID");
+          return -1;
+        }
         final byte[] returnBytes = new byte[128];
         if (process.getInputStream().read(returnBytes) == -1) {
           LOG.log(Level.FINE, "No data read because end of stream was reached");
