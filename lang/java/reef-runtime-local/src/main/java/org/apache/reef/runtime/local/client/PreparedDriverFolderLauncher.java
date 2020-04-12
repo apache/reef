1541fc8209c30d787bf5881213fbfbdd798935ea
Wooyeon Lee <wylee@apache.org>
2020-04-12 19:12:11 +0900

    throw driver process fail exception (local runtime)

--- lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/client/PreparedDriverFolderLauncher.java
+++ lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/client/PreparedDriverFolderLauncher.java
@@ -30,6 +30,7 @@ import javax.inject.Inject;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -89,8 +90,14 @@ public class PreparedDriverFolderLauncher {
         new LoggingRunnableProcessObserver(),
         stdoutFilePath,
         stderrFilePath);
-    this.executor.submit(process);
-    this.executor.shutdown();
+
+    try {
+      this.executor.submit(process).get();
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException("Driver process failed", e);
+    } finally {
+      this.executor.shutdown();
+    }
   }
 
   private List<String> makeLaunchCommand() {
