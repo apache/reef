apache/reef

commit 0715691b64b5ab887c14a20db880537f8240da0a

[REEF-2063] Propagate exceptions of driver process fail in local runtime (#1499)
[REEF-2063] Propagate exceptions of driver process fail in local runtime

Summary of changes:
- Catch and rethrow exceptions from spawning a driver process for a local job (local runtime).
- As a result, the client process can notice that the job has been failed to be submitted.

JIRA:
  [REEF-2063](https://issues.apache.org/jira/projects/REEF/issues/REEF-2063)
  
Pull Request:
  Closes #1499

lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/client/PreparedDriverFolderLauncher.java
@@ -30,6 +30,7 @@ import javax.inject.Inject;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -89,8 +90,15 @@ public class PreparedDriverFolderLauncher {
         new LoggingRunnableProcessObserver(),
         stdoutFilePath,
         stderrFilePath);
-    this.executor.submit(process);
-    this.executor.shutdown();
+
+    try {
+      this.executor.submit(process).get();
+    } catch (InterruptedException | ExecutionException e) {
+      LOG.log(Level.SEVERE, "Driver process failed");
+      throw new RuntimeException("Driver process failed", e);
+    } finally {
+      this.executor.shutdown();
+    }
   }
 
   private List<String> makeLaunchCommand() {

lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/process/RunnableProcess.java
@@ -200,6 +200,7 @@ public final class RunnableProcess implements Runnable {
 
       } catch (final IOException ex) {
         LOG.log(Level.SEVERE, "Unable to spawn process " + this.id + " with command " + this.command, ex);
+        throw new RuntimeException("Unable to spawn process " + this.id + " with command " + this.command, ex);
       }
 
     } finally {

commit 5eae868143286305de532c61e7047bd5cd805a35

[REEF-2066] Bugfix: unit tests that depend on client-side timeout hung indefinitely
Some unit tests depend on client-side DriverLauncher killing the Driver
process by timeout. That behavior was broken in PR #1499, and we have to
roll back that change.

This commit is a roll back of change from #1499

JIRA: [REEF-2066](https://issues.apache.org/jira/projects/REEF/issues/REEF-2066)

lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/client/PreparedDriverFolderLauncher.java
@@ -30,7 +30,6 @@ import javax.inject.Inject;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -90,15 +89,8 @@ public class PreparedDriverFolderLauncher {
         new LoggingRunnableProcessObserver(),
         stdoutFilePath,
         stderrFilePath);
-
-    try {
-      this.executor.submit(process).get();
-    } catch (InterruptedException | ExecutionException e) {
-      LOG.log(Level.SEVERE, "Driver process failed");
-      throw new RuntimeException("Driver process failed", e);
-    } finally {
-      this.executor.shutdown();
-    }
+    this.executor.submit(process);
+    this.executor.shutdown();
   }
 
   private List<String> makeLaunchCommand() {

lang/java/reef-runtime-local/src/main/java/org/apache/reef/runtime/local/process/RunnableProcess.java
@@ -200,7 +200,6 @@ public final class RunnableProcess implements Runnable {
 
       } catch (final IOException ex) {
         LOG.log(Level.SEVERE, "Unable to spawn process " + this.id + " with command " + this.command, ex);
-        throw new RuntimeException("Unable to spawn process " + this.id + " with command " + this.command, ex);
       }
 
     } finally {